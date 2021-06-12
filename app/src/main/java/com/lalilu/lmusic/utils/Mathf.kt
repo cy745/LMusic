de6a8e5603372b959c12ea4ea0c37408705b0e59
lalilu <1248393538@qq.com>
2021-06-12 21:23:28 +0800

    fix worry code

--- app/src/main/java/com/lalilu/lmusic/utils/Mathf.kt
+++ app/src/main/java/com/lalilu/lmusic/utils/Mathf.kt
@@ -3,8 +3,8 @@ package com.lalilu.lmusic.utils
 class Mathf {
     companion object {
         fun <T : Number> clamp(min: T, max: T, num: T): T {
-            if (num.toDouble() < min.toDouble()) return num
-            if (num.toDouble() > max.toDouble()) return max
+            if (num.toDouble() < min.toDouble()) return max
+            if (num.toDouble() > max.toDouble()) return min
             return num
         }
     }
