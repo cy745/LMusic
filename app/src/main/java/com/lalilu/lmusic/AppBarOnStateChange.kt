a4097c5057adc61ea769920fdc94183e0e32e756
lalilu <1248393538@qq.com>
2021-06-19 14:51:29 +0800

    create enum class AppBarState to replace variables

--- app/src/main/java/com/lalilu/lmusic/AppBarOnStateChange.kt
+++ app/src/main/java/com/lalilu/lmusic/AppBarOnStateChange.kt
@@ -4,30 +4,34 @@ import com.google.android.material.appbar.AppBarLayout
 import kotlin.math.abs
 
 abstract class AppBarOnStateChange : AppBarLayout.OnOffsetChangedListener {
-    companion object {
-        const val STATE_EXPANDED = 0
-        const val STATE_COLLAPSED = 1
-        const val STATE_INTERMEDIATE = 2
+    enum class AppBarState {
+        STATE_EXPANDED, STATE_COLLAPSED, STATE_INTERMEDIATE
     }
 
-    private var mCurrentState: Int = STATE_EXPANDED
+    private var mCurrentState: AppBarState = AppBarState.STATE_EXPANDED
 
     override fun onOffsetChanged(appBarLayout: AppBarLayout?, verticalOffset: Int) {
         if (appBarLayout == null) return
         if (verticalOffset == 0) {
-            if (mCurrentState != STATE_EXPANDED) onStateChanged(appBarLayout, STATE_EXPANDED)
-            mCurrentState = STATE_EXPANDED
-        } else if (abs(verticalOffset) >= appBarLayout.totalScrollRange) {
-            if (mCurrentState != STATE_COLLAPSED) onStateChanged(appBarLayout, STATE_COLLAPSED)
-            mCurrentState = STATE_COLLAPSED
-        } else {
-            if (mCurrentState != STATE_INTERMEDIATE) onStateChanged(
+            if (mCurrentState != AppBarState.STATE_EXPANDED) onStateChanged(
                 appBarLayout,
-                STATE_INTERMEDIATE
+                AppBarState.STATE_EXPANDED
             )
-            mCurrentState = STATE_INTERMEDIATE
+            mCurrentState = AppBarState.STATE_EXPANDED
+        } else if (abs(verticalOffset) >= appBarLayout.totalScrollRange) {
+            if (mCurrentState != AppBarState.STATE_COLLAPSED) onStateChanged(
+                appBarLayout,
+                AppBarState.STATE_COLLAPSED
+            )
+            mCurrentState = AppBarState.STATE_COLLAPSED
+        } else {
+            if (mCurrentState != AppBarState.STATE_INTERMEDIATE) onStateChanged(
+                appBarLayout,
+                AppBarState.STATE_INTERMEDIATE
+            )
+            mCurrentState = AppBarState.STATE_INTERMEDIATE
         }
     }
 
-    abstract fun onStateChanged(appBarLayout: AppBarLayout?, state: Int)
+    abstract fun onStateChanged(appBarLayout: AppBarLayout?, state: AppBarState)
 }
