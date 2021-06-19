2fdd8e9d9680c66c896e78bfed185ace1d73364c
lalilu <200141nm>
2021-06-20 01:10:36 +0800

    make ItemTouchCallback to be add independently single class in single file

---
+++ app/src/main/java/com/lalilu/lmusic/utils/ItemTouchCallback.kt
@@ -0,0 +1,48 @@
+package com.lalilu.lmusic.utils
+
+import android.graphics.Canvas
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.RecyclerView
+import com.lalilu.lmusic.adapter2.ItemTouch
+import kotlin.math.abs
+
+class ItemTouchCallback(private val itemTouch: ItemTouch) : ItemTouchHelper.Callback() {
+
+    override fun getMovementFlags(
+        recyclerView: RecyclerView,
+        viewHolder: RecyclerView.ViewHolder
+    ): Int {
+        val swipeFlags = ItemTouchHelper.RIGHT
+        return makeMovementFlags(0, swipeFlags)
+    }
+
+    override fun onMove(
+        recyclerView: RecyclerView,
+        viewHolder: RecyclerView.ViewHolder,
+        target: RecyclerView.ViewHolder
+    ): Boolean {
+        return itemTouch.onItemMove(viewHolder)
+    }
+
+    override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+        itemTouch.onItemSwiped(viewHolder)
+    }
+
+    override fun onChildDraw(
+        c: Canvas,
+        recyclerView: RecyclerView,
+        viewHolder: RecyclerView.ViewHolder,
+        dX: Float,
+        dY: Float,
+        actionState: Int,
+        isCurrentlyActive: Boolean
+    ) {
+        if (actionState == ItemTouchHelper.ACTION_STATE_SWIPE) {
+            //滑动时改变Item的透明度
+            val alpha: Float = 1 - abs(dX) / viewHolder.itemView.width
+            viewHolder.itemView.alpha = alpha
+        }
+        val x = (dX * 0.7).toFloat()
+        super.onChildDraw(c, recyclerView, viewHolder, x, dY, actionState, isCurrentlyActive)
+    }
+}
