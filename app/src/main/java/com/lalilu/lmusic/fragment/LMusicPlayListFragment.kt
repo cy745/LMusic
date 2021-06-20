088a5c3a2f6b893c0d17f7c548c692911f4e57d6
lalilu <1248393538@qq.com>
2021-06-20 15:12:48 +0800

    setup and initialize Playlist style

--- app/src/main/java/com/lalilu/lmusic/fragment/LMusicPlayListFragment.kt
+++ app/src/main/java/com/lalilu/lmusic/fragment/LMusicPlayListFragment.kt
@@ -4,19 +4,44 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.OvershootInterpolator
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.lalilu.R
 import com.lalilu.databinding.FragmentPlayListBinding
-import com.lalilu.lmusic.adapter2.MusicListAdapter
+import com.lalilu.lmusic.adapter2.LMusicPlayListAdapter
+import jp.wasabeef.recyclerview.animators.FadeInAnimator
 
 class LMusicPlayListFragment : Fragment() {
     private lateinit var mRecyclerView: RecyclerView
-    private lateinit var mAdapter: MusicListAdapter
+    private lateinit var mAdapter: LMusicPlayListAdapter
+    private lateinit var mViewModel: LMusicViewModel
+
+    private fun initializeObserver() {
+        mViewModel.playlist.observeForever {
+            it?.let {
+                mAdapter.playLists = it.toMutableList()
+                mAdapter.notifyDataSetChanged()
+            }
+        }
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         mRecyclerView = FragmentPlayListBinding.bind(view).playListRecyclerView
-        super.onViewCreated(view, savedInstanceState)
+        mRecyclerView.adapter = LMusicPlayListAdapter(requireContext())
+        mAdapter = mRecyclerView.adapter as LMusicPlayListAdapter
+        mViewModel = LMusicViewModel.getInstance(null)
+
+        mRecyclerView.isNestedScrollingEnabled = false
+        mRecyclerView.layoutManager = LinearLayoutManager(context)
+        mRecyclerView.itemAnimator = FadeInAnimator(OvershootInterpolator()).apply {
+            this.addDuration = 300
+            this.moveDuration = 200
+            this.removeDuration = 50
+        }
+
+        initializeObserver()
     }
 
     override fun onCreateView(
