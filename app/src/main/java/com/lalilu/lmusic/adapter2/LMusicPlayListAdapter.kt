b3a45a281e657b0a754b185239f6797654eefc32
lalilu <200141nm>
2021-06-20 15:12:48 +0800

    setup and initialize Playlist style

--- app/src/main/java/com/lalilu/lmusic/adapter2/LMusicPlayListAdapter.kt
+++ app/src/main/java/com/lalilu/lmusic/adapter2/LMusicPlayListAdapter.kt
@@ -1,21 +1,30 @@
 package com.lalilu.lmusic.adapter2
 
 import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.lalilu.R
+import com.lalilu.databinding.ItemPlayListItemBinding
+import com.lalilu.media.entity.LMusicPlayList
 
 class LMusicPlayListAdapter(private val context: Context) :
-    RecyclerView.Adapter<MusicListAdapter.SongHolder>() {
+    RecyclerView.Adapter<LMusicPlayListAdapter.PlayListItemHolder>() {
+    var playLists = mutableListOf<LMusicPlayList>()
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MusicListAdapter.SongHolder {
-        TODO("Not yet implemented")
+    override fun onBindViewHolder(holder: PlayListItemHolder, position: Int) {
+        holder.binding.playListTitle.text = playLists[position].playListTitle
     }
 
-    override fun onBindViewHolder(holder: MusicListAdapter.SongHolder, position: Int) {
-        TODO("Not yet implemented")
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PlayListItemHolder {
+        val view = LayoutInflater.from(context).inflate(R.layout.item_play_list_item, parent, false)
+        return PlayListItemHolder(view)
     }
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+    override fun getItemCount(): Int = playLists.size
+
+    inner class PlayListItemHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var binding: ItemPlayListItemBinding = ItemPlayListItemBinding.bind(itemView)
     }
 }
