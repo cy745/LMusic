5a9d8875df55c1af8b336539c558afd545ea944f
lalilu <1248393538@qq.com>
2021-06-20 13:24:19 +0800

    rename Fragment and xml file

--- app/src/main/java/com/lalilu/lmusic/fragment/LMusicPlayListFragment.kt
+++ app/src/main/java/com/lalilu/lmusic/fragment/LMusicPlayListFragment.kt
@@ -6,14 +6,20 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.lalilu.R
+import com.lalilu.databinding.FragmentPlayListBinding
 
-class PlayingFragment : Fragment() {
+class LMusicPlayListFragment : Fragment() {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        FragmentPlayListBinding.bind(view)
+        super.onViewCreated(view, savedInstanceState)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_playing, container, false)
+        return inflater.inflate(R.layout.fragment_play_list, container, false)
     }
 
 }
