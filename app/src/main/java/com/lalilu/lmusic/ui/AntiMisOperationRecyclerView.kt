09f6d71f980a7c2deab11f42beaabe2336cf1168
lalilu <1248393538@qq.com>
2021-06-20 20:44:45 +0800

    reform the code.

--- app/src/main/java/com/lalilu/lmusic/ui/AntiMisOperationRecyclerView.kt
+++ app/src/main/java/com/lalilu/lmusic/ui/AntiMisOperationRecyclerView.kt
@@ -7,24 +7,22 @@ import androidx.recyclerview.widget.RecyclerView
 
 class AntiMisOperationRecyclerView @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
-) : RecyclerView(context, attrs, defStyleAttr), RecyclerView.OnItemTouchListener {
+) : RecyclerView(context, attrs, defStyleAttr) {
+
     private var limitPadding = 40
 
     init {
-        addOnItemTouchListener(this)
+        addOnItemTouchListener(object : OnItemTouchListener {
+            override fun onInterceptTouchEvent(rv: RecyclerView, e: MotionEvent): Boolean {
+                return isMisOperation(e.rawX, limitPadding, width - limitPadding)
+            }
+
+            override fun onTouchEvent(rv: RecyclerView, e: MotionEvent) {}
+            override fun onRequestDisallowInterceptTouchEvent(disallowIntercept: Boolean) {}
+        })
     }
 
     private fun isMisOperation(raw: Number, limitLeft: Number, limitRight: Number): Boolean {
         return raw.toLong() < limitLeft.toLong() || raw.toLong() > limitRight.toLong()
-    }
-
-    override fun onInterceptTouchEvent(rv: RecyclerView, e: MotionEvent): Boolean {
-        return isMisOperation(e.rawX, limitPadding, width - limitPadding)
-    }
-
-    override fun onTouchEvent(rv: RecyclerView, e: MotionEvent) {
-    }
-
-    override fun onRequestDisallowInterceptTouchEvent(disallowIntercept: Boolean) {
     }
 }
