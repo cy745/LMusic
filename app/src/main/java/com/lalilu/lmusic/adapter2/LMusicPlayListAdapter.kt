ba6eb39cbce56749eda2f51186eebbef41ca23ec
lalilu <1248393538@qq.com>
2021-06-20 13:33:02 +0800

    create LMusicPlayListAdapter

---
+++ app/src/main/java/com/lalilu/lmusic/adapter2/LMusicPlayListAdapter.kt
@@ -0,0 +1,21 @@
+package com.lalilu.lmusic.adapter2
+
+import android.content.Context
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class LMusicPlayListAdapter(private val context: Context) :
+    RecyclerView.Adapter<MusicListAdapter.SongHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MusicListAdapter.SongHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: MusicListAdapter.SongHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}
