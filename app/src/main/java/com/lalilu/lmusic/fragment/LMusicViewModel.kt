68e215147d60bd2973e46849fc21268c5f27871c
lalilu <200141nm>
2021-06-20 15:11:49 +0800

    add playlist LiveData from LMusicMediaModule#database

--- app/src/main/java/com/lalilu/lmusic/fragment/LMusicViewModel.kt
+++ app/src/main/java/com/lalilu/lmusic/fragment/LMusicViewModel.kt
@@ -7,14 +7,17 @@ import android.support.v4.media.session.MediaControllerCompat
 import android.support.v4.media.session.PlaybackStateCompat
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
+import com.lalilu.media.LMusicMediaModule
 import org.jetbrains.annotations.Nullable
 
 class LMusicViewModel private constructor(application: Application) :
     AndroidViewModel(application) {
+    private val mediaDatabase = LMusicMediaModule.getInstance(application).database
     val mediaList = MutableLiveData<MutableList<MediaBrowserCompat.MediaItem>>(null)
     val metadata = MutableLiveData<MediaMetadataCompat>(null)
     val playBackState = MutableLiveData<PlaybackStateCompat>(null)
     val mediaController = MutableLiveData<MediaControllerCompat>(null)
+    val playlist = mediaDatabase.playlistDao().getAllLiveData()
 
     companion object {
         @Volatile
