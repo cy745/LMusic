b9f7ef5c7f31e07d8784c7883901c6accd67e685
lalilu <1248393538@qq.com>
2021-06-21 12:54:10 +0800

    handle Appbar scroll percentage to setup alpha of paletteDraweeView

--- app/src/main/java/com/lalilu/lmusic/ui/SquareAppBarLayout.kt
+++ app/src/main/java/com/lalilu/lmusic/ui/SquareAppBarLayout.kt
@@ -2,21 +2,34 @@ package com.lalilu.lmusic.ui
 
 import android.content.Context
 import android.util.AttributeSet
+import android.view.MotionEvent
 import com.google.android.material.appbar.AppBarLayout
 import com.lalilu.lmusic.utils.AppBarOnStateChange
 import com.lalilu.lmusic.utils.AppBarOnStateChange.AppBarState
+import net.opacapp.multilinecollapsingtoolbar.CollapsingToolbarLayout
 
 class SquareAppBarLayout @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : AppBarLayout(context, attrs, defStyleAttr) {
     var mAppbarState = AppBarState.STATE_EXPANDED
+    var paletteDraweeView: PaletteDraweeView? = null
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
         super.onMeasure(widthMeasureSpec, widthMeasureSpec)
     }
 
+    override fun onFinishInflate() {
+        super.onFinishInflate()
+        paletteDraweeView =
+            (getChildAt(0) as CollapsingToolbarLayout).getChildAt(0) as PaletteDraweeView
+    }
+
     init {
         addOnOffsetChangedListener(object : AppBarOnStateChange() {
+            override fun onStatePercentage(percent: Float) {
+                paletteDraweeView?.let { it.alpha = percent }
+            }
+
             override fun onStateChanged(appBarLayout: AppBarLayout?, state: AppBarState) {
                 mAppbarState = state
             }
