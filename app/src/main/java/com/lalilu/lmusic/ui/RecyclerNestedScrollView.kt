6b7427c204561758a8328c839c474568e8a9ed5d
lalilu <200141nm>
2021-06-21 12:56:15 +0800

    remove useless code

--- app/src/main/java/com/lalilu/lmusic/ui/RecyclerNestedScrollView.kt
+++ app/src/main/java/com/lalilu/lmusic/ui/RecyclerNestedScrollView.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.widget.LinearLayout
 import androidx.core.widget.NestedScrollView
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.appbar.AppBarLayout
 import com.lalilu.lmusic.fragment.LMusicViewModel
 import com.lalilu.lmusic.utils.AppBarOnStateChange
 
