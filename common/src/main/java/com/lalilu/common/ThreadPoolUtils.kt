d4228a61ef866f3d11fe7da0e9750307e189353c
lalilu <1248393538@qq.com>
2021-06-26 15:00:01 +0800

    重新设计媒体扫描模块

---
+++ common/src/main/java/com/lalilu/common/ThreadPoolUtils.kt
@@ -0,0 +1,22 @@
+package com.lalilu.common
+
+import java.util.concurrent.LinkedBlockingQueue
+import java.util.concurrent.ThreadPoolExecutor
+import java.util.concurrent.TimeUnit
+
+/**
+ * 线程池工具类
+ *
+ */
+class ThreadPoolUtils {
+    companion object {
+        fun newFixedThreadPool(numThread: Int): ThreadPoolExecutor {
+            return ThreadPoolExecutor(
+                numThread, numThread,
+                0L,
+                TimeUnit.MILLISECONDS,
+                LinkedBlockingQueue()
+            )
+        }
+    }
+}
