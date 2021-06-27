1af48e77f2bdf078501b46de54466b574dbdae55
lalilu <1248393538@qq.com>
2021-06-27 20:42:54 +0800

    适配RecyclerView的滑动和拖拽功能，用于给歌单内歌曲排序

--- app/src/main/java/com/lalilu/lmusic/adapter/LMusicPlayingAdapter.kt
+++ app/src/main/java/com/lalilu/lmusic/adapter/LMusicPlayingAdapter.kt
@@ -2,6 +2,7 @@ package com.lalilu.lmusic.adapter
 
 import android.support.v4.media.MediaBrowserCompat
 import com.chad.library.adapter.base.BaseQuickAdapter
+import com.chad.library.adapter.base.module.DraggableModule
 import com.chad.library.adapter.base.viewholder.BaseDataBindingHolder
 import com.lalilu.R
 import com.lalilu.databinding.ItemSongMediaItemBinding
@@ -9,7 +10,7 @@ import com.lalilu.databinding.ItemSongMediaItemBinding
 class LMusicPlayingAdapter :
     BaseQuickAdapter<MediaBrowserCompat.MediaItem, BaseDataBindingHolder<ItemSongMediaItemBinding>>(
         R.layout.item_song_media_item
-    ) {
+    ), DraggableModule {
 
     override fun convert(
         holder: BaseDataBindingHolder<ItemSongMediaItemBinding>,
--- app/src/main/java/com/lalilu/lmusic/fragment/LMusicPlayingFragment.kt
+++ app/src/main/java/com/lalilu/lmusic/fragment/LMusicPlayingFragment.kt
@@ -1,5 +1,7 @@
 package com.lalilu.lmusic.fragment
 
+import android.graphics.Canvas
+import android.media.MediaMetadata
 import android.os.Bundle
 import android.support.v4.media.MediaBrowserCompat
 import android.support.v4.media.session.MediaControllerCompat
@@ -7,12 +9,18 @@ import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.chad.library.adapter.base.listener.OnItemDragListener
+import com.chad.library.adapter.base.listener.OnItemSwipeListener
 import com.lalilu.R
 import com.lalilu.common.LMusicList
 import com.lalilu.databinding.FragmentNowPlayingBinding
 import com.lalilu.lmusic.adapter.LMusicPlayingAdapter
 import com.lalilu.lmusic.ui.AntiMisOperationRecyclerView
 import com.lalilu.player.LMusicPlayerModule
+import com.lalilu.player.service.LMusicService.Companion.ACTION_MOVED_SONG
+import com.lalilu.player.service.LMusicService.Companion.ACTION_SWIPED_SONG
+import com.lalilu.player.service.LMusicService.Companion.NEW_MEDIA_ORDER_LIST
 
 
 class LMusicPlayingFragment : Fragment(R.layout.fragment_now_playing) {
@@ -30,6 +38,48 @@ class LMusicPlayingFragment : Fragment(R.layout.fragment_now_playing) {
                     it.putInt(LMusicList.LIST_TRANSFORM_ACTION, LMusicList.ACTION_MOVE_TO)
                 })
         }
+        mAdapter.draggableModule.setOnItemSwipeListener(object : OnItemSwipeListener {
+            var mediaId: String? = null
+            override fun onItemSwipeStart(viewHolder: RecyclerView.ViewHolder?, pos: Int) {
+                mediaId = mAdapter.getItem(pos).mediaId
+            }
+
+            override fun clearView(viewHolder: RecyclerView.ViewHolder?, pos: Int) {}
+            override fun onItemSwiped(viewHolder: RecyclerView.ViewHolder?, pos: Int) {
+                println(mediaId ?: return)
+                mediaControllerCompat?.transportControls?.sendCustomAction(
+                    ACTION_SWIPED_SONG, Bundle().also {
+                        it.putString(
+                            MediaMetadata.METADATA_KEY_MEDIA_ID, mediaId
+                        )
+                    })
+            }
+
+            override fun onItemSwipeMoving(
+                canvas: Canvas?,
+                viewHolder: RecyclerView.ViewHolder?,
+                dX: Float,
+                dY: Float,
+                isCurrentlyActive: Boolean
+            ) {
+            }
+        })
+        mAdapter.draggableModule.setOnItemDragListener(object : OnItemDragListener {
+            override fun onItemDragStart(viewHolder: RecyclerView.ViewHolder?, pos: Int) {}
+            override fun onItemDragMoving(
+                source: RecyclerView.ViewHolder?, from: Int,
+                target: RecyclerView.ViewHolder?, to: Int
+            ) {
+            }
+
+            override fun onItemDragEnd(viewHolder: RecyclerView.ViewHolder?, pos: Int) {
+                val map = playerModule.mediaList.value?.map { it.mediaId } ?: return
+                mediaControllerCompat?.transportControls?.sendCustomAction(
+                    ACTION_MOVED_SONG, Bundle().also {
+                        it.putStringArrayList(NEW_MEDIA_ORDER_LIST, ArrayList(map))
+                    })
+            }
+        })
     }
 
     private fun initializeObserver() {
@@ -52,6 +102,11 @@ class LMusicPlayingFragment : Fragment(R.layout.fragment_now_playing) {
         mRecyclerView.layoutManager = LinearLayoutManager(context)
         mAdapter.setEmptyView(R.layout.item_empty_view)
         mAdapter.setDiffCallback(DiffMediaItem())
+        mAdapter.animationEnable = true
+        mAdapter.isAnimationFirstOnly = false
+        mAdapter.draggableModule.isDragEnabled = true
+        mAdapter.draggableModule.isSwipeEnabled = true
+        mAdapter.draggableModule.attachToRecyclerView(mRecyclerView)
 
         mViewModel = LMusicViewModel.getInstance(null)
         playerModule = LMusicPlayerModule.getInstance(null)
