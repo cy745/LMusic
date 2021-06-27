f69e34f76a31d3147e879a63592c62bbe01d3b4c
lalilu <1248393538@qq.com>
2021-06-27 17:04:09 +0800

    继承BaseQuickAdapter创建LMusicPlayingAdapter

---
+++ app/src/main/java/com/lalilu/lmusic/adapter/LMusicPlayingAdapter.kt
@@ -0,0 +1,21 @@
+package com.lalilu.lmusic.adapter
+
+import android.support.v4.media.MediaBrowserCompat
+import com.chad.library.adapter.base.BaseQuickAdapter
+import com.chad.library.adapter.base.viewholder.BaseDataBindingHolder
+import com.lalilu.R
+import com.lalilu.databinding.ItemSongMediaItemBinding
+
+class LMusicPlayingAdapter :
+    BaseQuickAdapter<MediaBrowserCompat.MediaItem, BaseDataBindingHolder<ItemSongMediaItemBinding>>(
+        R.layout.item_song_media_item
+    ) {
+
+    override fun convert(
+        holder: BaseDataBindingHolder<ItemSongMediaItemBinding>,
+        item: MediaBrowserCompat.MediaItem
+    ) {
+        val binding = holder.dataBinding ?: return
+        binding.mediaItem = item
+    }
+}
