6b683cfdadfeb251f3abb519f8faf8fb06361dde
lalilu <745703cy>
2021-08-22 18:06:55 +0800

    为List指明元素类型

--- app/src/main/java/com/lalilu/lmusic/binding_adapter/RecyclerViewBindingAdapter.java
+++ app/src/main/java/com/lalilu/lmusic/binding_adapter/RecyclerViewBindingAdapter.java
@@ -12,10 +12,12 @@ import com.chad.library.adapter.base.BaseNodeAdapter;
 import com.google.android.material.appbar.AppBarLayout;
 import com.lalilu.common.ColorUtils;
 import com.lalilu.lmusic.adapter.LMusicPlayingAdapter;
+import com.lalilu.lmusic.adapter.node.FirstNode;
 import com.lalilu.lmusic.ui.PaletteDraweeView;
 import com.lalilu.lmusic.ui.seekbar.LMusicSeekBar;
 import com.lalilu.lmusic.utils.ColorAnimator;
 import com.lalilu.media.entity.Music;
+import com.lalilu.media.entity.Playlist;
 
 import net.opacapp.multilinecollapsingtoolbar.CollapsingToolbarLayout;
 
@@ -74,14 +76,14 @@ public class RecyclerViewBindingAdapter {
             value = {"setMusicList"},
             requireAll = false
     )
-    public static void setMusicList(RecyclerView recyclerView, List list) {
+    public static void setMusicList(RecyclerView recyclerView, List<Music> list) {
         if (recyclerView.getAdapter() != null && list != null) {
             LMusicPlayingAdapter adapter = (LMusicPlayingAdapter) recyclerView.getAdapter();
 
             if (adapter.getData().size() > 0) {
                 long id = adapter.getData().get(0).getMusicId();
                 adapter.setDiffNewData(list, () -> {
-                    if (id != ((Music) list.get(0)).getMusicId()) {
+                    if (id != list.get(0).getMusicId()) {
                         recyclerView.scrollToPosition(0);
                     }
                 });
@@ -95,7 +97,7 @@ public class RecyclerViewBindingAdapter {
             value = {"setPlayList"},
             requireAll = false
     )
-    public static void setPlayList(RecyclerView recyclerView, List list) {
+    public static void setPlayList(RecyclerView recyclerView, List<FirstNode<Playlist>> list) {
         if (recyclerView.getAdapter() != null && list != null) {
             BaseNodeAdapter adapter = (BaseNodeAdapter) recyclerView.getAdapter();
             adapter.setList(list);
