5d1527b06b2e22e56e130882cfc4222402bca205
lalilu <745703cy>
2021-08-24 14:16:58 +0800

    添加注释

--- app/src/main/java/com/lalilu/lmusic/ui/AntiMisOperationRecyclerView.kt
+++ app/src/main/java/com/lalilu/lmusic/ui/AntiMisOperationRecyclerView.kt
@@ -5,17 +5,20 @@ import android.util.AttributeSet
 import android.view.MotionEvent
 import androidx.recyclerview.widget.RecyclerView
 
+/**
+ * 附带边缘防误触功能的 RecyclerView
+ */
 class AntiMisOperationRecyclerView @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : RecyclerView(context, attrs, defStyleAttr) {
 
     private var limitPadding = 40
 
-    override fun onTouchEvent(e: MotionEvent?): Boolean {
+    override fun onTouchEvent(e: MotionEvent): Boolean {
         val location = IntArray(2)
         this.getLocationInWindow(location)
 
-        val result = e?.action == MotionEvent.ACTION_MOVE
+        val result = e.action == MotionEvent.ACTION_MOVE
                 && location[1] > measuredWidth
         requestDisallowInterceptTouchEvent(result)
         return super.onTouchEvent(e)
