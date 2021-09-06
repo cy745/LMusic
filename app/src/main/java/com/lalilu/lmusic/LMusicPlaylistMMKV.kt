15dfd1eefa766ef3b48f1727a91f2c0b244f19fc
lalilu <745703cy>
2021-09-06 13:52:36 +0800

    添加查找指定元素的方法

--- app/src/main/java/com/lalilu/lmusic/LMusicPlaylistMMKV.kt
+++ app/src/main/java/com/lalilu/lmusic/LMusicPlaylistMMKV.kt
@@ -1,6 +1,7 @@
 package com.lalilu.lmusic
 
 import android.os.Parcelable
+import com.lalilu.lmusic.utils.SnowFlakeUtils
 import com.lalilu.media.entity.LPlaylist
 import com.lalilu.media.entity.LSong
 import com.tencent.mmkv.MMKV
@@ -9,26 +10,50 @@ import kotlinx.parcelize.Parcelize
 /**
  * 用MMKV实现的歌单存储方法
  */
-class LMusicPlaylistMMKV {
+class LMusicPlaylistMMKV private constructor() {
     private val mmkv = MMKV.defaultMMKV()
 
     companion object {
         const val PLAYLIST_LOCAL = "playlist_local"
         const val PLAYLIST_LOCAL_TITLE = "本地歌曲"
+        const val PLAYLIST_LOCAL_ID = -1L
 
         const val PLAYLISTS_ALL = "all_playlist"
+
+        @Volatile
+        private var instance: LMusicPlaylistMMKV? = null
+
+        fun getInstance(): LMusicPlaylistMMKV {
+            instance ?: synchronized(LMusicPlaylistMMKV::class.java) {
+                instance = instance ?: LMusicPlaylistMMKV()
+            }
+            return instance!!
+        }
     }
 
     /**
      * 读取本地歌曲歌单
      */
-    fun readLocal(): LPlaylist {
+    fun readLocalPlaylist(): LPlaylist {
         return mmkv.decodeParcelable(PLAYLIST_LOCAL, LPlaylist::class.java)
-            ?: LPlaylist(PLAYLIST_LOCAL_TITLE)
+            ?: LPlaylist(PLAYLIST_LOCAL_ID, PLAYLIST_LOCAL_TITLE)
     }
 
-    fun saveSongToLocal(song: LSong) {
-        saveLocal(readLocal().also {
+    /**
+     * 通过Id读取歌曲
+     */
+    fun readLocalSongById(id: Long): LSong? {
+        readLocalPlaylist().songs?.let { songs ->
+            return songs[songs.indexOf(LSong(id, ""))]
+        }
+        return null
+    }
+
+    /**
+     * 保存歌曲到本地歌单中
+     */
+    fun saveSongToLocalPlaylist(song: LSong) {
+        saveLocalPlaylist(readLocalPlaylist().also {
             if (it.songs?.indexOf(song) == -1) {
                 it.songs?.add(song)
             }
@@ -38,26 +63,35 @@ class LMusicPlaylistMMKV {
     /**
      * 保存本地歌曲歌单
      */
-    fun saveLocal(playlist: LPlaylist) {
+    fun saveLocalPlaylist(playlist: LPlaylist) {
         mmkv.encode(PLAYLIST_LOCAL, playlist)
     }
 
     /**
      * 创建新的歌单
      */
-    fun create(title: String, intro: String?, artUri: String) {
-        val playlist = LPlaylist(title, intro, artUri)
+    fun createPlaylist(title: String, intro: String?, artUri: String) {
+        val playlist = LPlaylist(SnowFlakeUtils.getFlowIdInstance().nextId(), title, intro, artUri)
 
-        save(read().also {
+        saveAllPlaylist(readAllPlaylist().also {
             it.playlists.add(playlist)
         })
+    }
+
+    /**
+     * 通过歌单Id获取歌单
+     */
+    fun readPlaylistById(id: Long): LPlaylist? {
+        readAllPlaylist().also {
+            return it.playlists[it.playlists.indexOf(LPlaylist(id, ""))]
+        }
     }
 
     /**
      * 添加歌曲到指定歌单中
      */
     fun addSongToPlaylist(song: LSong, playlist: LPlaylist) {
-        save(read().also {
+        saveAllPlaylist(readAllPlaylist().also {
             it.playlists[it.playlists.indexOf(playlist)].songs?.add(song)
         })
     }
@@ -65,7 +99,7 @@ class LMusicPlaylistMMKV {
     /**
      * 读取所有自创建歌单 (本地歌单除外)
      */
-    fun read(): LocalPlaylists {
+    fun readAllPlaylist(): LocalPlaylists {
         return mmkv.decodeParcelable(PLAYLISTS_ALL, LocalPlaylists::class.java)
             ?: LocalPlaylists(ArrayList())
     }
@@ -73,7 +107,7 @@ class LMusicPlaylistMMKV {
     /**
      * 保存自创建歌单
      */
-    fun save(playlists: LocalPlaylists) {
+    fun saveAllPlaylist(playlists: LocalPlaylists) {
         mmkv.encode(PLAYLISTS_ALL, playlists)
     }
 
