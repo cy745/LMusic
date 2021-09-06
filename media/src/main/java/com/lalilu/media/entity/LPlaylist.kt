7efde705cbaad5895715720fb2476983788fea08
lalilu <745703cy>
2021-09-06 13:52:16 +0800

    修改equals方法

--- media/src/main/java/com/lalilu/media/entity/LPlaylist.kt
+++ media/src/main/java/com/lalilu/media/entity/LPlaylist.kt
@@ -5,6 +5,7 @@ import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class LPlaylist(
+    var id: Long,
     // 歌单标题
     var title: String,
     // 歌单介绍
@@ -14,24 +15,18 @@ data class LPlaylist(
     // 歌单内歌曲
     var songs: ArrayList<LSong>? = null
 ) : Parcelable {
-
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
         if (javaClass != other?.javaClass) return false
 
         other as LPlaylist
 
-        if (title != other.title) return false
-        if (intro != other.intro) return false
-        if (artUri != other.artUri) return false
+        if (id != other.id) return false
 
         return true
     }
 
     override fun hashCode(): Int {
-        var result = title.hashCode()
-        result = 31 * result + (intro?.hashCode() ?: 0)
-        result = 31 * result + (artUri?.hashCode() ?: 0)
-        return result
+        return id.hashCode()
     }
 }
