d1fea48e526cfca3cd2c7d7f0408f9132787042c
lalilu <745703cy>
2021-09-06 13:51:40 +0800

    重新替换Database为MMKV的新类

---
+++ app/src/main/java/com/lalilu/lmusic/domain/request/NowPlaylistRequest2.kt
@@ -0,0 +1,27 @@
+package com.lalilu.lmusic.domain.request
+
+import com.lalilu.lmusic.LMusicPlaylistMMKV
+import com.lalilu.lmusic.domain.BaseRequest
+import com.lalilu.media.entity.LPlaylist
+
+/**
+ * 获取 playlist 的 request 对象，可通过 requestData 请求更新
+ */
+class NowPlaylistRequest2 : BaseRequest<LPlaylist>() {
+    private val lMusicPlaylistMMKV = LMusicPlaylistMMKV.getInstance()
+
+    override fun requestData(value: Any?) {
+        // 若传进来的 value 为空或非法，则重新从数据库中获取全部歌曲
+        if (value == null || value !is Long) {
+            requestData()
+            return
+        }
+        // getPlaylistWithSongsByIdWithOrder 获取到的 playlist 保持存储时的顺序
+        val playlist = lMusicPlaylistMMKV.readPlaylistById(value)
+        if (playlist != null) postData(playlist) else requestData()
+    }
+
+    override fun requestData() {
+        postData(lMusicPlaylistMMKV.readLocalPlaylist())
+    }
+}
