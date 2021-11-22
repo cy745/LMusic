11b598121f8571de327fc444f5f585609d017483
lalilu <lalilu>
2021-11-22 17:12:55 +0800

    因布局调整，将对应代码转移

--- app/src/main/java/com/lalilu/lmusic/fragment/PlayingFragment.kt
+++ app/src/main/java/com/lalilu/lmusic/fragment/PlayingFragment.kt
@@ -1,5 +1,8 @@
 package com.lalilu.lmusic.fragment
 
+import android.os.Bundle
+import android.support.v4.media.session.PlaybackStateCompat
+import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import com.lalilu.BR
 import com.lalilu.R
@@ -7,18 +10,24 @@ import com.lalilu.databinding.FragmentNowPlayingBinding
 import com.lalilu.lmusic.adapter.LMusicPlayingAdapter
 import com.lalilu.lmusic.base.BaseFragment
 import com.lalilu.lmusic.base.DataBindingConfig
+import com.lalilu.lmusic.domain.entity.LSong
 import com.lalilu.lmusic.event.SharedViewModel
 import com.lalilu.lmusic.service.LMusicPlayerModule
 import com.lalilu.lmusic.state.PlayingFragmentViewModel
+import com.lalilu.lmusic.utils.Mathf
 import com.lalilu.lmusic.utils.OnItemDragAdapter
 import com.lalilu.lmusic.utils.OnItemSwipedAdapter
-import com.lalilu.lmusic.domain.entity.LSong
+import java.util.*
+import kotlin.collections.ArrayList
+import kotlin.concurrent.schedule
 
 class PlayingFragment : BaseFragment() {
     private lateinit var mState: PlayingFragmentViewModel
     private lateinit var mEvent: SharedViewModel
     private lateinit var mAdapter: LMusicPlayingAdapter
     private lateinit var playerModule: LMusicPlayerModule
+    private var positionTimer: Timer? = null
+
     override var delayLoadDuration: Long = 100
 
     override fun initViewModel() {
@@ -68,5 +77,39 @@ class PlayingFragment : BaseFragment() {
         mEvent.nowPlaylistRequest.getData().observe(viewLifecycleOwner) {
             mState.musicList.value = it
         }
+
+        val binding = (mBinding as FragmentNowPlayingBinding)
+//        binding.fmTabLayout.bindToViewPager(binding.fmViewpager)
+        binding.fmLyricViewX.setLabel("暂无歌词")
+
+        mState.nowBgPalette.observe(viewLifecycleOwner) {
+            mEvent.nowBgPalette.postValue(it)
+        }
+        mEvent.nowPlayingRequest.getData().observe(viewLifecycleOwner) {
+            it?.let { mState.nowPlayingMusic.postValue(it) }
+        }
+        mState.nowPlayingMusic.observe(viewLifecycleOwner) {
+            val lyric = it.mLocalInfo?.mLyric
+            binding.fmLyricViewX.loadLyric(lyric)
+        }
+        playerModule.playBackState.observe(viewLifecycleOwner) {
+            it ?: return@observe
+            var currentDuration = Mathf.getPositionFromPlaybackStateCompat(it)
+
+            positionTimer?.cancel()
+            if (it.state == PlaybackStateCompat.STATE_PLAYING)
+                positionTimer = Timer().apply {
+                    this.schedule(0, 1000) {
+                        binding.fmLyricViewX.updateTime(currentDuration)
+                        currentDuration += 1000
+                    }
+                }
+        }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val binding = (mBinding as FragmentNowPlayingBinding)
+        mActivity!!.setSupportActionBar(binding.fmToolbar)
     }
 }
