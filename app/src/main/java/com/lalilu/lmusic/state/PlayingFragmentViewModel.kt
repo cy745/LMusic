4f8a44868de5b50c7f687f4b862b6e148210983b
lalilu <lalilu>
2021-11-22 17:12:55 +0800

    因布局调整，将对应代码转移

--- app/src/main/java/com/lalilu/lmusic/state/PlayingFragmentViewModel.kt
+++ app/src/main/java/com/lalilu/lmusic/state/PlayingFragmentViewModel.kt
@@ -2,8 +2,12 @@ package com.lalilu.lmusic.state
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.palette.graphics.Palette
 import com.lalilu.lmusic.domain.entity.LPlaylist
+import com.lalilu.lmusic.domain.entity.LSong
 
 class PlayingFragmentViewModel : ViewModel() {
     val musicList: MutableLiveData<LPlaylist> = MutableLiveData()
+    val nowBgPalette: MutableLiveData<Palette> = MutableLiveData()
+    val nowPlayingMusic: MutableLiveData<LSong> = MutableLiveData()
 }
