599422b343eaa6077a7bfa220f547e875bec1527
lalilu <lalilu>
2021-11-22 17:08:53 +0800

    简化mainFragment的职责，使其专职负责各个页面的跳转

--- app/src/main/java/com/lalilu/lmusic/fragment/MainFragment.kt
+++ app/src/main/java/com/lalilu/lmusic/fragment/MainFragment.kt
@@ -1,7 +1,6 @@
 package com.lalilu.lmusic.fragment
 
 import android.os.Bundle
-import android.support.v4.media.session.PlaybackStateCompat
 import android.view.View
 import androidx.databinding.library.baseAdapters.BR
 import androidx.recyclerview.widget.RecyclerView
@@ -13,15 +12,11 @@ import com.lalilu.lmusic.base.DataBindingConfig
 import com.lalilu.lmusic.event.SharedViewModel
 import com.lalilu.lmusic.service.LMusicPlayerModule
 import com.lalilu.lmusic.state.MainViewModel
-import com.lalilu.lmusic.utils.Mathf.Companion.getPositionFromPlaybackStateCompat
-import java.util.*
-import kotlin.concurrent.schedule
 
 class MainFragment : BaseFragment() {
     private lateinit var mState: MainViewModel
     private lateinit var mEvent: SharedViewModel
     private lateinit var playerModule: LMusicPlayerModule
-    private var positionTimer: Timer? = null
 
     private var mPagerAdapter: LMusicFragmentStateAdapter? = null
     override var delayLoadDuration: Long = 100
@@ -42,42 +37,14 @@ class MainFragment : BaseFragment() {
     }
 
     override fun loadInitData() {
-        val binding = (mBinding as FragmentMainBinding)
-        binding.fmTabLayout.bindToViewPager(binding.fmViewpager)
-        binding.fmLyricViewX.setLabel("暂无歌词")
-
-        mState.nowBgPalette.observe(viewLifecycleOwner) {
-            mEvent.nowBgPalette.postValue(it)
-        }
-        mEvent.nowPlayingRequest.getData().observe(viewLifecycleOwner) {
-            it?.let { mState.nowPlayingMusic.postValue(it) }
-        }
         mEvent.pageRequest.getData().observe(viewLifecycleOwner) {
             it?.let { mState.nowPageInt.postValue(it) }
-        }
-        mState.nowPlayingMusic.observe(viewLifecycleOwner) {
-            val lyric = it.mLocalInfo?.mLyric
-            binding.fmLyricViewX.loadLyric(lyric)
-        }
-        playerModule.playBackState.observe(viewLifecycleOwner) {
-            it ?: return@observe
-            var currentDuration = getPositionFromPlaybackStateCompat(it)
-
-            positionTimer?.cancel()
-            if (it.state == PlaybackStateCompat.STATE_PLAYING)
-                positionTimer = Timer().apply {
-                    this.schedule(0, 1000) {
-                        binding.fmLyricViewX.updateTime(currentDuration)
-                        currentDuration += 1000
-                    }
-                }
         }
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val binding = (mBinding as FragmentMainBinding)
-        mActivity!!.setSupportActionBar(binding.fmToolbar)
 
         val child = binding.fmViewpager.getChildAt(0)
         if (child is RecyclerView) child.overScrollMode = View.OVER_SCROLL_NEVER
--- app/src/main/java/com/lalilu/lmusic/state/MainViewModel.kt
+++ app/src/main/java/com/lalilu/lmusic/state/MainViewModel.kt
@@ -2,11 +2,7 @@ package com.lalilu.lmusic.state
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.palette.graphics.Palette
-import com.lalilu.lmusic.domain.entity.LSong
 
 class MainViewModel : ViewModel() {
     val nowPageInt: MutableLiveData<Int> = MutableLiveData(0)
-    val nowBgPalette: MutableLiveData<Palette> = MutableLiveData()
-    val nowPlayingMusic: MutableLiveData<LSong> = MutableLiveData()
 }
