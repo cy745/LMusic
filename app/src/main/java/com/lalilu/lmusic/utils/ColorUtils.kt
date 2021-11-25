9883af0e0b68f33bfbb08b4fb95533e8e00b9a24
lalilu <lalilu>
2021-11-25 13:27:29 +0800

    更新代码结构

--- app/src/main/java/com/lalilu/lmusic/utils/ColorUtils.kt
+++ app/src/main/java/com/lalilu/lmusic/utils/ColorUtils.kt
@@ -11,22 +11,20 @@ fun Palette?.getAutomaticColor(): Int {
     return oldColor
 }
 
-class ColorUtils {
-    companion object {
-        fun isLightColor(color: Int): Boolean {
-            val darkness =
-                1 - (0.299 * Color.red(color) + 0.587 * Color.green(color) + 0.114 * Color.blue(
-                    color
-                )) / 255
-            return darkness < 0.5
-        }
+object ColorUtils {
+    fun isLightColor(color: Int): Boolean {
+        val darkness =
+            1 - (0.299 * Color.red(color) + 0.587 * Color.green(color) + 0.114 * Color.blue(
+                color
+            )) / 255
+        return darkness < 0.5
+    }
 
-        fun getAutomaticColor(palette: Palette?): Int {
-            if (palette == null) return Color.DKGRAY
-            var oldColor = palette.getDarkVibrantColor(Color.LTGRAY)
-            if (isLightColor(oldColor))
-                oldColor = palette.getDarkMutedColor(Color.LTGRAY)
-            return oldColor
-        }
+    fun getAutomaticColor(palette: Palette?): Int {
+        if (palette == null) return Color.DKGRAY
+        var oldColor = palette.getDarkVibrantColor(Color.LTGRAY)
+        if (isLightColor(oldColor))
+            oldColor = palette.getDarkMutedColor(Color.LTGRAY)
+        return oldColor
     }
 }
