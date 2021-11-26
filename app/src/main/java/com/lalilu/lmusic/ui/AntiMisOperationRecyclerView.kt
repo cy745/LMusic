460dbd0880133d36df86bb020ee1d770eb82cc15
lalilu <lalilu>
2021-11-27 00:53:42 +0800

    toFix：在平板上的小窗模式中，防误触的面积计算错误，待修复解决

--- app/src/main/java/com/lalilu/lmusic/ui/AntiMisOperationRecyclerView.kt
+++ app/src/main/java/com/lalilu/lmusic/ui/AntiMisOperationRecyclerView.kt
@@ -2,7 +2,6 @@ package com.lalilu.lmusic.ui
 
 import android.content.Context
 import android.util.AttributeSet
-import android.view.MotionEvent
 import androidx.recyclerview.widget.RecyclerView
 
 /**
@@ -12,30 +11,31 @@ class AntiMisOperationRecyclerView @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : RecyclerView(context, attrs, defStyleAttr) {
 
-    private var limitPadding = 40
-
-    override fun onTouchEvent(e: MotionEvent): Boolean {
-        val location = IntArray(2)
-        this.getLocationInWindow(location)
-
-        val result = e.action == MotionEvent.ACTION_MOVE
-                && location[1] > measuredWidth
-        requestDisallowInterceptTouchEvent(result)
-        return super.onTouchEvent(e)
-    }
-
-    init {
-        addOnItemTouchListener(object : OnItemTouchListener {
-            override fun onInterceptTouchEvent(rv: RecyclerView, e: MotionEvent): Boolean {
-                return isMisOperation(e.rawX, limitPadding, width - limitPadding)
-            }
-
-            override fun onTouchEvent(rv: RecyclerView, e: MotionEvent) {}
-            override fun onRequestDisallowInterceptTouchEvent(disallowIntercept: Boolean) {}
-        })
-    }
-
-    private fun isMisOperation(raw: Number, limitLeft: Number, limitRight: Number): Boolean {
-        return raw.toLong() < limitLeft.toLong() || raw.toLong() > limitRight.toLong()
-    }
+    //todo 在平板上的小窗模式中，防误触的面积计算错误，待修复解决
+//    private var limitPadding = 40
+//
+//    override fun onTouchEvent(e: MotionEvent): Boolean {
+//        val location = IntArray(2)
+//        this.getLocationInWindow(location)
+//
+//        val result = e.action == MotionEvent.ACTION_MOVE
+//                && location[1] > measuredWidth
+//        requestDisallowInterceptTouchEvent(result)
+//        return super.onTouchEvent(e)
+//    }
+//
+//    init {
+//        addOnItemTouchListener(object : OnItemTouchListener {
+//            override fun onInterceptTouchEvent(rv: RecyclerView, e: MotionEvent): Boolean {
+//                return isMisOperation(e.rawX, limitPadding, width - limitPadding)
+//            }
+//
+//            override fun onTouchEvent(rv: RecyclerView, e: MotionEvent) {}
+//            override fun onRequestDisallowInterceptTouchEvent(disallowIntercept: Boolean) {}
+//        })
+//    }
+//
+//    private fun isMisOperation(raw: Number, limitLeft: Number, limitRight: Number): Boolean {
+//        return raw.toLong() < limitLeft.toLong() || raw.toLong() > limitRight.toLong()
+//    }
 }
