a1670eba69fe37acd400bccb5f4aebc1af7287e3
lalilu <lalilu>
2021-11-26 22:36:00 +0800

    转移Activity中的组件至MainFragment，以适配旋转切换布局

--- app/src/main/java/com/lalilu/lmusic/state/MainActivityViewModel.kt
+++ app/src/main/java/com/lalilu/lmusic/state/MainActivityViewModel.kt
@@ -1,9 +1,6 @@
 package com.lalilu.lmusic.state
 
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.palette.graphics.Palette
 
 class MainActivityViewModel : ViewModel() {
-    val nowBgPalette: MutableLiveData<Palette> = MutableLiveData()
 }
