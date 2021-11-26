9dee12d080adbcfef887d350b2c575f3b8f1bd72
lalilu <lalilu>
2021-11-26 22:32:00 +0800

    更新DeviceUtil，添加判断设备是否是平板的方法

--- app/src/main/java/com/lalilu/lmusic/utils/DeviceUtil.kt
+++ app/src/main/java/com/lalilu/lmusic/utils/DeviceUtil.kt
@@ -4,6 +4,8 @@ import android.content.Context
 import android.os.Build
 import android.util.DisplayMetrics
 import android.view.WindowManager
+import kotlin.math.pow
+import kotlin.math.sqrt
 
 /**
  * 获取设备特有信息的工具集合类
@@ -20,5 +22,38 @@ object DeviceUtil {
         val outMetrics = DisplayMetrics()
         windowManager.defaultDisplay.getRealMetrics(outMetrics)
         return outMetrics.heightPixels
+    }
+
+    fun getWidth(context: Context): Int {
+        val windowManager = context.getSystemService(Context.WINDOW_SERVICE) as WindowManager
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            return windowManager.currentWindowMetrics.bounds.width()
+        }
+
+        val outMetrics = DisplayMetrics()
+        windowManager.defaultDisplay.getRealMetrics(outMetrics)
+        return outMetrics.widthPixels
+    }
+
+    /**
+     * 是否是平板
+     *
+     * @param context 上下文
+     * @return 是平板则返回true，反之返回false
+     *
+     * ————————————————
+     * 版权声明：本文为CSDN博主「Fantasy丶Lin」的原创文章，遵循CC 4.0 BY-SA版权协议，转载请附上原文出处链接及本声明。
+     * 原文链接：https://blog.csdn.net/Fantasy_Lin_/article/details/111828002
+     */
+    fun isPad(context: Context): Boolean {
+        val windowManager = context.getSystemService(Context.WINDOW_SERVICE) as WindowManager
+        val display = windowManager.defaultDisplay
+        val dm = DisplayMetrics()
+        display.getMetrics(dm)
+        val x = (dm.widthPixels / dm.xdpi).pow(2)
+        val y = (dm.heightPixels / dm.ydpi).pow(2)
+        val screenInches = sqrt(x + y)
+        return screenInches >= 7.0
     }
 }
