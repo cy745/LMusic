e73f69875c0cd31e1dc9a27cf42277cde7aa5b8d
lalilu <lalilu>
2021-11-27 14:44:51 +0800

    转移属性至AppBarStatusHelper集中管理

--- app/src/main/java/com/lalilu/lmusic/ui/appbar/SquareAppBarLayout.kt
+++ app/src/main/java/com/lalilu/lmusic/ui/appbar/SquareAppBarLayout.kt
@@ -8,17 +8,14 @@ class SquareAppBarLayout @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : AppBarLayout(context, attrs, defStyleAttr) {
 
-    var fullyExpend = false
-    var mBottom: Int = 0
-
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
         super.onMeasure(widthMeasureSpec, widthMeasureSpec)
     }
 
     override fun onLayout(changed: Boolean, l: Int, t: Int, r: Int, b: Int) {
         // 如果处于展开状态则重新布局一次AppbarLayout的bottom
-        if (fullyExpend && changed) {
-            this.layout(l, t, r, mBottom)
+        if (AppBarStatusHelper.fullyExpend && changed) {
+            this.layout(l, t, r, AppBarStatusHelper.mBottom)
         }
 
         super.onLayout(changed, l, t, r, b)
