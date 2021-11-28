05126c4224723294f5fbf8b28221e22db4542030
lalilu <lalilu>
2021-11-28 15:11:05 +0800

    简化BitmapUtils

--- app/src/main/java/com/lalilu/lmusic/utils/BitmapUtils.kt
+++ app/src/main/java/com/lalilu/lmusic/utils/BitmapUtils.kt
@@ -5,63 +5,62 @@ import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.media.MediaMetadataRetriever
 import android.net.Uri
+import android.os.Environment
 import androidx.core.net.toFile
 import java.io.File
 import java.io.FileOutputStream
 
-class BitmapUtils {
-    companion object {
+object BitmapUtils {
+    fun saveThumbnailToSandBox(
+        context: Context,
+        mediaItemId: Long,
+        mediaItemUri: Uri
+    ): Uri {
+        val path: File = context.getExternalFilesDir(Environment.DIRECTORY_PICTURES)!!
 
-        @Throws
-        fun saveThumbnailToSandBox(
-            context: Context,
-            path: File,
-            mediaItemId: Long,
-            mediaItemUri: Uri
-        ): Uri? {
+        try {
             val metadataRetriever = MediaMetadataRetriever()
             metadataRetriever.setDataSource(context, mediaItemUri)
 
-            val embeddedPic = metadataRetriever.embeddedPicture ?: return null
+            val embeddedPic = metadataRetriever.embeddedPicture ?: return Uri.EMPTY
             val outputStream = FileOutputStream("$path/${mediaItemId}")
             outputStream.write(embeddedPic)
             outputStream.flush()
             outputStream.close()
             metadataRetriever.release()
+        } catch (e: Exception) {
+            return Uri.EMPTY
+        }
+        return loadThumbnail(path, mediaItemId)
+    }
 
-            return loadThumbnail(path, mediaItemId)
+    fun loadThumbnail(path: File, mediaItemId: Long): Uri {
+        val file = File("$path/${mediaItemId}")
+        return Uri.fromFile(file)
+    }
+
+    fun loadBitmapFromUri(uri: Uri, toSize: Int): Bitmap? {
+        val outOption = BitmapFactory.Options().also {
+            it.inJustDecodeBounds = true
+        }
+        try {
+            BitmapFactory.decodeStream(uri.toFile().inputStream(), null, outOption)
+        } catch (e: Exception) {
+            return null
         }
 
-        fun loadThumbnail(path: File, mediaItemId: Long): Uri {
-            val file = File("$path/${mediaItemId}")
-            return Uri.fromFile(file)
+        val outWidth = outOption.outWidth
+        val outHeight = outOption.outHeight
+        if (outWidth == -1 || outHeight == -1) return null
+
+        var scaleValue: Int = if (outWidth > toSize) outWidth / toSize else toSize / outWidth
+        if (scaleValue < 1) scaleValue = 1
+
+        outOption.also {
+            it.inJustDecodeBounds = false
+            it.inSampleSize = scaleValue
         }
 
-        fun loadBitmapFromUri(uri: Uri, toSize: Int): Bitmap? {
-            val outOption = BitmapFactory.Options().also {
-                it.inJustDecodeBounds = true
-            }
-            try {
-                BitmapFactory.decodeStream(uri.toFile().inputStream(), null, outOption)
-            } catch (e: Exception) {
-                return null
-            }
-
-            val outWidth = outOption.outWidth
-            val outHeight = outOption.outHeight
-            if (outWidth == -1 || outHeight == -1) return null
-
-            var scaleValue: Int = if (outWidth > toSize) outWidth / toSize else toSize / outWidth
-            if (scaleValue < 1) scaleValue = 1
-
-            outOption.also {
-                it.inJustDecodeBounds = false
-                it.inSampleSize = scaleValue
-            }
-
-            return BitmapFactory.decodeStream(uri.toFile().inputStream(), null, outOption)
-        }
-
-
+        return BitmapFactory.decodeStream(uri.toFile().inputStream(), null, outOption)
     }
 }
