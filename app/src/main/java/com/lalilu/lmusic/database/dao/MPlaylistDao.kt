060b09c4b044d6f7bb13d4b5e37fee8c7190149d
lalilu <lalilu>
2021-11-28 23:18:10 +0800

    getById标注可能为空

--- app/src/main/java/com/lalilu/lmusic/database/dao/MPlaylistDao.kt
+++ app/src/main/java/com/lalilu/lmusic/database/dao/MPlaylistDao.kt
@@ -8,7 +8,7 @@ import com.lalilu.lmusic.domain.entity.PlaylistWithSongs
 interface MPlaylistDao {
     @Transaction
     @Query("SELECT * FROM m_playlist WHERE playlist_id = :id;")
-    fun getById(id: Long): PlaylistWithSongs
+    fun getById(id: Long): PlaylistWithSongs?
 
     @Transaction
     @Query("SELECT * FROM m_playlist;")
