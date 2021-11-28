8bc48c239e13064be8bebd00d9099a1cfca07313
lalilu <lalilu>
2021-11-28 20:11:14 +0800

    规范Request的异步同步使用

--- app/src/main/java/com/lalilu/lmusic/domain/request/NowMSongRequest.kt
+++ app/src/main/java/com/lalilu/lmusic/domain/request/NowMSongRequest.kt
@@ -7,13 +7,21 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 
+/**
+ * 获取 MSong 的 request 对象，可通过 requireData 请求更新
+ */
 class NowMSongRequest : BaseRequest<MSong>() {
     private val dao = LMusicDataBase.getInstance(null).songDao()
 
-    override fun requestData(value: Any?) {
+    override fun requireData(value: Any?) {
+        super.requireData(value)
         if (value == null || value !is Long) return
+
         GlobalScope.launch(Dispatchers.IO) {
             postData(dao.getById(value))
         }
     }
+
+    override fun requestData() {}
+    override fun requestData(value: Any?) {}
 }
--- app/src/main/java/com/lalilu/lmusic/domain/request/NowPlaylistWithSongsRequest.kt
+++ app/src/main/java/com/lalilu/lmusic/domain/request/NowPlaylistWithSongsRequest.kt
@@ -8,16 +8,20 @@ import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 
 /**
- * 获取 PlaylistWithSongs 的 request 对象，可通过 requestData 请求更新
+ * 获取 PlaylistWithSongs 的 request 对象，可通过 requireData 请求更新
  */
 class NowPlaylistWithSongsRequest : BaseRequest<PlaylistWithSongs>() {
     private val dao = LMusicDataBase.getInstance(null).playlistDao()
 
-    override fun requestData(value: Any?) {
+    override fun requireData(value: Any?) {
+        super.requestData(value)
         if (value == null || value !is Long) return
 
         GlobalScope.launch(Dispatchers.IO) {
             postData(dao.getById(value))
         }
     }
+
+    override fun requestData() {}
+    override fun requestData(value: Any?) {}
 }
