8a96b8d14b453a8459f72b3495bebf6b537e0b41
lalilu <lalilu>
2021-11-28 20:12:56 +0800

    优化代码

--- app/src/main/java/com/lalilu/lmusic/event/SharedViewModel.kt
+++ app/src/main/java/com/lalilu/lmusic/event/SharedViewModel.kt
@@ -3,11 +3,15 @@ package com.lalilu.lmusic.event
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.palette.graphics.Palette
-import com.lalilu.lmusic.domain.entity.LPlaylist
-import com.lalilu.lmusic.domain.entity.LSong
+import com.lalilu.lmusic.database.LMusicDataBase
+import com.lalilu.lmusic.domain.entity.MSong
+import com.lalilu.lmusic.domain.entity.PlaylistWithSongs
 import com.lalilu.lmusic.domain.request.*
 import com.lalilu.lmusic.utils.Mathf
 import com.tencent.mmkv.MMKV
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 /**
  *  为数据的全局同步而设置的 ViewModel ，即 mEvent，
@@ -15,12 +19,17 @@ import com.tencent.mmkv.MMKV
  *
  */
 class SharedViewModel : ViewModel() {
+    private val database = LMusicDataBase.getInstance(null)
+
     val nowBgPalette: MutableLiveData<Palette> = MutableLiveData()
 
     val nowPlaylistId = MutableLiveData<Long>(null)
     val nowPlayingId = MutableLiveData<Long>(null)
 
+    @Deprecated("nowMSongRequest 替代，后期删除")
     val nowPlayingRequest = NowPlayingRequest()
+
+    @Deprecated("nowPlaylistWithSongsRequest 替代，后期删除")
     val nowPlaylistRequest = NowPlaylistRequest()
 
     val nowMSongRequest = NowMSongRequest()
@@ -36,54 +45,37 @@ class SharedViewModel : ViewModel() {
     }
 
     init {
-        // 监听正在播放中的 playlistId
-        nowPlaylistId.observeForever {
-            // 如果 nowPlaylistId 的 value 为空则从 mmkv 获取信息
-            // 否则将其写入 mmkv 的映射区
-            var playlistId = it
+        // 监听正在播放的歌曲的 songId
+        nowPlayingId.observeForever {
+            GlobalScope.launch(Dispatchers.IO) {
+                val songId = it ?: mmkv.decodeLong(LAST_MUSIC_ID, 0)
+                mmkv.encode(LAST_MUSIC_ID, songId)
 
-            if (playlistId == null) {
-                playlistId = mmkv.decodeLong(LAST_PLAYLIST_ID, 0)
-            } else {
-                mmkv.encode(LAST_PLAYLIST_ID, playlistId)
+                val song = database.songDao().getById(songId)
+                nowMSongRequest.postData(song)
+
+                val oldPlaylist = nowPlaylistWithSongsRequest.getData().value ?: return@launch
+                val newPlayList = listOrderChange(oldPlaylist, song) ?: return@launch
+                nowPlaylistWithSongsRequest.postData(newPlayList)
             }
-
-            // 根据 playlistId 更新指定的 playlist 到 nowPlaylistRequest
-            nowPlaylistRequest.requestData(playlistId)
         }
-
-        // 监听正在播放中的 music
-        nowPlayingId.observeForever { songId ->
-            // 如果 music 为空，则从 mmkv 获取上一次播放的 musicId
-            // 否则将 music 的 musicId 存入 mmkv 的映射区
-            var nowSongId = songId
-
-            if (nowSongId == null) {
-                nowSongId = mmkv.decodeLong(LAST_MUSIC_ID, 0)
-            } else {
-                mmkv.encode(LAST_MUSIC_ID, nowSongId)
-            }
-
-            // 请求获取指定Id的Song
-            nowPlayingRequest.requestData(nowSongId)
-
-            // 根据正在播放的 song 改变 playlist 的顺序
-            // 即正在播放的 song 处于 playlist 的首部
-            val oldPlaylist = nowPlaylistRequest.getData().value ?: return@observeForever
-            val nowPlayingSong = nowPlayingRequest.getData().value ?: return@observeForever
-            val result = listOrderChange(oldPlaylist, nowPlayingSong) ?: return@observeForever
-            nowPlaylistRequest.postData(result)
+        // 监听正在播放的歌单的 playlistId
+        nowPlaylistId.observeForever {
+            val playlistId = it ?: mmkv.decodeLong(LAST_PLAYLIST_ID, 0)
+            mmkv.encode(LAST_PLAYLIST_ID, playlistId)
+            nowPlaylistWithSongsRequest.requireData(playlistId)
         }
     }
 
-    private fun listOrderChange(oldList: LPlaylist, song: LSong): LPlaylist? {
-        val nowPosition = oldList.songs?.indexOf(song)
-        if (nowPosition == -1 || nowPosition == null) return null
+    private fun listOrderChange(oldList: PlaylistWithSongs, newSong: MSong): PlaylistWithSongs? {
+        val nowPosition = oldList.songs.indexOf(newSong)
+        if (nowPosition == -1) return null
 
-        val songList = oldList.songs ?: return null
-        oldList.songs = ArrayList(songList.map {
-            val position =
-                Mathf.clampInLoop(0, songList.size - 1, songList.indexOf(it), nowPosition)
+        val songList = oldList.songs
+        oldList.songs = ArrayList(songList.map { song ->
+            val position = Mathf.clampInLoop(
+                0, songList.size - 1, songList.indexOf(song), nowPosition
+            )
             songList[position]
         })
 
