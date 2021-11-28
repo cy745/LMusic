0b8d0a868ecedfb1fc2b6e6081089792da4e0c97
lalilu <lalilu>
2021-11-28 19:04:59 +0800

    创建MSongPlayingAdapter

---
+++ app/src/main/java/com/lalilu/lmusic/adapter/MSongPlayingAdapter.kt
@@ -0,0 +1,40 @@
+package com.lalilu.lmusic.adapter
+
+import androidx.recyclerview.widget.DiffUtil
+import com.chad.library.adapter.base.BaseQuickAdapter
+import com.chad.library.adapter.base.module.DraggableModule
+import com.chad.library.adapter.base.viewholder.BaseDataBindingHolder
+import com.lalilu.R
+import com.lalilu.databinding.ItemPlayingBinding
+import com.lalilu.lmusic.domain.entity.MSong
+
+class MSongPlayingAdapter :
+    BaseQuickAdapter<MSong, BaseDataBindingHolder<ItemPlayingBinding>>(
+        R.layout.item_playing
+    ), DraggableModule {
+
+    init {
+        setDiffCallback(DiffSong())
+        setEmptyView(R.layout.item_empty_view)
+    }
+
+    inner class DiffSong : DiffUtil.ItemCallback<MSong>() {
+        override fun areItemsTheSame(oldItem: MSong, newItem: MSong): Boolean {
+            return oldItem.songId == newItem.songId
+        }
+
+        override fun areContentsTheSame(oldItem: MSong, newItem: MSong): Boolean {
+            return oldItem.songId == newItem.songId &&
+                    oldItem.songTitle == newItem.songTitle
+        }
+    }
+
+    override fun convert(holder: BaseDataBindingHolder<ItemPlayingBinding>, item: MSong) {
+        val binding = holder.dataBinding ?: return
+        binding.title = item.songTitle
+        binding.duration = item.songDuration
+        binding.artist = item.showingArtist
+        binding.mimeType = item.songMimeType
+        binding.coverUri = item.songCoverUri
+    }
+}
