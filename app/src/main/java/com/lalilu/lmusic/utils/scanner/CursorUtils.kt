738816f7e1ebbe9af0ea22ec1f94db133a1e6ac8
lalilu <lalilu>
2021-11-28 15:11:33 +0800

    创建CursorUtils，方便从Cursor中获取指定的数据

---
+++ app/src/main/java/com/lalilu/lmusic/utils/scanner/CursorUtils.kt
@@ -0,0 +1,45 @@
+package com.lalilu.lmusic.utils.scanner
+
+import android.database.Cursor
+import android.provider.MediaStore
+
+
+fun Cursor.getSongId(): Long {
+    return this.getLong(this.getColumnIndex(MediaStore.Audio.Media._ID))
+}
+
+fun Cursor.getSongTitle(): String {
+    return this.getString(this.getColumnIndex(MediaStore.Audio.Media.TITLE))
+}
+
+fun Cursor.getAlbumId(): Long {
+    return this.getLong(this.getColumnIndex(MediaStore.Audio.Media.ALBUM_ID))
+}
+
+fun Cursor.getAlbumTitle(): String {
+    return this.getString(this.getColumnIndex(MediaStore.Audio.Media.ALBUM))
+}
+
+fun Cursor.getArtist(): String {
+    return this.getString(this.getColumnIndex(MediaStore.Audio.Media.ARTIST))
+}
+
+fun Cursor.getArtists(): List<String> {
+    return this.getArtist().split("/")
+}
+
+fun Cursor.getSongSize(): Long {
+    return this.getLong(this.getColumnIndex(MediaStore.Audio.Media.SIZE))
+}
+
+fun Cursor.getSongData(): String {
+    return this.getString(this.getColumnIndex(MediaStore.Audio.Media.DATA))
+}
+
+fun Cursor.getSongDuration(): Long {
+    return this.getLong(this.getColumnIndex(MediaStore.Audio.Media.DURATION))
+}
+
+fun Cursor.getSongMimeType(): String {
+    return this.getString(this.getColumnIndex(MediaStore.Audio.Media.MIME_TYPE))
+}
