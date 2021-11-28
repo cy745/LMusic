6f1b636ccdacfd10832b032b3ef9b53e34b6fe87
lalilu <lalilu>
2021-11-28 19:03:26 +0800

    简化dao接口

--- app/src/main/java/com/lalilu/lmusic/database/dao/MPlaylistDao.kt
+++ app/src/main/java/com/lalilu/lmusic/database/dao/MPlaylistDao.kt
@@ -2,8 +2,6 @@ package com.lalilu.lmusic.database.dao
 
 import androidx.room.*
 import com.lalilu.lmusic.domain.entity.MPlaylist
-import com.lalilu.lmusic.domain.entity.MSong
-import com.lalilu.lmusic.domain.entity.PlaylistSongCrossRef
 import com.lalilu.lmusic.domain.entity.PlaylistWithSongs
 
 @Dao
@@ -17,23 +15,11 @@ interface MPlaylistDao {
     fun getAll(): List<PlaylistWithSongs>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun save(playlist: MPlaylist)
-
-
-    @Transaction
-    @Insert(onConflict = OnConflictStrategy.REPLACE)
-    fun savePlaylistWithSongs(playlist: MPlaylist, song: MSong, crossRef: PlaylistSongCrossRef)
+    fun save(playlist: MPlaylist)
 
     @Transaction
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    fun savePlaylistWithSongs(playlist: MPlaylist, songs: List<MSong>) {
-        songs.forEach { song ->
-            savePlaylistWithSongs(
-                playlist, song,
-                PlaylistSongCrossRef(playlist.playlistId, song.songId)
-            )
-        }
-    }
+    fun save(playlist: List<MPlaylist>)
 
     @Query("DELETE FROM m_playlist;")
     suspend fun deleteAll()
--- app/src/main/java/com/lalilu/lmusic/database/dao/MSongDao.kt
+++ app/src/main/java/com/lalilu/lmusic/database/dao/MSongDao.kt
@@ -1,7 +1,8 @@
 package com.lalilu.lmusic.database.dao
 
 import androidx.room.*
-import com.lalilu.lmusic.domain.entity.*
+import com.lalilu.lmusic.domain.entity.FullSongInfo
+import com.lalilu.lmusic.domain.entity.MSong
 
 @Dao
 interface MSongDao {
@@ -19,33 +20,12 @@ interface MSongDao {
     @Query("SELECT * FROM m_song WHERE song_id = :songId")
     fun getSingleFullSong(songId: Long): FullSongInfo
 
-    @Transaction
-    @Insert(onConflict = OnConflictStrategy.REPLACE)
-    fun saveSongWithArtists(song: MSong, artist: MArtist, crossRef: ArtistSongCrossRef)
-
-    @Transaction
-    @Insert(onConflict = OnConflictStrategy.REPLACE)
-    fun saveSongWithArtists(song: MSong, artists: List<MArtist>) {
-        artists.forEach { artist ->
-            saveSongWithArtists(
-                song, artist, ArtistSongCrossRef(song.songId, artist.artistName)
-            )
-        }
-    }
-
-    @Transaction
-    @Insert(onConflict = OnConflictStrategy.REPLACE)
-    fun saveSongWithPlaylist(song: MSong, playlists: List<MPlaylist>)
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    fun save(song: MSong)
 
     @Query("DELETE FROM m_song;")
     suspend fun deleteAll()
 
     @Delete
     fun delete(song: MSong)
-
-    // 60219
-    // QU4RTZ[中須かすみ(CV.相良茉優)、近江彼方(CV.鬼頭明里)、エマ・ヴェルデ(CV.指出毬亜)、天王寺璃奈(CV.田中ちえ美)]
-
-    // 60225
-    // DiverDiva
 }
