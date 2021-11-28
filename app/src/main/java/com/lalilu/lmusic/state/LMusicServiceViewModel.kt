2e28ea87c46d0b8a2496f2e2f673f6c64b3e366a
lalilu <lalilu>
2021-11-28 15:52:45 +0800

    添加livedata字段

--- app/src/main/java/com/lalilu/lmusic/state/LMusicServiceViewModel.kt
+++ app/src/main/java/com/lalilu/lmusic/state/LMusicServiceViewModel.kt
@@ -4,8 +4,13 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.lalilu.lmusic.domain.entity.LPlaylist
 import com.lalilu.lmusic.domain.entity.LSong
+import com.lalilu.lmusic.domain.entity.MSong
+import com.lalilu.lmusic.domain.entity.PlaylistWithSongs
 
 class LMusicServiceViewModel : ViewModel() {
     val nowPlayingList = MutableLiveData<LPlaylist>()
     val nowPlayingMusic = MutableLiveData<LSong>()
+
+    val playingPlaylist = MutableLiveData<PlaylistWithSongs>()
+    val playingSong = MutableLiveData<MSong>()
 }
