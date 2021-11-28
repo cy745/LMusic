a4750d366b4e5aec0837e7d502d2e802059b29ac
lalilu <lalilu>
2021-11-28 19:10:11 +0800

    简化代码

--- app/src/main/java/com/lalilu/lmusic/LMusicApp.kt
+++ app/src/main/java/com/lalilu/lmusic/LMusicApp.kt
@@ -6,12 +6,9 @@ import com.hjq.permissions.XXPermissions
 import com.lalilu.lmusic.base.BaseApplication
 import com.lalilu.lmusic.database.LMusicDataBase
 import com.lalilu.lmusic.service.LMusicPlayerModule
-import com.lalilu.lmusic.utils.scanner.LMusicScanner
 import com.tencent.mmkv.MMKV
 
 class LMusicApp : BaseApplication() {
-    lateinit var playlistMMKV: LMusicPlaylistMMKV
-    lateinit var lmusicScanner: LMusicScanner
 
     override fun onCreate() {
         super.onCreate()
@@ -19,9 +16,6 @@ class LMusicApp : BaseApplication() {
         XXPermissions.setScopedStorage(true)
         LMusicPlayerModule.getInstance(this)
         LMusicDataBase.getInstance(this)
-
-        playlistMMKV = LMusicPlaylistMMKV.getInstance()
-        lmusicScanner = LMusicScanner(this)
 
         val config = ImagePipelineConfig.newBuilder(this)
             .setDownsampleEnabled(true)
