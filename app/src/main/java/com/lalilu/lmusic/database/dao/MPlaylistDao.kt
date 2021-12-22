89fe3917af744c5e37c190258bf72339bfc8c899
lalilu <1248393538@qq.com>
2021-12-22 10:36:53 +0800

    添加获取Flow的数据获取方法

--- app/src/main/java/com/lalilu/lmusic/database/dao/MPlaylistDao.kt
+++ app/src/main/java/com/lalilu/lmusic/database/dao/MPlaylistDao.kt
@@ -3,6 +3,7 @@ package com.lalilu.lmusic.database.dao
 import androidx.room.*
 import com.lalilu.lmusic.domain.entity.MPlaylist
 import com.lalilu.lmusic.domain.entity.PlaylistWithSongs
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface MPlaylistDao {
@@ -11,8 +12,16 @@ interface MPlaylistDao {
     fun getById(id: Long): PlaylistWithSongs?
 
     @Transaction
+    @Query("SELECT * FROM m_playlist WHERE playlist_id = :id;")
+    fun getByIdFlow(id: Long): Flow<PlaylistWithSongs?>
+
+    @Transaction
     @Query("SELECT * FROM m_playlist;")
     fun getAll(): List<PlaylistWithSongs>
+
+    @Transaction
+    @Query("SELECT * FROM m_playlist;")
+    fun getAllFlow(): Flow<List<PlaylistWithSongs>>
 
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     fun save(playlist: MPlaylist)
