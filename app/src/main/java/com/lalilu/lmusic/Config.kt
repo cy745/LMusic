09c36634b1b22a272d2efb3d4115b7c34a542d5b
lalilu <1248393538@qq.com>
2021-12-22 10:45:55 +0800

    处理由Service获取到的数据，设置外部的直接访问只能读不能写

--- app/src/main/java/com/lalilu/lmusic/Config.kt
+++ app/src/main/java/com/lalilu/lmusic/Config.kt
@@ -1,8 +1,8 @@
 package com.lalilu.lmusic
 
-class Config {
-
-    companion object {
-        const val MEDIA_MIME_TYPE = "media_mime_type";
-    }
+object Config {
+    const val MEDIA_MIME_TYPE = "media_mime_type"
+    const val LAST_METADATA = "LAST_METADATA"
+    const val LAST_PLAYBACK_STATE = "LAST_PLAYBACK_STATE"
+    const val LAST_PLAYLIST_ID = "LAST_PLAYLIST_ID"
 }
--- app/src/main/java/com/lalilu/lmusic/service/LMusicPlayerModule.kt
+++ app/src/main/java/com/lalilu/lmusic/service/LMusicPlayerModule.kt
@@ -7,26 +7,63 @@ import android.support.v4.media.MediaMetadataCompat
 import android.support.v4.media.session.MediaControllerCompat
 import android.support.v4.media.session.PlaybackStateCompat
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import com.lalilu.lmusic.Config.LAST_METADATA
+import com.lalilu.lmusic.Config.LAST_PLAYBACK_STATE
+import com.lalilu.lmusic.utils.Mathf
+import com.tencent.mmkv.MMKV
 import dagger.hilt.android.qualifiers.ApplicationContext
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
 import java.util.logging.Logger
 import javax.inject.Inject
 import javax.inject.Singleton
+import kotlin.coroutines.CoroutineContext
 
 @Singleton
 class LMusicPlayerModule @Inject constructor(
     @ApplicationContext context: Context
-) : ViewModel() {
+) : ViewModel(), CoroutineScope {
+    override val coroutineContext: CoroutineContext = Dispatchers.Default
+
     private val logger = Logger.getLogger(this.javaClass.name)
+    private val mmkv = MMKV.defaultMMKV()
 
-    private val _metadata = MutableLiveData<MediaMetadataCompat>(null)
-    val metadata: LiveData<MediaMetadataCompat>
-        get() = _metadata
+    private val _metadata =
+        MutableStateFlow(mmkv.decodeParcelable(LAST_METADATA, MediaMetadataCompat::class.java))
 
-    private val _playBackState = MutableLiveData<PlaybackStateCompat>(null)
-    val playBackState: LiveData<PlaybackStateCompat>
-        get() = _playBackState
+    private val _playBackState =
+        MutableStateFlow(
+            mmkv.decodeParcelable(LAST_PLAYBACK_STATE, PlaybackStateCompat::class.java)
+        )
+
+    private val _mediaItems: MutableStateFlow<MutableList<MediaBrowserCompat.MediaItem>> =
+        MutableStateFlow(ArrayList())
+
+    @ExperimentalCoroutinesApi
+    val metadata: LiveData<MediaMetadataCompat?>
+        get() = _metadata.mapLatest {
+            if (it != null) mmkv.encode(LAST_METADATA, it)
+            return@mapLatest it
+        }.asLiveData()
+
+    @ExperimentalCoroutinesApi
+    val playBackState: LiveData<PlaybackStateCompat?>
+        get() = _playBackState.mapLatest {
+            if (it != null) mmkv.encode(LAST_PLAYBACK_STATE, it)
+            return@mapLatest it
+        }.asLiveData()
+
+    val mediaItem: LiveData<MutableList<MediaBrowserCompat.MediaItem>>
+        get() = _mediaItems.combine(_metadata) { mediaItem, metadata ->
+            listOrderChange(mediaItem, metadata?.description?.mediaId) ?: mediaItem
+        }.flowOn(Dispatchers.Default)
+            .conflate()
+            .asLiveData()
 
     lateinit var mediaController: MediaControllerCompat
     private var controllerCallback: MusicControllerCallback = MusicControllerCallback()
@@ -37,10 +74,31 @@ class LMusicPlayerModule @Inject constructor(
         connectionCallback, null
     )
 
+    private fun listOrderChange(
+        oldList: List<MediaBrowserCompat.MediaItem>,
+        mediaId: String?
+    ): MutableList<MediaBrowserCompat.MediaItem>? {
+        mediaId ?: return null
+
+        val nowPosition = oldList.indexOfFirst { item ->
+            item.description.mediaId == mediaId
+        }
+        if (nowPosition == -1) return null
+
+        return ArrayList(oldList.map { song ->
+            val position = Mathf.clampInLoop(
+                0, oldList.size - 1, oldList.indexOf(song), nowPosition
+            )
+            oldList[position]
+        })
+    }
+
     fun connect() = mediaBrowser.connect()
     fun disconnect() = mediaBrowser.disconnect().also {
         mediaController.unregisterCallback(controllerCallback)
     }
+
+    fun subscribe(parentId: String) = mediaBrowser.subscribe(parentId, subscriptionCallback)
 
     inner class MusicConnectionCallback(private val context: Context) :
         MediaBrowserCompat.ConnectionCallback() {
@@ -49,7 +107,7 @@ class LMusicPlayerModule @Inject constructor(
                 registerCallback(controllerCallback)
             }
 
-            mediaBrowser.subscribe("ACCESS_ID", subscriptionCallback)
+            subscribe(MSongService.MEDIA_ID_EMPTY_ROOT)
             logger.info("[MusicConnectionCallback]#onConnected")
         }
     }
@@ -59,21 +117,26 @@ class LMusicPlayerModule @Inject constructor(
             parentId: String,
             children: MutableList<MediaBrowserCompat.MediaItem>
         ) {
-            // TODO: 2021/12/20 处理Service连接成功后，Service下传的数据列表
-
-            logger.info("[MusicSubscriptionCallback]#onChildrenLoaded: $parentId")
+            launch {
+                _mediaItems.emit(children)
+                logger.info("[MusicSubscriptionCallback]#onChildrenLoaded: $parentId")
+            }
         }
     }
 
     inner class MusicControllerCallback : MediaControllerCompat.Callback() {
         override fun onPlaybackStateChanged(state: PlaybackStateCompat?) {
-            this@LMusicPlayerModule._playBackState.postValue(state ?: return)
-            logger.info("[MusicControllerCallback]#onPlaybackStateChanged: " + state.state)
+            launch {
+                _playBackState.emit(state ?: return@launch)
+                logger.info("[MusicControllerCallback]#onPlaybackStateChanged: ${state.state}")
+            }
         }
 
         override fun onMetadataChanged(metadata: MediaMetadataCompat?) {
-            this@LMusicPlayerModule._metadata.postValue(metadata ?: return)
-            logger.info("[MusicControllerCallback]#onMetadataChanged")
+            launch {
+                _metadata.emit(metadata ?: return@launch)
+                logger.info("[MusicControllerCallback]#onMetadataChanged: ${metadata.description.title}")
+            }
         }
     }
 }
