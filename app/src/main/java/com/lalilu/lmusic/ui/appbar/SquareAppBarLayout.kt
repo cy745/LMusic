4df6163a033926a221c69fcba552e9c7facf7263
lalilu <1248393538@qq.com>
2021-12-23 11:23:27 +0800

    拦截展开后指定区域的触摸事件，避免与导航手势冲突

--- app/src/main/java/com/lalilu/lmusic/ui/appbar/SquareAppBarLayout.kt
+++ app/src/main/java/com/lalilu/lmusic/ui/appbar/SquareAppBarLayout.kt
@@ -1,12 +1,18 @@
 package com.lalilu.lmusic.ui.appbar
 
+import android.annotation.SuppressLint
 import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Rect
 import android.util.AttributeSet
+import android.view.MotionEvent
 import com.google.android.material.appbar.AppBarLayout
 
 class SquareAppBarLayout @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : AppBarLayout(context, attrs, defStyleAttr) {
+    private val rect = Rect(0, 0, 0, 0)
+    private val size = 100
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
         super.onMeasure(widthMeasureSpec, widthMeasureSpec)
@@ -19,5 +25,17 @@ class SquareAppBarLayout @JvmOverloads constructor(
         }
 
         super.onLayout(changed, l, t, r, b)
+    }
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+        rect.bottom = height
+        rect.top = height - size
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        return if (event.rawY > rect.top && event.rawY < rect.bottom) true
+        else super.onTouchEvent(event)
     }
 }
