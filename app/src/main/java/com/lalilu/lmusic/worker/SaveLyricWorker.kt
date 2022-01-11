4e17c49ff4c0bab29d20046ecfac4c2b236c127d
lalilu <1248393538@qq.com>
2022-01-11 14:25:57 +0800

    拆分扫描后保存数据至数据库的任务为若干个Worker

---
+++ app/src/main/java/com/lalilu/lmusic/worker/SaveLyricWorker.kt
@@ -0,0 +1,52 @@
+package com.lalilu.lmusic.worker
+
+import android.content.Context
+import androidx.hilt.work.HiltWorker
+import androidx.work.CoroutineWorker
+import androidx.work.WorkerParameters
+import com.lalilu.lmusic.database.repository.ScannerRepository
+import com.lalilu.lmusic.domain.entity.MSongDetail
+import com.lalilu.lmusic.utils.ThreadPoolUtils
+import dagger.assisted.Assisted
+import dagger.assisted.AssistedInject
+import kotlinx.coroutines.asCoroutineDispatcher
+import kotlinx.coroutines.withContext
+import org.jaudiotagger.audio.AudioFileIO
+import org.jaudiotagger.tag.FieldKey
+import java.io.File
+import java.util.logging.Level
+import java.util.logging.Logger
+
+/**
+ * 负责提取并保存歌词
+ */
+@HiltWorker
+class SaveLyricWorker @AssistedInject constructor(
+    @Assisted context: Context,
+    @Assisted workerParams: WorkerParameters,
+    private val repository: ScannerRepository
+) : CoroutineWorker(context, workerParams) {
+    private val mExecutor = ThreadPoolUtils.CachedThreadPool
+
+    override suspend fun doWork(): Result = withContext(mExecutor.asCoroutineDispatcher()) {
+        try {
+            Logger.getLogger("org.jaudiotagger").level = Level.OFF
+            val songId = inputData.getLong("songId", -1L)
+            val songSize = inputData.getLong("songSize", -1L)
+            val songData = inputData.getString("songData")
+
+            if (songData == null) Result.failure()
+
+            val audioTag = AudioFileIO.read(File(songData!!)).tag
+            val lyric = audioTag.getFields(FieldKey.LYRICS).run {
+                if (isNotEmpty()) get(0).toString() else ""
+            }
+            repository.saveSongDetail(
+                MSongDetail(songId, lyric, songSize, songData)
+            )
+            Result.success()
+        } catch (e: Exception) {
+            Result.failure()
+        }
+    }
+}
