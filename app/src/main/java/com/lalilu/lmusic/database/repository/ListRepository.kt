0b830e197779bec65e2d1d006c28bf563d2771cb
lalilu <1248393538@qq.com>
2022-01-17 10:27:49 +0800

    完善播放列表切换的功能

--- app/src/main/java/com/lalilu/lmusic/database/repository/ListRepository.kt
+++ app/src/main/java/com/lalilu/lmusic/database/repository/ListRepository.kt
@@ -2,9 +2,11 @@ package com.lalilu.lmusic.database.repository
 
 import com.lalilu.lmusic.database.dao.MAlbumDao
 import com.lalilu.lmusic.database.dao.MPlaylistDao
+import com.lalilu.lmusic.database.dao.MSongDao
 import javax.inject.Inject
 
 class ListRepository @Inject constructor(
     val playlistDao: MPlaylistDao,
-    val albumDao: MAlbumDao
+    val albumDao: MAlbumDao,
+    val songDao: MSongDao
 )
