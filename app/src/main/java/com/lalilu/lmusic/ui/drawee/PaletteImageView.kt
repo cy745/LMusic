3e736985ec48cb0082aca72d664d98cd17060590
lalilu <1248393538@qq.com>
2022-01-27 14:59:28 +0800

    优化代码

--- app/src/main/java/com/lalilu/lmusic/ui/drawee/PaletteImageView.kt
+++ app/src/main/java/com/lalilu/lmusic/ui/drawee/PaletteImageView.kt
@@ -7,7 +7,6 @@ import android.util.AttributeSet
 import androidx.appcompat.widget.AppCompatImageView
 import androidx.lifecycle.MutableLiveData
 import androidx.palette.graphics.Palette
-import coil.clear
 import coil.imageLoader
 import coil.request.ImageRequest
 import com.commit451.nativestackblur.NativeStackBlur
@@ -19,6 +18,9 @@ import kotlinx.coroutines.withContext
 import kotlin.coroutines.CoroutineContext
 import kotlin.math.roundToInt
 
+/**
+ * 附带有Palette的ImageView
+ */
 class PaletteImageView @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null
 ) : AppCompatImageView(context, attrs), CoroutineScope {
@@ -28,16 +30,12 @@ class PaletteImageView @JvmOverloads constructor(
     private var samplingValue: Int = 400
 
     private var sourceBitmap: Bitmap? = null
-    private var blurBitmap: Bitmap? = null
     private var samplingBitmap: Bitmap? = null
+    private var blurBitmap: Bitmap? = null
+
     private var sourceRect = Rect()
     private var samplingRect = Rect()
     private var destRect = RectF()
-
-    fun blurBg(percent: Float) = launch(Dispatchers.IO) {
-        blurRadius = (percent * 50).roundToInt()
-        updateBg(blurRadius)
-    }
 
     override fun onDraw(canvas: Canvas) {
         destRect.set(0f, 0f, width.toFloat(), height.toFloat())
@@ -51,50 +49,69 @@ class PaletteImageView @JvmOverloads constructor(
         }
     }
 
-    private suspend fun updateBg(radius: Int) =
-        withContext(Dispatchers.IO) {
-            blurBitmap = if (radius == 0 || samplingBitmap == null) null
-            else NativeStackBlur.process(samplingBitmap, radius)
+    fun blurBg(percent: Float) = launch(Dispatchers.IO) {
+        blurRadius = (percent * 50).roundToInt()
+        updateBg(blurRadius)
+    }
 
+    private suspend inline fun updateBg(radius: Int) =
+        withContext(Dispatchers.IO) {
+            blurBitmap = createBlurBitmap(samplingBitmap, radius)
             withContext(Dispatchers.Main) {
                 invalidate()
             }
         }
 
+    private suspend inline fun updatePalette(source: Bitmap?) =
+        withContext(Dispatchers.IO) {
+            source ?: return@withContext
+            palette.postValue(Palette.from(source).generate())
+        }
+
+    private suspend inline fun createBlurBitmap(source: Bitmap?, radius: Int): Bitmap? =
+        withContext(Dispatchers.IO) {
+            if (source == null || radius == 0) return@withContext null
+            return@withContext NativeStackBlur.process(source, radius)
+        }
+
+    private suspend inline fun createSamplingBitmap(source: Bitmap?, samplingValue: Int): Bitmap? =
+        withContext(Dispatchers.IO) {
+            source ?: return@withContext source
+
+            val width = source.width
+            val height = source.height
+            val matrix = Matrix()
+
+            val scaleWidth = samplingValue.toFloat() / width
+            val scaleHeight = samplingValue.toFloat() / height
+            matrix.postScale(scaleWidth, scaleHeight)
+
+            return@withContext Bitmap.createBitmap(
+                source, 0, 0, width, height, matrix, false
+            )
+        }
+
     override fun setImageURI(uri: Uri?) {
         if (uri == null) {
-            launch {
-                samplingBitmap = null
-                updateBg(blurRadius)
-                this@PaletteImageView.clear()
-            }
+            this.sourceBitmap = null
+            this.samplingBitmap = null
+            this.blurBitmap = null
+            invalidate()
             return
         }
 
         context.imageLoader.enqueue(
             ImageRequest.Builder(context)
                 .data(uri)
+                .allowHardware(false)
                 .target {
                     launch(Dispatchers.IO) {
                         sourceBitmap = it.toBitmap()
-
-                        val width = sourceBitmap!!.width
-                        val height = sourceBitmap!!.height
-                        val matrix = Matrix()
-
-                        val scaleWidth = samplingValue.toFloat() / width
-                        val scaleHeight = samplingValue.toFloat() / height
-                        matrix.postScale(scaleWidth, scaleHeight)
-
-                        samplingBitmap = Bitmap.createBitmap(
-                            sourceBitmap!!, 0, 0, width, height, matrix, false
-                        )
+                        samplingBitmap = createSamplingBitmap(sourceBitmap, samplingValue)
                         updateBg(blurRadius)
+                        updatePalette(samplingBitmap)
                     }
                 }.build()
         )
-//        this.load(uri) {
-//            crossfade(500)
-//        }
     }
 }
