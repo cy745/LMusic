92d82c55861fd160825468171afae4864a71b6d4
lalilu <1248393538@qq.com>
2022-01-28 22:38:39 +0800

    优化代码，完善过渡效果

--- app/src/main/java/com/lalilu/lmusic/ui/drawee/PaletteImageView.kt
+++ app/src/main/java/com/lalilu/lmusic/ui/drawee/PaletteImageView.kt
@@ -6,6 +6,8 @@ import android.graphics.*
 import android.net.Uri
 import android.util.AttributeSet
 import android.view.View
+import android.view.animation.AccelerateInterpolator
+import android.view.animation.DecelerateInterpolator
 import androidx.annotation.FloatRange
 import androidx.annotation.IntRange
 import androidx.core.animation.addListener
@@ -32,13 +34,14 @@ import kotlin.math.roundToInt
 class PaletteImageView @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null
 ) : View(context, attrs), CoroutineScope {
+    override val coroutineContext: CoroutineContext = Dispatchers.IO
 
     @Inject
     lateinit var stackBlur: StackBlurUtils
 
-    override val coroutineContext: CoroutineContext = Dispatchers.IO
     var palette: MutableLiveData<Palette> = MutableLiveData(null)
     var maxOffset = 0
+    private var maxBlurRadius = 50
 
     @IntRange(from = 0, to = 2000)
     var samplingValue: Int = 400
@@ -53,11 +56,18 @@ class PaletteImageView @JvmOverloads constructor(
 
     @IntRange(from = 0, to = 50)
     var blurRadius = 0
-        set(value) {
+        internal set(value) {
             field = value
             launch(Dispatchers.IO) {
                 updateBg(blurRadius)
             }
+        }
+
+    @FloatRange(from = 0.0, to = 1.0)
+    var blurPercent: Float = 0f
+        internal set(value) {
+            field = value
+            blurRadius = (value * maxBlurRadius).roundToInt()
         }
 
     @FloatRange(from = 0.0, to = 1.0)
@@ -76,17 +86,6 @@ class PaletteImageView @JvmOverloads constructor(
     private var destRect = RectF()
 
     /**
-     * 计算图片可拖动展开的最大位移距离
-     */
-    private fun calcMaxOffset(source: Bitmap?): Int {
-        source ?: return 0
-
-        return if (source.width / source.height > 0) 0
-        else (width * source.height / source.width - width)
-            .coerceAtLeast(0)
-    }
-
-    /**
      * 按照CenterCrop的规则，获取调整后的dest位置
      *
      * @param source Bitmap的大小
@@ -95,8 +94,7 @@ class PaletteImageView @JvmOverloads constructor(
     private fun centerCrop(
         source: Rect,
         dest: RectF,
-        offsetPercent: Float,
-        scalePercent: Float
+        offsetPercent: Float
     ): RectF {
         val w = source.width().toFloat()
         val h = source.height().toFloat()
@@ -107,13 +105,17 @@ class PaletteImageView @JvmOverloads constructor(
         val offset: Float
 
         if (sourceWHRatio > x / y) {
+            // 横向长图的情况
+            maxOffset = 0
             offset = abs(x - sourceWHRatio * y) / 2
             dest.set(
-                dest.left - offset * (1 - offsetPercent), dest.top,
-                x + offset * (1 + offsetPercent), y
+                dest.left - offset, dest.top,
+                x + offset, y
             )
         } else {
-            offset = abs(y - x / sourceWHRatio) / 2
+            // 竖向长图的情况
+            maxOffset = abs(y - x / sourceWHRatio).toInt()
+            offset = maxOffset / 2f
             dest.set(
                 dest.left, dest.top - offset * (1 - offsetPercent),
                 x, y + offset * (1 + offsetPercent)
@@ -122,11 +124,30 @@ class PaletteImageView @JvmOverloads constructor(
         return dest
     }
 
+    private val dInterpolator = DecelerateInterpolator()
+    private val aInterpolator = AccelerateInterpolator()
+
+    /**
+     * 将图片的位置移动至ImageView的中心，并且
+     */
+    private fun scaleTransform(dest: RectF, scalePercent: Float) {
+        val dValue = dInterpolator.getInterpolation(scalePercent)
+        val aValue = aInterpolator.getInterpolation(scalePercent)
+        val offsetValue = (height.toFloat() - dest.height()) / 2f * dValue
+        val scaleValue = 1 + (height.toFloat() / dest.height() - 1) * aValue
+
+        dest.top += offsetValue
+        dest.bottom += offsetValue
+        scaleX = scaleValue
+        scaleY = scaleValue
+    }
+
     override fun onDraw(canvas: Canvas) {
         destRect.set(0f, 0f, width.toFloat(), width.toFloat())
         sourceBitmap?.let {
             sourceRect.set(0, 0, it.width, it.height)
-            centerCrop(sourceRect, destRect, dragPercent, scalePercent)
+            centerCrop(sourceRect, destRect, dragPercent)
+            scaleTransform(destRect, scalePercent)
             canvas.drawBitmap(it, sourceRect, destRect, bitmapPainter)
         }
         blurBitmap?.let {
@@ -151,11 +172,19 @@ class PaletteImageView @JvmOverloads constructor(
         }
     }
 
+    /**
+     * 调用animator执行fadeIn过渡
+     */
     private suspend fun fadeIn() = withContext(Dispatchers.Main) {
         if (animator.isStarted || animator.isRunning) animator.end()
         animator.start()
     }
 
+    /**
+     * 传入模糊半径更新Blur背景
+     *
+     * @param radius 用于图片Blur的模糊半径参数
+     */
     private suspend inline fun updateBg(radius: Int) =
         withContext(Dispatchers.IO) {
             blurBitmap = createBlurBitmap(samplingBitmap, radius)
@@ -164,18 +193,37 @@ class PaletteImageView @JvmOverloads constructor(
             }
         }
 
+    /**
+     * 更新Palette调色盘
+     *
+     * @param source 源图
+     */
     private suspend inline fun updatePalette(source: Bitmap?) =
         withContext(Dispatchers.IO) {
             source ?: return@withContext
             palette.postValue(Palette.from(source).generate())
         }
 
+    /**
+     * 创建Blur的Bitmap
+     *
+     * @param source 源图
+     * @param radius Blur所需的模糊半径参数
+     * @return 经过模糊化的Bitmap
+     */
     private suspend inline fun createBlurBitmap(source: Bitmap?, radius: Int): Bitmap? =
         withContext(Dispatchers.IO) {
             if (radius == 0) return@withContext null
             return@withContext stackBlur.processWithCache(source, radius)
         }
 
+    /**
+     * 重采样图片，降低图片大小，用于Blur
+     *
+     * @param source 源图
+     * @param samplingValue 输出图片的最大边大小
+     * @return 经过重采样的Bitmap
+     */
     private suspend inline fun createSamplingBitmap(source: Bitmap?, samplingValue: Int): Bitmap? =
         withContext(Dispatchers.IO) {
             source ?: return@withContext source
@@ -193,6 +241,12 @@ class PaletteImageView @JvmOverloads constructor(
             )
         }
 
+    /**
+     * 加载指定uri的图片
+     *
+     * @param uri 图片地址（可为本地亦或是网络图片）
+     *
+     */
     fun setImageURI(uri: Uri?) {
         if (uri == null) {
             this.sourceBitmap = null
@@ -210,7 +264,6 @@ class PaletteImageView @JvmOverloads constructor(
                     launch(Dispatchers.IO) {
                         fadeIn()
                         sourceBitmap = it.toBitmap()
-                        maxOffset = calcMaxOffset(sourceBitmap)
                         samplingBitmap = createSamplingBitmap(sourceBitmap, samplingValue)
                         updatePalette(samplingBitmap)
                         updateBg(blurRadius)
