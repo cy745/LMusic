37676a75e5f398c686f667e274928bcb7f8716ad
lalilu <1248393538@qq.com>
2022-01-29 23:29:36 +0800

    抽离控制滑动拦截的逻辑

--- app/src/main/java/com/lalilu/lmusic/ui/MyCoordinatorLayout.kt
+++ app/src/main/java/com/lalilu/lmusic/ui/MyCoordinatorLayout.kt
@@ -5,6 +5,7 @@ import android.util.AttributeSet
 import android.view.MotionEvent
 import androidx.coordinatorlayout.widget.CoordinatorLayout
 import com.lalilu.lmusic.utils.AntiSlideConflict
+import kotlin.math.abs
 
 /**
  *  可处理滑动冲突的 CoordinatorLayout
@@ -12,7 +13,10 @@ import com.lalilu.lmusic.utils.AntiSlideConflict
 class MyCoordinatorLayout @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : CoordinatorLayout(context, attrs, defStyleAttr) {
-    private val antiErrorSlide = AntiSlideConflict()
+    private val antiErrorSlide = AntiSlideConflict(
+        whenToSolve = { disX, disY -> abs(disX) != abs(disY) },
+        isInterceptNow = { disX, disY -> abs(disX) < abs(disY) || disX > 0 }
+    )
 
     override fun dispatchTouchEvent(ev: MotionEvent): Boolean {
         antiErrorSlide.dispatchTouchEvent(ev, parent)
