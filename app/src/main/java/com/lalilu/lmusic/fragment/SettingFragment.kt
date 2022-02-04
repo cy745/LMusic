f2dcf1a0a117095db272170095679164be3f69a2
lalilu <1248393538@qq.com>
2022-02-04 22:50:57 +0800

    完善dialog的自定义布局，尝试结合BottomSheetDialog和PreferenceFragment

---
+++ app/src/main/java/com/lalilu/lmusic/fragment/SettingFragment.kt
@@ -0,0 +1,11 @@
+package com.lalilu.lmusic.fragment
+
+import android.os.Bundle
+import androidx.preference.PreferenceFragmentCompat
+import com.lalilu.R
+
+class SettingFragment : PreferenceFragmentCompat() {
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.settings_preference, null)
+    }
+}
