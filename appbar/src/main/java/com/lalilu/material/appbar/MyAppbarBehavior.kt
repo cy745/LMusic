230a3f15375a0cf7ff9a7ab9e9930d00cda10a2e
lalilu <1248393538@qq.com>
2022-02-10 17:07:22 +0800

    开始重构Behavior

---
+++ appbar/src/main/java/com/lalilu/material/appbar/MyAppbarBehavior.kt
@@ -0,0 +1,11 @@
+package com.lalilu.material.appbar
+
+import android.content.Context
+import android.util.AttributeSet
+
+class MyAppbarBehavior(
+    context: Context?, attrs: AttributeSet?
+) : ExpendHeaderBehavior<AppBarLayout>(context, attrs) {
+
+
+}
