48a2c191992a70d3faeaf01e7c4465ce3ca837c8
lalilu <1248393538@qq.com>
2022-02-21 14:39:59 +0800

    暂时去除Room

--- app/src/main/java/com/lalilu/lmusic/domain/entity/MArtist.kt
+++ app/src/main/java/com/lalilu/lmusic/domain/entity/MArtist.kt
@@ -1,12 +1,5 @@
 package com.lalilu.lmusic.domain.entity
 
-import androidx.room.ColumnInfo
-import androidx.room.Entity
-import androidx.room.PrimaryKey
-
-@Entity(tableName = "m_artist")
 data class MArtist(
-    @PrimaryKey
-    @ColumnInfo(name = "artist_name")
     val artistName: String
 )
--- app/src/main/java/com/lalilu/lmusic/domain/entity/MPlaylist.kt
+++ app/src/main/java/com/lalilu/lmusic/domain/entity/MPlaylist.kt
@@ -1,23 +1,14 @@
 package com.lalilu.lmusic.domain.entity
 
 import android.net.Uri
-import androidx.room.ColumnInfo
-import androidx.room.Entity
-import androidx.room.PrimaryKey
 
-@Entity(tableName = "m_playlist")
 data class MPlaylist(
 
-    @PrimaryKey
-    @ColumnInfo(name = "playlist_id")
     val playlistId: Long,
 
-    @ColumnInfo(name = "playlist_title")
     val playlistTitle: String = "New Playlist.",
 
-    @ColumnInfo(name = "playlist_cover_uri")
     var playlistCoverUri: Uri = Uri.EMPTY,
 
-    @ColumnInfo(name = "playlist_info")
     val playlistInfo: String = "empty.",
 )
