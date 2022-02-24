960edfb116ac62eba279bc59d34cfbb4c2a74310
lalilu <1248393538@qq.com>
2022-02-25 01:35:49 +0800

    初步完善popupWindow的创建和回调逻辑

--- app/src/main/java/com/lalilu/lmusic/fragment/SongDetailFragment.kt
+++ app/src/main/java/com/lalilu/lmusic/fragment/SongDetailFragment.kt
@@ -3,18 +3,24 @@ package com.lalilu.lmusic.fragment
 import androidx.navigation.fragment.navArgs
 import com.lalilu.BR
 import com.lalilu.R
+import com.lalilu.databinding.FragmentSongDetailBinding
 import com.lalilu.lmusic.base.DataBindingConfig
 import com.lalilu.lmusic.base.DataBindingFragment
 import com.lalilu.lmusic.datasource.BaseMediaSource
+import com.lalilu.lmusic.datasource.LMusicDataBase
 import com.lalilu.lmusic.fragment.viewmodel.SongDetailViewModel
+import com.lalilu.lmusic.ui.MyPopupWindow
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.*
 import javax.inject.Inject
+import kotlin.coroutines.CoroutineContext
+
 
 @AndroidEntryPoint
 @ExperimentalCoroutinesApi
-class SongDetailFragment : DataBindingFragment() {
+class SongDetailFragment : DataBindingFragment(), CoroutineScope {
     private val args: SongDetailFragmentArgs by navArgs()
+    override val coroutineContext: CoroutineContext = Dispatchers.IO
 
     @Inject
     lateinit var mState: SongDetailViewModel
@@ -22,14 +28,32 @@ class SongDetailFragment : DataBindingFragment() {
     @Inject
     lateinit var mediaSource: BaseMediaSource
 
+    @Inject
+    lateinit var dataBase: LMusicDataBase
+
     override fun getDataBindingConfig(): DataBindingConfig {
         return DataBindingConfig(R.layout.fragment_song_detail)
             .addParam(BR.vm, mState)
     }
 
     override fun onViewCreated() {
-        mState._song.postValue(
-            mediaSource.getSongById(args.songId)
-        )
+        val binding = mBinding as FragmentSongDetailBinding
+        launch(Dispatchers.IO) {
+            mState._song.postValue(
+                mediaSource.getSongById(args.songId)
+            )
+        }
+        binding.songDetailAddSongToPlaylistButton.setOnClickListener {
+            launch(Dispatchers.IO) {
+                val list = dataBase.playlistDao().getAll().toMutableList()
+                withContext(Dispatchers.Main) {
+                    MyPopupWindow(requireActivity()) {
+                        println("checked: ${it.size}")
+                    }.apply {
+                        setData(list)
+                    }.showAsDropDown(binding.root)
+                }
+            }
+        }
     }
 }
