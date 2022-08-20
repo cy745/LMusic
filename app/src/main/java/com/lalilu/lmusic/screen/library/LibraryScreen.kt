bf83667e4b25cc99b4b5d3cff7e321525b54cbde
qiu745 <1248393538@qq.com>
2022-08-20 11:01:22 +0800

    初步实现Smartbar的部分逻辑

--- app/src/main/java/com/lalilu/lmusic/screen/library/LibraryScreen.kt
+++ app/src/main/java/com/lalilu/lmusic/screen/library/LibraryScreen.kt
@@ -1,28 +1,48 @@
 package com.lalilu.lmusic.screen.library
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Icon
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.layout.FixedScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import androidx.navigation.compose.currentBackStackEntryAsState
 import com.lalilu.R
 import com.lalilu.lmedia.indexer.Indexer
+import com.lalilu.lmusic.screen.MainScreenData
 import com.lalilu.lmusic.screen.component.SmartBar
+import com.lalilu.lmusic.screen.navigate
+import com.lalilu.lmusic.utils.LocalNavigatorHost
 
 @Composable
 fun LibraryScreen(
     contentPaddingForFooter: Dp = 0.dp
 ) {
+    LaunchedEffect(Unit) {
+        SmartBar.setBarItem {
+            NavigateBar()
+        }
+    }
     LazyColumn(
         modifier = Modifier
             .fillMaxSize(),
@@ -123,26 +143,99 @@ fun LibraryScreen(
                 )
             }
         }
+    }
+}
 
+@Composable
+fun NavigateBar() {
+    val navController = LocalNavigatorHost.current
+    val currentBackStack by navController.currentBackStackEntryAsState()
+    val currentRoute = currentBackStack?.destination?.route
+    val isLibraryScreen = currentRoute?.contains(MainScreenData.Library.name) ?: false
 
-//        LazyVerticalGrid(
-//            columns = GridCells.Fixed(if (currentWindowSize == WindowSize.Expanded) 3 else 2),
-//            contentPadding = PaddingValues(20.dp),
-//            modifier = Modifier.fillMaxSize(),
-//            verticalArrangement = Arrangement.spacedBy(20.dp),
-//            horizontalArrangement = Arrangement.spacedBy(10.dp)
-//        ) {
-//            MainScreenData.values().forEach {
-//                if (it.showNavigateButton) {
-//                    item {
-//                        DestinationCard(
-//                            route = it,
-//                            navigateTo = navigateTo
-//                        )
-//                    }
-//                }
-//            }
-//        }
+    LazyRow(
+        modifier = Modifier
+            .height(52.dp)
+            .fillMaxWidth()
+            .padding(horizontal = 20.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        MainScreenData.values().forEach {
+            if (it.showNavigateButton) {
+                item {
+                    NavigateItem(
+                        navController = navController,
+                        currentRoute = currentRoute,
+                        routeData = it
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun NavigateItem(
+    navController: NavController,
+    routeData: MainScreenData,
+    currentRoute: String?,
+    baseColor: Color = MaterialTheme.colors.primary,
+) {
+    val icon = painterResource(id = routeData.icon)
+    val title = stringResource(id = routeData.title)
+    val selected = currentRoute?.contains(routeData.name) ?: false
+    val imageAlpha = if (selected) 1f else 0.6f
+    val iconTintColor = if (selected) baseColor else {
+        MaterialTheme.colors.onSurface.copy(alpha = 0.6f)
+    }
+    val backgroundColor by animateColorAsState(
+        if (selected) baseColor.copy(alpha = 0.12f) else Color.Transparent
+    )
+
+    @OptIn(ExperimentalMaterialApi::class)
+    Surface(
+        color = backgroundColor,
+        onClick = {
+            navController.navigate(
+                from = MainScreenData.Library.name,
+                to = routeData.name
+            )
+        },
+        shape = CircleShape,
+        modifier = Modifier
+            .fillMaxHeight()
+            .aspectRatio(1f)
+    ) {
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .animateContentSize(),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Image(
+                    painter = icon,
+                    contentDescription = title,
+                    colorFilter = ColorFilter.tint(iconTintColor),
+                    alpha = imageAlpha,
+                    contentScale = FixedScale(if (selected) 1.1f else 1f)
+                )
+                AnimatedVisibility(visible = selected) {
+                    Text(
+                        text = title,
+                        modifier = Modifier.fillMaxWidth(),
+                        textAlign = TextAlign.Center,
+                        fontWeight = FontWeight.Medium,
+                        fontSize = 12.sp,
+                        letterSpacing = 0.1.sp
+                    )
+                }
+            }
+        }
     }
 }
 
