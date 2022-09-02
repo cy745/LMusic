239375e85bea5fd26c8189ebc85a41fa235d1623
lalilu <1248393538@qq.com>
2022-09-03 01:39:23 +0800

    优化Notification逻辑

--- app/src/main/java/com/lalilu/lmusic/service/LMusicNotification.kt
+++ app/src/main/java/com/lalilu/lmusic/service/LMusicNotification.kt
@@ -3,25 +3,27 @@ package com.lalilu.lmusic.service
 import android.app.*
 import android.content.Context
 import android.content.Intent
-import android.graphics.drawable.BitmapDrawable
+import android.graphics.Bitmap
+import android.graphics.Color
 import android.os.Build
 import android.support.v4.media.session.MediaSessionCompat
 import android.support.v4.media.session.PlaybackStateCompat
 import androidx.annotation.RequiresApi
 import androidx.core.app.NotificationCompat
 import androidx.core.content.ContextCompat
+import androidx.media.app.NotificationCompat.MediaStyle
 import androidx.media.session.MediaButtonReceiver
 import androidx.palette.graphics.Palette
 import coil.imageLoader
 import coil.request.ImageRequest
 import com.lalilu.R
 import com.lalilu.common.getAutomaticColor
+import com.lalilu.lmedia.entity.LSong
+import com.lalilu.lmusic.datasource.MDataBase
+import com.lalilu.lmusic.utils.extension.toBitmap
 import dagger.hilt.android.qualifiers.ApplicationContext
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.collectLatest
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 import javax.inject.Inject
 import javax.inject.Singleton
 import kotlin.coroutines.CoroutineContext
@@ -31,7 +33,8 @@ import kotlin.coroutines.CoroutineContext
  */
 @Singleton
 class LMusicNotification @Inject constructor(
-    @ApplicationContext private val mContext: Context
+    @ApplicationContext private val mContext: Context,
+    private val database: MDataBase
 ) : CoroutineScope {
     override val coroutineContext: CoroutineContext = Dispatchers.Default
 
@@ -123,9 +126,12 @@ class LMusicNotification @Inject constructor(
     ) as NotificationManager
 
     private var currentIsStop: Boolean = true
-    private var lastNotification: Notification? = null
     private var currentLyricTemp: String? = null
 
+    @Volatile
+    private var lastNotificationBuilder: NotificationCompat.Builder? = null
+    private var lastBitmap: Pair<LSong, Bitmap>? = null
+    private var lastColor: Pair<LSong, Int>? = null
 
     /**
      *  API 26 以上需要注册Channel，否则不显示通知。
@@ -137,16 +143,16 @@ class LMusicNotification @Inject constructor(
         notificationManager.cancelAll()
         launch {
             LMusicLyricManager.currentSentence.collectLatest {
-                updateLyric(it)
                 currentLyricTemp = it
+                updateLyric(it)
             }
         }
     }
 
     suspend fun updateNotification(
+        data: Any?,
+        service: Service,
         mediaSession: MediaSessionCompat,
-        data: Any? = null,
-        service: Service? = null
     ) {
         val controller = mediaSession.controller
         val token = mediaSession.sessionToken
@@ -156,7 +162,7 @@ class LMusicNotification @Inject constructor(
         val repeatMode = mediaSession.controller.repeatMode
         val isPlaying = state.state == PlaybackStateCompat.STATE_PLAYING
 
-        val style = androidx.media.app.NotificationCompat.MediaStyle()
+        val style = MediaStyle()
             .setMediaSession(token)
             .setShowActionsInCompactView(0, 1, 2)
             .setShowCancelButton(true)
@@ -164,14 +170,15 @@ class LMusicNotification @Inject constructor(
 
         val builder = NotificationCompat.Builder(mContext, playerChannelName + "_ID")
         builder.setStyle(style)
-            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setSmallIcon(R.mipmap.ic_launcher)
+            .setDeleteIntent(mStopAction.actionIntent)
             .setContentIntent(controller.sessionActivity)
             .setContentTitle(description.title)
             .setContentText(description.subtitle)
             .setSubText(description.description)
             .setShowWhen(false)
             .setAutoCancel(false)
-            .setOngoing(isPlaying)
+            .setOngoing(false)
             .setOnlyAlertOnce(true)
             .setCategory(NotificationCompat.CATEGORY_TRANSPORT)
             .setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
@@ -190,42 +197,67 @@ class LMusicNotification @Inject constructor(
         builder.addAction(mStopAction)
 
         withContext(Dispatchers.IO) {
-            mContext.imageLoader.execute(
-                ImageRequest.Builder(mContext)
-                    .allowHardware(false)
-                    .data(data)
-                    .build()
-            ).drawable.let { drawable ->
-                drawable?.let { it as BitmapDrawable }?.bitmap?.let {
-                    val palette = Palette.from(it).generate()
-                    builder.color = palette.getAutomaticColor()
-                    builder.setLargeIcon(it)
-                }
-                lastNotification = builder.build().apply {
-                    tickerText = currentLyricTemp
-                    flags = flags.or(FLAG_ALWAYS_SHOW_TICKER)
-                    flags = flags.or(FLAG_ONLY_UPDATE_TICKER)
-                }
-                if (service != null) {
-                    println("startForeground: notify")
-                    service.startForeground(NOTIFICATION_PLAYER_ID, lastNotification!!)
-                    currentIsStop = false
-                    return@withContext
-                }
-                println("notify")
-                notificationManager.notify(NOTIFICATION_PLAYER_ID, lastNotification)
-                currentIsStop = false
+            if (data is LSong) builder.loadCoverAndPalette(data)
+
+            lastNotificationBuilder = builder
+            pushNotification(isPlaying, service, builder.build())
+            currentIsStop = false
+        }
+    }
+
+    private fun NotificationCompat.Builder.loadCoverAndPalette(data: LSong) {
+        var bitmap: Bitmap? = null
+        var color: Int = Color.TRANSPARENT
+        lastBitmap?.takeIf { it.first == data }
+            ?.let { bitmap = it.second }
+        lastColor?.takeIf { it.first == data }
+            ?.let { color = it.second }
+
+        if (bitmap == null) {
+            val coverData = database.networkDataDao().getById(data.id)
+                ?.requireCoverUri()
+                ?: data
+
+            runBlocking {
+                bitmap = this@LMusicNotification.mContext.imageLoader.execute(
+                    ImageRequest.Builder(this@LMusicNotification.mContext)
+                        .allowHardware(false)
+                        .data(coverData)
+                        .size(400)
+                        .build()
+                ).drawable?.toBitmap() ?: return@runBlocking
             }
+
+            if (bitmap != null) {
+                color = Palette.from(bitmap!!)
+                    .generate()
+                    .getAutomaticColor()
+
+                lastBitmap = data to bitmap!!
+                lastColor = data to color
+            }
+        }
+
+        if (bitmap != null) {
+            this@loadCoverAndPalette.setLargeIcon(bitmap)
+            this@loadCoverAndPalette.color = color
+        }
+    }
+
+    private fun pushNotification(isPlaying: Boolean, service: Service, notification: Notification) {
+        if (isPlaying) {
+            service.startForeground(NOTIFICATION_PLAYER_ID, notification)
+        } else {
+            notificationManager.notify(NOTIFICATION_PLAYER_ID, notification)
         }
     }
 
     private fun updateLyric(text: String?) {
-        println("updateLyric: $text")
         if (currentIsStop) return
-        lastNotification?.apply {
+        println("updateLyric: $text")
+        lastNotificationBuilder?.build()?.apply {
             tickerText = text
             flags = flags.or(FLAG_ALWAYS_SHOW_TICKER)
-            flags = flags.or(FLAG_ONLY_UPDATE_TICKER)
             notificationManager.notify(NOTIFICATION_PLAYER_ID, this)
         }
     }
