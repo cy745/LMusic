acd9bf577439a8522c52bf21c6a146e3f8a290dd
lalilu <1248393538@qq.com>
2022-11-14 12:16:22 +0800

    优化SmartBar、SmartContainer

--- app/src/main/java/com/lalilu/lmusic/compose/component/SmartBar.kt
+++ app/src/main/java/com/lalilu/lmusic/compose/component/SmartBar.kt
@@ -3,31 +3,36 @@ package com.lalilu.lmusic.compose.component
 import androidx.compose.animation.AnimatedContent
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.ExperimentalAnimationApi
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.BoxScope
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.imePadding
+import androidx.compose.foundation.layout.isImeVisible
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.asLiveData
 import com.lalilu.lmusic.utils.extension.measure
 import com.lalilu.lmusic.utils.recomposeHighlighter
-import kotlinx.coroutines.FlowPreview
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.debounce
 
-@OptIn(FlowPreview::class)
 object SmartBar {
     private val mainBar: MutableState<(@Composable () -> Unit)?> = mutableStateOf(null)
     private val extraBar: MutableState<(@Composable () -> Unit)?> = mutableStateOf(null)
 
-    private val statusBarHeightDp = MutableStateFlow(0.dp)
     val smartBarHeightDpState = mutableStateOf(0.dp)
-    val statusBarHeightDpLiveData = statusBarHeightDp
-        .debounce(50L)
-        .asLiveData()
 
     @Composable
     @OptIn(ExperimentalAnimationApi::class, ExperimentalLayoutApi::class)
@@ -35,13 +40,8 @@ object SmartBar {
         modifier: Modifier = Modifier
     ) {
         val density = LocalDensity.current
-        val statusBarHeight = WindowInsets.statusBars.asPaddingValues().calculateTopPadding()
         val hasContent by remember(mainBar, extraBar) {
             derivedStateOf { mainBar.value != null || extraBar.value != null }
-        }
-
-        LaunchedEffect(statusBarHeight) {
-            statusBarHeightDp.emit(statusBarHeight)
         }
 
         Surface(
