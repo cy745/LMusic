18c2812a725f4efaa8ec5e070615b627c1bd0a6e
lalilu <1248393538@qq.com>
2022-11-22 23:54:29 +0800

    优化SmartBar的显示逻辑

--- app/src/main/java/com/lalilu/lmusic/compose/component/SmartBar.kt
+++ app/src/main/java/com/lalilu/lmusic/compose/component/SmartBar.kt
@@ -3,24 +3,10 @@ package com.lalilu.lmusic.compose.component
 import androidx.compose.animation.AnimatedContent
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.ExperimentalAnimationApi
-import androidx.compose.foundation.layout.BoxScope
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.ExperimentalLayoutApi
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.WindowInsets
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.imePadding
-import androidx.compose.foundation.layout.isImeVisible
-import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.derivedStateOf
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalDensity
@@ -36,9 +22,7 @@ object SmartBar {
 
     @Composable
     @OptIn(ExperimentalAnimationApi::class, ExperimentalLayoutApi::class)
-    fun BoxScope.SmartBarContent(
-        modifier: Modifier = Modifier
-    ) {
+    fun BoxScope.SmartBarContent(modifier: Modifier = Modifier) {
         val density = LocalDensity.current
         val hasContent by remember(mainBar, extraBar) {
             derivedStateOf { mainBar.value != null || extraBar.value != null }
@@ -48,7 +32,6 @@ object SmartBar {
             modifier = modifier
                 .recomposeHighlighter()
                 .align(Alignment.BottomCenter)
-                .imePadding()
                 .fillMaxWidth(),
             color = MaterialTheme.colors.background.copy(alpha = 0.95f)
         ) {
@@ -71,11 +54,15 @@ object SmartBar {
                 AnimatedContent(targetState = WindowInsets.isImeVisible to mainBar.value) { pair ->
                     pair.second.takeIf { !pair.first }?.invoke()
                 }
-                AnimatedVisibility(visible = mainBar.value != null) {
+                AnimatedVisibility(visible = mainBar.value != null && !WindowInsets.isImeVisible) {
                     Spacer(modifier = Modifier.height(10.dp))
                 }
                 AnimatedVisibility(visible = hasContent) {
-                    Spacer(modifier = Modifier.navigationBarsPadding())
+                    Spacer(
+                        modifier = Modifier
+                            .navigationBarsPadding()
+                            .imePadding()
+                    )
                 }
             }
         }
