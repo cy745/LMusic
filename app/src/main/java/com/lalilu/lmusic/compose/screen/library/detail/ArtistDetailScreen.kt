d26a6d850be644c0b253d710d00b1e5286263496
lalilu <1248393538@qq.com>
2023-01-24 22:39:15 +0800

    复用SongsScreen组件，简化AlbumDetail和ArtistDetail页面的结构

--- app/src/main/java/com/lalilu/lmusic/compose/screen/library/detail/ArtistDetailScreen.kt
+++ app/src/main/java/com/lalilu/lmusic/compose/screen/library/detail/ArtistDetailScreen.kt
@@ -1,22 +1,28 @@
 package com.lalilu.lmusic.compose.screen.library.detail
 
 import androidx.compose.animation.ExperimentalAnimationApi
-import androidx.compose.foundation.lazy.grid.itemsIndexed
-import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.hapticfeedback.HapticFeedbackType
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.navArgument
 import com.google.accompanist.navigation.animation.composable
 import com.lalilu.lmedia.entity.LArtist
-import com.lalilu.lmedia.entity.LSong
 import com.lalilu.lmedia.indexer.Library
-import com.lalilu.lmusic.compose.component.SmartContainer
-import com.lalilu.lmusic.compose.component.card.SongCard
+import com.lalilu.lmusic.compose.component.navigate.NavigatorHeader
 import com.lalilu.lmusic.compose.screen.BaseScreen
 import com.lalilu.lmusic.compose.screen.ScreenData
-import com.lalilu.lmusic.viewmodel.LocalPlayingVM
-import com.lalilu.lmusic.viewmodel.PlayingViewModel
+import com.lalilu.lmusic.compose.screen.library.SongsScreen
+import com.lalilu.lmusic.utils.extension.dayNightTextColor
+import com.lalilu.lmusic.viewmodel.LocalSongsVM
+import com.lalilu.lmusic.viewmodel.SongsViewModel
 
 @OptIn(ExperimentalAnimationApi::class)
 object ArtistDetailScreen : BaseScreen() {
@@ -42,54 +48,40 @@ object ArtistDetailScreen : BaseScreen() {
     }
 }
 
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
 private fun ArtistDetailScreen(
     artist: LArtist,
-    playingVM: PlayingViewModel = LocalPlayingVM.current
+    songsVM: SongsViewModel = LocalSongsVM.current
 ) {
-    val songs = artist.songs
-    val navToSongAction = SongDetailScreen.navToByArgv(hapticType = HapticFeedbackType.LongPress)
-
-    val onSongSelected: (LSong) -> Unit = { song ->
-        playingVM.playSongWithPlaylist(songs, song)
+    LaunchedEffect(artist) {
+        songsVM.updateBySongs(artist.songs)
     }
 
-    SmartContainer.LazyVerticalGrid(
-        columns = { if (it == WindowWidthSizeClass.Expanded) 2 else 1 },
-    ) {
-//        item {
-//            NavigatorHeaderWithButtons(
-//                title = artist.name,
-//                subTitle = "${artist.getSongCount()} 首歌曲"
-//            ) {
-//                LazyListSortToggleButton(sortByState = sortByState) {
-//                    sortByState = next(sortByState)
-//                }
-//                SortToggleButton(sortDesc = sortDesc) {
-//                    sortDesc = !sortDesc
-//                }
-//            }
-//        }
-
-        itemsIndexed(items = songs) { index, item ->
-            SongCard(
-                song = { item },
-                lyricRepository = playingVM.lyricRepository,
-                onClick = { onSongSelected(item) },
-                onLongClick = { navToSongAction(item.id) }
-            )
-//            SongCard(
-//                modifier = Modifier.animateItemPlacement(),
-//                index = index,
-//                getSong = { item },
-//                onItemClick = onSongSelected,
-//                onItemLongClick = { navToSongAction(it.id) }
-//            )
+    SongsScreen(showAll = false) { songs, showSortBar ->
+        item {
+            NavigatorHeader(
+                title = artist.name,
+                subTitle = "共 ${songs.size} 首歌曲"
+            ) {
+                Surface(
+                    shape = RoundedCornerShape(50.dp),
+                    color = dayNightTextColor(0.05f),
+                    onClick = showSortBar
+                ) {
+                    Text(
+                        modifier = Modifier.padding(horizontal = 15.dp, vertical = 5.dp),
+                        style = MaterialTheme.typography.subtitle2,
+                        color = dayNightTextColor(0.7f),
+                        text = "排序"
+                    )
+                }
+            }
         }
     }
 }
 
 @Composable
 fun EmptyArtistDetailScreen() {
-
+    Text(text = "无法获取该歌手信息", modifier = Modifier.padding(20.dp))
 }
