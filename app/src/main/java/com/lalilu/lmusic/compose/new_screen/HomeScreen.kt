2dd701383909f9b95b7b4c3cc4cc8f854d9bff4d
lalilu <1248393538@qq.com>
2023-02-24 14:08:06 +0800

    新建页面

---
+++ app/src/main/java/com/lalilu/lmusic/compose/new_screen/HomeScreen.kt
@@ -0,0 +1,14 @@
+package com.lalilu.lmusic.compose.new_screen
+
+import androidx.compose.runtime.Composable
+import com.ramcosta.composedestinations.annotation.Destination
+import com.ramcosta.composedestinations.navigation.DestinationsNavigator
+
+@HomeNavGraph(start = true)
+@Destination
+@Composable
+fun HomeScreen(
+    navigator: DestinationsNavigator
+) {
+
+}
---
+++ app/src/main/java/com/lalilu/lmusic/compose/new_screen/PlaylistsScreen.kt
@@ -0,0 +1,11 @@
+package com.lalilu.lmusic.compose.new_screen
+
+import androidx.compose.runtime.Composable
+import com.ramcosta.composedestinations.annotation.Destination
+
+@PlaylistNavGraph(start = true)
+@Destination
+@Composable
+fun PlaylistsScreen() {
+
+}
