a7e0e21512e7fe656f88fa35d4ddf54979c58c33
lalilu <1248393538@qq.com>
2023-07-20 09:56:58 +0800

    Compose版本升级，Kotlin升级

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     id("com.android.application").version("8.1.0-beta03").apply(false)
-    id("org.jetbrains.kotlin.android").version("1.8.10").apply(false)
-    id("com.google.devtools.ksp").version("1.8.10-1.0.9").apply(false)
+    id("org.jetbrains.kotlin.android").version("1.9.0").apply(false)
+    id("com.google.devtools.ksp").version("1.9.0-1.0.11").apply(false)
 }
