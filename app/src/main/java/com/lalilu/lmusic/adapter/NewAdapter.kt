96ec9548f6d848b8cb97058e77569b345ebed04c
lalilu <1248393538@qq.com>
2023-07-26 10:55:32 +0800

    解决长按回调触发后仍可滑动RecyclerView的问题

--- app/src/main/java/com/lalilu/lmusic/adapter/NewAdapter.kt
+++ app/src/main/java/com/lalilu/lmusic/adapter/NewAdapter.kt
@@ -144,6 +144,7 @@ class NewPlayingAdapter private constructor(
     }
 
     override fun onLongClick(v: View?): Boolean {
+        v?.parent?.requestDisallowInterceptTouchEvent(true)
         onLongClickCallback?.onLongClick(v?.tag.toString())
         return true
     }
