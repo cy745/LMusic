008524564ec8899dffb67552114cb6ad2c1d4292
lalilu <1248393538@qq.com>
2023-09-20 19:02:58 +0800

    [modify]暂时去除无用代码

--- app/src/main/java/com/lalilu/lmusic/MainActivity.kt
+++ app/src/main/java/com/lalilu/lmusic/MainActivity.kt
@@ -1,7 +1,5 @@
 package com.lalilu.lmusic
 
-import android.annotation.SuppressLint
-import android.content.Intent
 import android.content.pm.PackageManager
 import android.media.AudioManager
 import android.os.Bundle
@@ -16,62 +14,20 @@ import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_YES
 import androidx.core.app.ActivityCompat
 import androidx.lifecycle.lifecycleScope
 import com.blankj.utilcode.util.ActivityUtils
-import com.blankj.utilcode.util.LogUtils
-import com.blankj.utilcode.util.ToastUtils
 import com.lalilu.common.SystemUiUtil
 import com.lalilu.lmedia.LMedia
-import com.lalilu.lmedia.indexer.FilterType
-import com.lalilu.lmedia.indexer.Indexer
 import com.lalilu.lmusic.Config.REQUIRE_PERMISSIONS
 import com.lalilu.lmusic.compose.App
 import com.lalilu.lmusic.datastore.SettingsSp
 import com.lalilu.lmusic.helper.LastTouchTimeHelper
 import com.lalilu.lmusic.service.LMusicBrowser
-import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.flow.flatMapLatest
 import kotlinx.coroutines.flow.launchIn
-import kotlinx.coroutines.flow.mapLatest
 import kotlinx.coroutines.flow.onEach
 import org.koin.android.ext.android.inject
 
-@OptIn(
-    ExperimentalCoroutinesApi::class
-)
 class MainActivity : AppCompatActivity() {
     private val settingsSp: SettingsSp by inject()
     private val browser: LMusicBrowser by inject()
-    private var hasNewIntent = false
-
-    override fun onResume() {
-        super.onResume()
-        if (hasNewIntent) {
-            browser.whenConnected { handleIntent(intent) }
-            hasNewIntent = false
-        }
-    }
-
-
-    @SuppressLint("MissingSuperCall")
-    override fun onNewIntent(intent: Intent) {
-        super.onNewIntent(intent)
-        setIntent(intent)
-        hasNewIntent = true
-    }
-
-    private fun handleIntent(intent: Intent) {
-        if (intent.data == null) return
-
-        val start = System.currentTimeMillis()
-        LMedia.read(this, intent.data) {
-            if (it?.id == null) {
-                ToastUtils.showShort("解析失败: [${intent.data}]")
-                return@read
-            }
-            LogUtils.i("[onNewIntent]: 解析完成, 耗时：${System.currentTimeMillis() - start}ms", it)
-            browser.addToNext(it.id)
-            browser.playById(it.id)
-        }
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -96,32 +52,8 @@ class MainActivity : AppCompatActivity() {
             )
         }.launchIn(lifecycleScope)
 
-        /**
-         * 在LMedia初始化完成前，设置元素筛选器逻辑
-         */
-        Indexer.setFilterPipe {
-            settingsSp.run {
-                enableUnknownFilter.flow(true).flatMapLatest { hideUnknown ->
-                    blockedPaths.flow(true).flatMapLatest { blockedPaths ->
-                        durationFilter.flow(true).mapLatest { minDuration ->
-                            mutableListOf<FilterType>().also {
-                                if (blockedPaths != null) {
-                                    it.add(FilterType.Path(blockedPaths))
-                                }
-                                if (hideUnknown == true) {
-                                    it.add(FilterType.UnknownArtist)
-                                }
-                                if (minDuration != null && minDuration > 0) {
-                                    it.add(FilterType.Duration(minDuration))
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
+        LMedia.initialize(this)
 
-        Indexer.startListen()
         lifecycle.addObserver(browser)
         SystemUiUtil.immerseNavigationBar(this)
         SystemUiUtil.immersiveCutout(window)
@@ -132,7 +64,6 @@ class MainActivity : AppCompatActivity() {
         setContent { App.Content(activity = this) }
 
         volumeControlStream = AudioManager.STREAM_MUSIC
-        browser.whenConnected { handleIntent(intent) }
     }
 
     override fun dispatchTouchEvent(ev: MotionEvent?): Boolean {
