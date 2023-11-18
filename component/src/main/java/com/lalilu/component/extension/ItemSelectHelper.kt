90c3e34dc57d8c495f4086072b97db77384c9edf
lalilu <1248393538@qq.com>
2023-11-19 03:23:42 +0800

    [modify]添加SelectAction,使外部可统一实现和封装对应的操作

--- component/src/main/java/com/lalilu/component/extension/ItemSelectHelper.kt
+++ component/src/main/java/com/lalilu/component/extension/ItemSelectHelper.kt
@@ -1,9 +1,12 @@
 package com.lalilu.component.extension
 
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.graphics.Color
 
 data class ItemSelectHelper(
     val isSelecting: MutableState<Boolean>,
@@ -42,4 +45,21 @@ fun rememberItemSelectHelper(
             selected = selected
         )
     }
+}
+
+/**
+ * 定义针对选择器可执行的动作
+ */
+sealed interface SelectAction {
+    data class StaticAction(
+        @StringRes val title: Int,
+        @DrawableRes val icon: Int? = null,
+        @StringRes val info: Int? = null,
+        val color: Color = Color.Transparent,
+        val onAction: (ItemSelectHelper) -> Unit
+    ) : SelectAction
+
+    data class ComposeAction(
+        val content: @Composable (ItemSelectHelper) -> Unit
+    ) : SelectAction
 }
