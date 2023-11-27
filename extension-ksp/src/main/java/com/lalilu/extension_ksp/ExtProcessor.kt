805cb14bab7aaed40c201b7444ecc8307cd2897a
lalilu <1248393538@qq.com>
2023-11-27 14:54:48 +0800

    [modify]调整ksp生成代码的逻辑，解决Ksp经常不生成代码的问题

--- extension-ksp/src/main/java/com/lalilu/extension_ksp/ExtProcessor.kt
+++ extension-ksp/src/main/java/com/lalilu/extension_ksp/ExtProcessor.kt
@@ -6,6 +6,7 @@ import com.google.devtools.ksp.processing.Resolver
 import com.google.devtools.ksp.processing.SymbolProcessor
 import com.google.devtools.ksp.symbol.KSAnnotated
 import com.google.devtools.ksp.symbol.KSClassDeclaration
+import com.google.devtools.ksp.validate
 import com.squareup.kotlinpoet.FileSpec
 import com.squareup.kotlinpoet.FunSpec
 import com.squareup.kotlinpoet.ParameterizedTypeName.Companion.parameterizedBy
@@ -19,23 +20,32 @@ class ExtProcessor(
 ) : SymbolProcessor {
     companion object {
         private val listType = List::class.parameterizedBy(String::class)
-        val GENERATE_PACKAGE_NAME = "lalilu.extension_ksp"
-        val GENERATE_FILE_NAME = "ExtensionsConstants"
+        const val GENERATE_PACKAGE_NAME = "lalilu.extension_ksp"
+        const val GENERATE_FILE_NAME = "ExtensionsConstants"
 
         // 需手动修改与extension_core的保持一致
-        private val TARGET_ANNOTATION = "com.lalilu.extension_core.Ext"
+        private const val TARGET_ANNOTATION = "com.lalilu.extension_core.Ext"
     }
 
+    private val classNames: HashSet<String> = LinkedHashSet()
+
     override fun process(resolver: Resolver): List<KSAnnotated> {
-        val symbols = resolver.getSymbolsWithAnnotation(TARGET_ANNOTATION).toList()
+        val symbols = resolver.getSymbolsWithAnnotation(TARGET_ANNOTATION)
             .filterIsInstance<KSClassDeclaration>()
+            .toList()
 
         if (symbols.isEmpty()) return emptyList()
 
+        classNames.addAll(symbols.map { it.toClassName().toString() })
+
+        // 筛选返回不可解析的symbols
+        return symbols.filter { !it.validate() }.toList()
+    }
+
+    override fun finish() {
+        super.finish()
         val packageName = GENERATE_PACKAGE_NAME
         val fileName = GENERATE_FILE_NAME
-        val classNames = symbols.map { it.toClassName() }
-
         val listValue = "listOf(${classNames.joinToString(",") { "\"$it\"" }})"
 
         val function = FunSpec.builder("getClasses")
@@ -52,6 +62,5 @@ class ExtProcessor(
             .addType(classType)
             .build()
             .writeTo(codeGenerator, true)
-        return emptyList()
     }
 }
