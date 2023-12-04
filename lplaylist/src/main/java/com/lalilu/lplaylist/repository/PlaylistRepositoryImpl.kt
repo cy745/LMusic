4105f68af688cb62c52c3c1990f93159b6c580a7
lalilu <1248393538@qq.com>
2023-12-04 18:12:37 +0800

    [modify]实现PlaylistRepository的方法

--- lplaylist/src/main/java/com/lalilu/lplaylist/repository/PlaylistRepositoryImpl.kt
+++ lplaylist/src/main/java/com/lalilu/lplaylist/repository/PlaylistRepositoryImpl.kt
@@ -1,11 +1,16 @@
 package com.lalilu.lplaylist.repository
 
+import com.lalilu.common.base.SpListItem
 import com.lalilu.lplaylist.entity.LPlaylist
 
 
 internal class PlaylistRepositoryImpl(
     private val sp: PlaylistSp
 ) : PlaylistRepository {
+    override fun getPlaylists(): SpListItem<LPlaylist> {
+        return sp.playlistList
+    }
+
     override fun save(playlist: LPlaylist) {
         sp.playlistList.add(0, playlist)
     }
@@ -15,13 +20,35 @@ internal class PlaylistRepositoryImpl(
     }
 
     override fun removeById(id: String) {
-        val list = sp.playlistList.value
-        val index = list.indexOfFirst { it.id == id }
+        val result = sp.playlistList.value.filter { it.id != id }
+        sp.playlistList.value = result
+    }
 
-        if (index in list.indices) {
-            val result = list.toMutableList()
-            result.removeAt(index)
-            sp.playlistList.value = result
+    override fun removeByIds(ids: List<String>) {
+        val result = sp.playlistList.value.filter { !ids.contains(it.id) }
+        sp.playlistList.value = result
+    }
+
+    override fun addMediaIdsToPlaylists(mediaIds: List<String>, playlistIds: List<String>) {
+        var changed = false
+        val playlists = sp.playlistList.value.toMutableList()
+
+        for (index in playlists.indices) {
+            val playlist = playlists[index]
+            val playlistId = playlist.id
+            val exist = playlistIds.any { it == playlistId }
+            if (!exist) continue
+
+            val mediaIdsSet = playlist.mediaIds.toHashSet()
+                .also {
+                    changed = true
+                    it.addAll(mediaIds)
+                }
+
+            playlists[index] = playlist.copy(mediaIds = mediaIdsSet.toList())
         }
+
+        if (!changed) return
+        sp.playlistList.value = playlists
     }
 }
