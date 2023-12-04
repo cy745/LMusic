0b78f812aa1b2406e5863870173acad67804909f
lalilu <1248393538@qq.com>
2023-12-04 23:25:57 +0800

    [modify]创建longClickable函数,实现自定义长按触发回调时长的需求

---
+++ component/src/main/java/com/lalilu/component/extension/ComposeModifierExt.kt
@@ -0,0 +1,84 @@
+package com.lalilu.component.extension
+
+import androidx.compose.foundation.Indication
+import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.hoverable
+import androidx.compose.foundation.indication
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.PressInteraction
+import androidx.compose.material.ripple.rememberRipple
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
+import androidx.compose.ui.hapticfeedback.HapticFeedbackType
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.platform.LocalHapticFeedback
+import androidx.compose.ui.semantics.Role
+import androidx.compose.ui.semantics.role
+import androidx.compose.ui.semantics.semantics
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.isActive
+import kotlinx.coroutines.launch
+
+
+/**
+ * 可自定义长按回调触发时长的Modifier
+ */
+fun Modifier.longClickable(
+    onClick: () -> Unit,
+    onLongClick: () -> Unit,
+    onTap: () -> Unit = {},
+    onRelease: () -> Unit = {},
+    enableHaptic: Boolean = true,
+    longClickMinTimeMillis: Long = 1000L,
+    indication: Indication? = null,
+    interactionSource: MutableInteractionSource,
+): Modifier = composed {
+    val haptic = LocalHapticFeedback.current
+    val scope = rememberCoroutineScope()
+
+    this
+        .semantics { role = Role.Button }
+        .indication(
+            interactionSource = interactionSource,
+            indication = indication ?: rememberRipple()
+        )
+        .hoverable(interactionSource, true)
+        .pointerInput(Unit) {
+            var timer: Job?
+
+            detectTapGestures(
+                onPress = {
+                    val press = PressInteraction.Press(it)
+                    interactionSource.emit(press)
+                    onTap()
+
+                    // tap的瞬间开始计时器
+                    timer = scope.launch(Dispatchers.IO) {
+                        delay(longClickMinTimeMillis)
+                        if (!isActive) return@launch
+
+                        onLongClick()
+                        if (enableHaptic) {
+                            haptic.performHapticFeedback(HapticFeedbackType.TextHandleMove)
+                        }
+                    }
+
+                    // 阻塞直到松手
+                    if (tryAwaitRelease()) {
+                        interactionSource.emit(PressInteraction.Release(press))
+                    } else {
+                        interactionSource.emit(PressInteraction.Cancel(press))
+                    }
+
+                    // 取消计时器
+                    timer?.cancel()
+                    onRelease()
+                },
+                onTap = { onClick() },
+                onLongPress = {}
+            )
+        }
+}
