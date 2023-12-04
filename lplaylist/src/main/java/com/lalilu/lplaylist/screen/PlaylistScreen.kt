9b73c695efe257d115c17f0c0273bc1bdc00e0ba
lalilu <1248393538@qq.com>
2023-12-04 18:13:41 +0800

    [modify]调整完善歌单列表页面的布局，添加删除歌单的Action

--- lplaylist/src/main/java/com/lalilu/lplaylist/screen/PlaylistScreen.kt
+++ lplaylist/src/main/java/com/lalilu/lplaylist/screen/PlaylistScreen.kt
@@ -1,36 +1,34 @@
 package com.lalilu.lplaylist.screen
 
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import cafe.adriel.voyager.core.model.ScreenModel
 import cafe.adriel.voyager.core.model.rememberScreenModel
 import com.lalilu.component.LLazyColumn
 import com.lalilu.component.SelectPanelWrapper
 import com.lalilu.component.base.DynamicScreen
+import com.lalilu.component.base.NavigatorHeader
 import com.lalilu.component.base.ScreenInfo
 import com.lalilu.component.base.TabScreen
 import com.lalilu.component.extension.rememberItemSelectHelper
 import com.lalilu.component.navigation.GlobalNavigator
+import com.lalilu.lplaylist.PlaylistActions
 import com.lalilu.lplaylist.R
 import com.lalilu.lplaylist.component.PlaylistCard
 import com.lalilu.lplaylist.entity.LPlaylist
-import com.lalilu.lplaylist.repository.PlaylistSp
+import com.lalilu.lplaylist.repository.PlaylistRepository
 import org.burnoutcrew.reorderable.ReorderableItem
 import org.burnoutcrew.reorderable.detectReorder
 import org.burnoutcrew.reorderable.rememberReorderableLazyListState
@@ -51,20 +49,18 @@ data object PlaylistScreen : DynamicScreen(), TabScreen {
 
     @Composable
     override fun Content() {
-        val sp: PlaylistSp = koinInject()
-
-        PlaylistScreen(sp = sp)
+        PlaylistScreen()
     }
 }
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
 private fun DynamicScreen.PlaylistScreen(
-    sp: PlaylistSp = koinInject(),
     playlistSM: PlaylistScreenModel = rememberScreenModel { PlaylistScreenModel() },
+    playlistRepo: PlaylistRepository = koinInject(),
     navigator: GlobalNavigator = koinInject()
 ) {
-    val playlists = sp.obtainList<LPlaylist>("Playlist", autoSave = false)
+    val playlists = playlistRepo.getPlaylists()
     val state = rememberReorderableLazyListState(
         maxScrollPerFrame = 200.dp,
         onMove = { from, to ->
@@ -89,6 +85,7 @@ private fun DynamicScreen.PlaylistScreen(
     }
 
     SelectPanelWrapper(
+        selectActions = { listOf(PlaylistActions.removePlaylists) },
         selector = rememberItemSelectHelper(
             isSelecting = playlistSM.isSelecting,
             selected = playlistSM.selectedItems
@@ -101,16 +98,12 @@ private fun DynamicScreen.PlaylistScreen(
                 .reorderable(state)
         ) {
             item {
-                Row(
+                NavigatorHeader(
                     modifier = Modifier
                         .statusBarsPadding()
-                        .fillMaxWidth()
-                        .padding(20.dp),
-                    horizontalArrangement = Arrangement.SpaceBetween,
-                    verticalAlignment = Alignment.CenterVertically
+                        .fillMaxWidth(),
+                    title = stringResource(id = R.string.playlist_screen_title)
                 ) {
-                    Text("Playlist")
-
                     IconButton(
                         onClick = { navigator.navigateTo(PlaylistCreateScreen()) }
                     ) {
