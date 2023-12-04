a2f2be5fc9bc7f093e0c511aad0789838cdf1ec8
lalilu <1248393538@qq.com>
2023-12-04 18:11:52 +0800

    [modify]扩展PlaylistRepository的方法

--- lplaylist/src/main/java/com/lalilu/lplaylist/repository/PlaylistRepository.kt
+++ lplaylist/src/main/java/com/lalilu/lplaylist/repository/PlaylistRepository.kt
@@ -1,10 +1,14 @@
 package com.lalilu.lplaylist.repository
 
+import com.lalilu.common.base.SpListItem
 import com.lalilu.lplaylist.entity.LPlaylist
 
 interface PlaylistRepository {
-
+    fun getPlaylists(): SpListItem<LPlaylist>
     fun save(playlist: LPlaylist)
     fun remove(playlist: LPlaylist)
     fun removeById(id: String)
+    fun removeByIds(ids: List<String>)
+
+    fun addMediaIdsToPlaylists(mediaIds: List<String>, playlistIds: List<String>)
 }
