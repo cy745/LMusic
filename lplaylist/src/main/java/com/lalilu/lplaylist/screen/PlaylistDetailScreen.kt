763cc53a717771a51d69c40abd55de5fae76bcd3
lalilu <1248393538@qq.com>
2023-12-04 19:00:52 +0800

    [modify]添加从某歌单移除指定歌曲的按钮，初步完善歌单详情页面

--- lplaylist/src/main/java/com/lalilu/lplaylist/screen/PlaylistDetailScreen.kt
+++ lplaylist/src/main/java/com/lalilu/lplaylist/screen/PlaylistDetailScreen.kt
@@ -1,8 +1,19 @@
 package com.lalilu.lplaylist.screen
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.graphics.Color
+import com.blankj.utilcode.util.ToastUtils
+import com.lalilu.common.base.Playable
 import com.lalilu.component.Songs
 import com.lalilu.component.base.DynamicScreen
+import com.lalilu.component.base.NavigatorHeader
+import com.lalilu.component.extension.SelectAction
+import com.lalilu.lplaylist.R
+import com.lalilu.component.R as componentR
+import com.lalilu.lplaylist.repository.PlaylistRepository
 import com.lalilu.lplaylist.repository.PlaylistSp
 import org.koin.compose.koinInject
 
@@ -21,12 +32,48 @@ private fun DynamicScreen.PlaylistDetailScreen(
     playlistId: String,
     sp: PlaylistSp = koinInject()
 ) {
-    val mediaIds = sp.obtainList<String>("PlaylistDetail_${playlistId}", autoSave = false)
+    val playlistRepo = koinInject<PlaylistRepository>()
+    val playlist by remember {
+        derivedStateOf { sp.playlistList.value.firstOrNull { it.id == playlistId } }
+    }
+
+    if (playlist == null) {
+        return
+    }
+
+    val deleteAction = remember {
+        SelectAction.StaticAction.Custom(
+            title = R.string.playlist_action_remove_from_playlist,
+            icon = componentR.drawable.ic_delete_bin_6_line,
+            color = Color.Red
+        ) { selector ->
+            val mediaIds = selector.selected.value.filterIsInstance(Playable::class.java)
+                .map { it.mediaId }
+
+            playlistRepo.removeMediaIdsFromPlaylist(mediaIds, playlistId)
+            ToastUtils.showShort("Removed from playlist")
+        }
+    }
 
     Songs(
-        mediaIds = mediaIds.value,
+        mediaIds = playlist!!.mediaIds,
+        selectActions = { getAll ->
+            listOf(
+                SelectAction.StaticAction.SelectAll(getAll),
+                SelectAction.StaticAction.ClearAll,
+                deleteAction
+            )
+        },
         supportListAction = { emptyList() },
-    ) {
+        headerContent = {
+            item {
+                NavigatorHeader(
+                    title = playlist!!.title,
+                    subTitle = playlist!!.subTitle
+                ) {
 
-    }
+                }
+            }
+        }
+    )
 }
