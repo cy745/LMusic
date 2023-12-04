62ea43ccd20c188b5c3e792d60d592307ff7a906
lalilu <1248393538@qq.com>
2023-12-04 18:12:51 +0800

    [modify]去除PlaylistViewModel

--- lplaylist/src/main/java/com/lalilu/lplaylist/PlaylistModule.kt
+++ lplaylist/src/main/java/com/lalilu/lplaylist/PlaylistModule.kt
@@ -3,13 +3,10 @@ package com.lalilu.lplaylist
 import com.lalilu.lplaylist.repository.PlaylistRepository
 import com.lalilu.lplaylist.repository.PlaylistRepositoryImpl
 import com.lalilu.lplaylist.repository.PlaylistSp
-import com.lalilu.lplaylist.viewmodel.PlaylistViewModel
 import org.koin.android.ext.koin.androidApplication
-import org.koin.androidx.viewmodel.dsl.viewModelOf
 import org.koin.dsl.module
 
 val PlaylistModule = module {
     single { PlaylistSp(androidApplication()) }
     single<PlaylistRepository> { PlaylistRepositoryImpl(get()) }
-    viewModelOf(::PlaylistViewModel)
 }
--- lplaylist/src/main/java/com/lalilu/lplaylist/viewmodel/PlaylistViewModel.kt
+++
@@ -1,6 +0,0 @@
-package com.lalilu.lplaylist.viewmodel
-
-import androidx.lifecycle.ViewModel
-
-class PlaylistViewModel : ViewModel() {
-}
