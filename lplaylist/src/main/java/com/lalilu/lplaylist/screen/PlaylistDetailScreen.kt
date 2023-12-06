e52fd3e17b289a3714455f8ac5f3d3b4946e883c
lalilu <1248393538@qq.com>
2023-12-06 21:16:04 +0800

    [modify]完善歌单详情页面的操作按钮逻辑

--- lplaylist/src/main/java/com/lalilu/lplaylist/screen/PlaylistDetailScreen.kt
+++ lplaylist/src/main/java/com/lalilu/lplaylist/screen/PlaylistDetailScreen.kt
@@ -2,6 +2,8 @@ package com.lalilu.lplaylist.screen
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -9,21 +11,25 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import cafe.adriel.voyager.core.model.ScreenModel
 import cafe.adriel.voyager.koin.getScreenModel
 import com.blankj.utilcode.util.ToastUtils
 import com.lalilu.common.base.Playable
+import com.lalilu.common.toCachedFlow
 import com.lalilu.component.Songs
 import com.lalilu.component.base.DynamicScreen
 import com.lalilu.component.base.NavigatorHeader
+import com.lalilu.component.base.ScreenAction
 import com.lalilu.component.extension.SelectAction
+import com.lalilu.component.navigation.GlobalNavigator
+import com.lalilu.component.viewmodel.IPlayingViewModel
 import com.lalilu.lplaylist.R
-import com.lalilu.lplaylist.entity.LPlaylist
-import com.lalilu.lplaylist.extension.replaceForFavourite
 import com.lalilu.lplaylist.repository.PlaylistRepository
 import com.lalilu.lplaylist.repository.PlaylistSp
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.combine
+import org.koin.compose.koinInject
 import com.lalilu.component.R as componentR
 
 data class PlaylistDetailScreen(
@@ -35,12 +41,16 @@ data class PlaylistDetailScreen(
         val playlistDetailSM: PlaylistDetailScreenModel = getScreenModel()
         playlistDetailSM.updatePlaylistId(playlistId)
 
-        PlaylistDetailScreen(playlistDetailSM = playlistDetailSM)
+        PlaylistDetailScreen(
+            playlistId = playlistId,
+            playlistDetailSM = playlistDetailSM
+        )
     }
 }
 
 class PlaylistDetailScreenModel(
     sp: PlaylistSp,
+    private val playingVM: IPlayingViewModel,
     private val playlistRepo: PlaylistRepository
 ) : ScreenModel {
     private val playlistId = MutableStateFlow("")
@@ -48,7 +58,7 @@ class PlaylistDetailScreenModel(
     val playlist = playlistId
         .combine(sp.playlistList.flow(true)) { id, playlists ->
             playlists?.firstOrNull { it.id == id }
-        }
+        }.toCachedFlow()
 
     val deleteAction = SelectAction.StaticAction.Custom(
         title = R.string.playlist_action_remove_from_playlist,
@@ -63,6 +73,40 @@ class PlaylistDetailScreenModel(
         ToastUtils.showShort("Removed from playlist")
     }
 
+    val playAllAction = ScreenAction.StaticAction(
+        title = R.string.playlist_action_play_all,
+        icon = componentR.drawable.ic_play_list_2_fill,
+        color = Color(0xFF008521)
+    ) {
+        val mediaIds = playlist.get()?.mediaIds ?: emptyList()
+
+        if (mediaIds.isEmpty()) {
+            ToastUtils.showShort("No item to play")
+        } else {
+            playingVM.play(
+                mediaIds = mediaIds,
+                mediaId = mediaIds.first()
+            )
+        }
+    }
+
+    val playAllRandomlyAction = ScreenAction.StaticAction(
+        title = R.string.playlist_action_play_randomly,
+        icon = componentR.drawable.ic_dice_line,
+        color = Color(0xFF8D01B4)
+    ) {
+        val mediaIds = playlist.get()?.mediaIds ?: emptyList()
+
+        if (mediaIds.isEmpty()) {
+            ToastUtils.showShort("No item to play")
+        } else {
+            playingVM.play(
+                mediaIds = mediaIds.shuffled(),
+                mediaId = mediaIds.random()
+            )
+        }
+    }
+
     fun updatePlaylistId(playlistId: String) {
         this.playlistId.tryEmit(playlistId)
     }
@@ -75,8 +119,10 @@ class PlaylistDetailScreenModel(
 
 @Composable
 private fun DynamicScreen.PlaylistDetailScreen(
+    playlistId: String,
     playlistDetailSM: PlaylistDetailScreenModel,
 ) {
+    val navigator = koinInject<GlobalNavigator>()
     val playlist by playlistDetailSM.playlist.collectAsState(initial = null)
 
     if (playlist == null) {
@@ -87,6 +133,13 @@ private fun DynamicScreen.PlaylistDetailScreen(
             Text(text = "Playlist Load Error")
         }
         return
+    }
+
+    RegisterActions {
+        listOf(
+            playlistDetailSM.playAllRandomlyAction,
+            playlistDetailSM.playAllAction
+        )
     }
 
     Songs(
@@ -104,9 +157,22 @@ private fun DynamicScreen.PlaylistDetailScreen(
         headerContent = {
             item {
                 NavigatorHeader(
-                    title = replaceForFavourite(playlist!!, LPlaylist::title),
-                    subTitle = replaceForFavourite(playlist!!, LPlaylist::subTitle)
-                )
+                    title = playlist!!.title,
+                    subTitle = playlist!!.subTitle
+                ) {
+                    IconButton(
+                        onClick = {
+                            navigator.navigateTo(
+                                PlaylistCreateOrEditScreen(targetPlaylistId = playlistId)
+                            )
+                        }
+                    ) {
+                        Icon(
+                            painter = painterResource(componentR.drawable.ic_edit_line),
+                            contentDescription = null
+                        )
+                    }
+                }
             }
         }
     )
