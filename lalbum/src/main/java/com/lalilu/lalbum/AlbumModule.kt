8b4228038072f9da78cd3ae7f21612b9a9fb371e
lalilu <1248393538@qq.com>
2023-12-07 13:27:54 +0800

    [modify]恢复专辑详情页的基础结构

--- lalbum/src/main/java/com/lalilu/lalbum/AlbumModule.kt
+++ lalbum/src/main/java/com/lalilu/lalbum/AlbumModule.kt
@@ -1,7 +1,9 @@
 package com.lalilu.lalbum
 
+import com.lalilu.lalbum.screen.AlbumDetailScreenModel
+import org.koin.core.module.dsl.factoryOf
 import org.koin.dsl.module
 
 val AlbumModule = module {
-
+    factoryOf(::AlbumDetailScreenModel)
 }
--- lalbum/src/main/java/com/lalilu/lalbum/screen/AlbumDetailScreen.kt
+++ lalbum/src/main/java/com/lalilu/lalbum/screen/AlbumDetailScreen.kt
@@ -1,9 +1,31 @@
 package com.lalilu.lalbum.screen
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import cafe.adriel.voyager.core.model.ScreenModel
+import cafe.adriel.voyager.core.model.screenModelScope
+import cafe.adriel.voyager.koin.getScreenModel
+import com.lalilu.component.Songs
 import com.lalilu.lalbum.R
 import com.lalilu.component.base.DynamicScreen
+import com.lalilu.component.base.LoadingScaffold
+import com.lalilu.component.base.NavigatorHeader
 import com.lalilu.component.base.ScreenInfo
+import com.lalilu.component.base.collectAsLoadingState
+import com.lalilu.lalbum.component.AlbumCoverCard
+import com.lalilu.lmedia.LMedia
+import com.lalilu.lmedia.entity.LAlbum
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.launch
 
 data class AlbumDetailScreen(
     private val albumId: String
@@ -15,122 +37,74 @@ data class AlbumDetailScreen(
 
     @Composable
     override fun Content() {
-//        AlbumDetail(albumId = albumId)
+        val albumDetailSM = getScreenModel<AlbumDetailScreenModel>()
+
+        LaunchedEffect(Unit) {
+            albumDetailSM.updateAlbumId(albumId)
+        }
+
+        AlbumDetail(albumDetailSM = albumDetailSM)
     }
 }
 
-//
-//@Composable
-//private fun DynamicScreen.AlbumDetail(
-//    albumId: String,
-//    historyVM: HistoryViewModel = singleViewModel(),
-//    mediaVM: LMediaViewModel = singleViewModel(),
-//) {
-//    val album = mediaVM.requireAlbum(albumId) ?: run {
-//        Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-//            Text(text = "[Error]加载失败 #$albumId")
-//        }
-//        return
-//    }
-//
-//    val listState = rememberLazyListState()
-//    val sortFor = remember { "AlbumDetail" }
-////    val supportSortPresets = remember {
-////        listOf(
-////            SortPreset.SortByAddTime,
-////            SortPreset.SortByTitle,
-//////            SortPreset.SortByLastPlayTime,
-//////            SortPreset.SortByPlayedTimes,
-////            SortPreset.SortByDuration,
-////            SortPreset.SortByDiskAndTrackNumber
-////        )
-////    }
-////    val supportSortRules = remember {
-////        listOf(
-////            SortRule.Normal,
-////            SortRule.Title,
-////            SortRule.CreateTime,
-////            SortRule.ModifyTime,
-////            SortRule.TrackNumber,
-////            SortRule.ItemsCount,
-////            SortRule.ItemsDuration,
-////            SortRule.PlayCount,
-////            SortRule.LastPlayTime
-////        )
-////    }
-////    val supportGroupRules = remember {
-////        listOf(
-////            GroupRule.DiskNumber,
-////            GroupRule.CreateTime,
-////            GroupRule.ModifyTime,
-////            GroupRule.PinYinFirstLetter,
-////            GroupRule.TitleFirstLetter
-////        )
-////    }
-////    val supportOrderRules = remember {
-////        listOf(
-////            OrderRule.Normal,
-////            OrderRule.Reverse,
-////            OrderRule.Shuffle
-////        )
-////    }
-//
-//    Songs(
-//        sortFor = sortFor,
-//        mediaIds = album.songs.map { it.mediaId },
-//        listState = listState,
-//        showPrefixContent = {
-//            it.value == SortRuleStatic.TrackNumber::class.java.name ||
-//                    it.value == SortRulePlayCount::class.java.name
-//        },
-//        supportListAction = { emptyList() },
-//        headerContent = {
-//            item {
-//                AlbumCoverCard(
-//                    modifier = Modifier.padding(horizontal = 20.dp, vertical = 10.dp),
-//                    shape = RoundedCornerShape(10.dp),
-//                    elevation = 2.dp,
-//                    imageData = { album },
-//                    onClick = { }
-//                )
-//            }
-//
-//            item {
-//                NavigatorHeader(
-//                    title = album.name,
-//                    subTitle = "共 ${it.value.values.flatten().size} 首歌曲，总时长 ${
-//                        album.requireItemsDuration().durationToTime()
-//                    }"
-//                )
-//            }
-//        },
-//        prefixContent = { item, sortRuleStr ->
-//            var icon = -1
-//            var text = ""
-//            when (sortRuleStr.value) {
-//                SortRuleStatic.TrackNumber::class.java.name -> {
-//                    icon = R.drawable.ic_music_line
-//                    text = if (item is LSong) item.track.toString() else ""
-//                }
-//
-//                SortRulePlayCount::class.java.name -> {
-//                    icon = R.drawable.headphone_fill
-//                    text = historyVM.requiteHistoryCountById(item.mediaId).toString()
-//                }
-//            }
-//            if (icon != -1) {
-//                Icon(
-//                    modifier = Modifier.size(10.dp),
-//                    painter = painterResource(id = icon),
-//                    contentDescription = ""
-//                )
-//            }
-//            if (text.isNotEmpty()) {
-//                Text(
-//                    text = text,
-//                    fontSize = 10.sp
-//                )
-//            }
-//        }
-//    )
-//}
+@OptIn(ExperimentalCoroutinesApi::class)
+class AlbumDetailScreenModel : ScreenModel {
+    private val albumId = MutableStateFlow<String?>(null)
+    val album = albumId.flatMapLatest { LMedia.getFlow<LAlbum>(it) }
+
+    fun updateAlbumId(albumId: String) = screenModelScope.launch {
+        this@AlbumDetailScreenModel.albumId.emit(albumId)
+    }
+}
+
+@Composable
+private fun DynamicScreen.AlbumDetail(
+    albumDetailSM: AlbumDetailScreenModel
+) {
+    val albumLoadingState = albumDetailSM.album.collectAsLoadingState()
+
+    LoadingScaffold(
+        modifier = Modifier.fillMaxSize(),
+        targetState = albumLoadingState,
+        onLoadErrorContent = {
+            Box(modifier = Modifier.fillMaxSize()) {
+                Text(text = "loading")
+            }
+        }
+    ) { album ->
+        Songs(
+            modifier = Modifier.fillMaxSize(),
+            mediaIds = album.songs.map { it.mediaId },
+            sortFor = "ALBUM_DETAIL",
+            supportListAction = { emptyList() },
+            headerContent = {
+                item {
+                    AlbumCoverCard(
+                        modifier = Modifier.padding(horizontal = 20.dp, vertical = 10.dp),
+                        shape = RoundedCornerShape(10.dp),
+                        elevation = 2.dp,
+                        imageData = { album },
+                        onClick = { }
+                    )
+                }
+
+                item {
+                    NavigatorHeader(
+                        title = album.name,
+                        subTitle = "共 ${it.value.values.flatten().size} 首歌曲，总时长 ${
+                            album.requireItemsDuration().durationToTime()
+                        }"
+                    )
+                }
+            }
+        )
+    }
+}
+
+fun Long.durationToTime(): String {
+    val hour = this / 3600000
+    val minute = this / 60000 % 60
+    val second = this / 1000 % 60
+    return if (hour > 0L) "%02d:%02d:%02d".format(hour, minute, second)
+    else "%02d:%02d".format(minute, second)
+}
