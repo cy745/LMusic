cb795baf452568d3f5d410595c0ecf66aa90b289
lalilu <1248393538@qq.com>
2023-12-07 13:52:59 +0800

    [modify]恢复专辑瀑布流列表页的基础结构

--- lalbum/src/main/java/com/lalilu/lalbum/AlbumModule.kt
+++ lalbum/src/main/java/com/lalilu/lalbum/AlbumModule.kt
@@ -1,9 +1,11 @@
 package com.lalilu.lalbum
 
 import com.lalilu.lalbum.screen.AlbumDetailScreenModel
+import com.lalilu.lalbum.screen.AlbumsScreenModel
 import org.koin.core.module.dsl.factoryOf
 import org.koin.dsl.module
 
 val AlbumModule = module {
     factoryOf(::AlbumDetailScreenModel)
+    factoryOf(::AlbumsScreenModel)
 }
