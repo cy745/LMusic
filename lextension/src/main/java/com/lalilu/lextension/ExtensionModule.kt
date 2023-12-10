ac4481f0d5a2d3de9faf5fb72ee6d05dec8fc8c1
lalilu <1248393538@qq.com>
2023-12-10 16:07:11 +0800

    [modify]初步完善Extension列表页的拖拽排序逻辑

--- lextension/src/main/java/com/lalilu/lextension/ExtensionModule.kt
+++ lextension/src/main/java/com/lalilu/lextension/ExtensionModule.kt
@@ -1,9 +1,13 @@
 package com.lalilu.lextension
 
+import com.lalilu.lextension.repository.ExtensionSp
 import com.lalilu.lextension.screen.ExtensionsScreenModel
+import org.koin.android.ext.koin.androidApplication
 import org.koin.core.module.dsl.factoryOf
 import org.koin.dsl.module
 
 val ExtensionModule = module {
+    single<ExtensionSp> { ExtensionSp(androidApplication()) }
+
     factoryOf(::ExtensionsScreenModel)
 }
