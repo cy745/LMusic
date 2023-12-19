20c8525039b8332c9ea426c043388ee0bd967110
lalilu <1248393538@qq.com>
2023-12-19 21:31:29 +0800

    [modify]添加debounce防抖逻辑

--- common/src/main/java/com/lalilu/common/UpdatableFlow.kt
+++ common/src/main/java/com/lalilu/common/UpdatableFlow.kt
@@ -1,18 +1,25 @@
 package com.lalilu.common
 
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.FlowPreview
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.FlowCollector
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.debounce
 import kotlinx.coroutines.flow.flatMapLatest
 
 /**
  * 将[Flow]转为可手动更新并将更新下传至其他[Flow]的[UpdatableFlow]
  */
-@OptIn(ExperimentalCoroutinesApi::class)
-class UpdatableFlow<T>(private val flow: Flow<T>) : Flow<T> {
-    private val currentTimeFlow = MutableStateFlow(System.currentTimeMillis())
-    private val rootFlow = currentTimeFlow.flatMapLatest { flow }
+@OptIn(ExperimentalCoroutinesApi::class, FlowPreview::class)
+class UpdatableFlow<T>(
+    private val flow: Flow<T>,
+    debouncingInterval: Long = 0
+) : Flow<T> {
+    private val currentTimeFlow = MutableStateFlow(-1L)
+    private val rootFlow = currentTimeFlow
+        .debounce { value -> if (value == -1L) 0 else debouncingInterval }
+        .flatMapLatest { flow }
 
     override suspend fun collect(collector: FlowCollector<T>) {
         rootFlow.collect(collector)
@@ -23,6 +30,8 @@ class UpdatableFlow<T>(private val flow: Flow<T>) : Flow<T> {
     }
 }
 
-fun <T> Flow<T>.toUpdatableFlow(): UpdatableFlow<T> {
-    return UpdatableFlow(this)
+fun <T> Flow<T>.toUpdatableFlow(
+    debouncingInterval: Long = 0
+): UpdatableFlow<T> {
+    return UpdatableFlow(this, debouncingInterval)
 }
