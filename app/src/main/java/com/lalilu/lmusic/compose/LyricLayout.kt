5c833bac0743e54927f91a15f129fb7f820fff05
lalilu <1248393538@qq.com>
2024-01-11 00:41:20 +0800

    [modify]解决歌词的行号更新过快导致滚动时抽动的问题，完善动态更新位移的逻辑

--- app/src/main/java/com/lalilu/lmusic/compose/LyricLayout.kt
+++ app/src/main/java/com/lalilu/lmusic/compose/LyricLayout.kt
@@ -5,6 +5,9 @@ import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.spring
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.combinedClickable
+import androidx.compose.foundation.interaction.collectIsDraggedAsState
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.PaddingValues
@@ -45,13 +48,21 @@ import androidx.dynamicanimation.animation.springAnimationOf
 import androidx.dynamicanimation.animation.withSpringForceProperties
 import com.dirror.lyricviewx.LyricEntry
 import com.lalilu.component.extension.rememberLazyListScrollToHelper
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.FlowPreview
 import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.debounce
 import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.mapLatest
+import kotlinx.coroutines.isActive
 import kotlin.random.Random
 
+@OptIn(FlowPreview::class, ExperimentalCoroutinesApi::class)
 @Composable
 fun LyricLayout(
+    modifier: Modifier = Modifier,
     currentTime: () -> Long = { 0L },
+    onItemLongClick: () -> Unit = {},
     lyricEntry: State<List<LyricEntry>> = remember { mutableStateOf(emptyList()) },
     fontFamily: State<FontFamily?> = remember { mutableStateOf<FontFamily?>(null) }
 ) {
@@ -83,6 +94,8 @@ fun LyricLayout(
         }
     }
 
+    val isDragged = listState.interactionSource.collectIsDraggedAsState()
+
     val currentValue = remember { mutableFloatStateOf(0f) }
     val targetValue = remember { mutableFloatStateOf(0f) }
     val deltaValue = remember { mutableFloatStateOf(0f) }
@@ -101,8 +114,9 @@ fun LyricLayout(
             dampingRatio = SpringForce.DAMPING_RATIO_NO_BOUNCY
             stiffness = SpringForce.STIFFNESS_VERY_LOW
         }.addEndListener { animation, canceled, value, velocity ->
-            targetRange.value = IntRange.EMPTY
-            println("Animation End  isCanceled: $canceled")
+            if (!canceled) {
+                targetRange.value = IntRange.EMPTY
+            }
         }
     }
 
@@ -121,75 +135,94 @@ fun LyricLayout(
                 list.forEach {
                     // 若当前更新的元素处于动画偏移值计算数据源的目标范围中
                     if (it.index in targetRange.value && animator.isRunning) {
-                        val oldValue = sizeMap[it.index] ?: 0
-                        // 则将变化值直接更新到targetValue，触发动画位移修正
-                        targetValue.floatValue += it.size - oldValue
+                        val delta = it.size - (sizeMap[it.index] ?: 0)
+
+                        if (delta != 0) {
+                            // 则将变化值直接更新到targetValue，触发动画位移修正
+                            println("update targetValue:  [${it.index} -> $delta]")
+                            targetValue.floatValue += delta
+                        }
                     }
                     sizeMap[it.index] = it.size
                 }
             }
     }
 
-    LaunchedEffect(line.value) {
-        // 1. 从当前可见元素直接查找offset （准确值）
-        // get the offset directly from the visibleItemsInfo
-        val offset = listState.layoutInfo.visibleItemsInfo
-            .firstOrNull { it.key == line.value?.time }
-            ?.offset
+    LaunchedEffect(Unit) {
+        snapshotFlow { line.value }
+            .distinctUntilChanged()
+            .mapLatest {
+                // 1. 从当前可见元素直接查找offset （准确值）
+                // get the offset directly from the visibleItemsInfo
+                val offset = listState.layoutInfo.visibleItemsInfo
+                    .firstOrNull { it.key == line.value?.time }
+                    ?.offset
 
-        if (offset != null) {
-            animator.cancel()
-            currentValue.floatValue = 0f
-            targetValue.floatValue =
-                offset.toFloat() + Random(System.currentTimeMillis()).nextFloat() * 0.1f
-            // 添加随机值，为了确保能触发LaunchedEffect重组
+                if (offset != null) {
+                    animator.cancel()
+                    currentValue.floatValue = 0f
+                    targetValue.floatValue =
+                        offset.toFloat() + Random(System.currentTimeMillis()).nextFloat() * 0.1f
+                    // 添加随机值，为了确保能触发LaunchedEffect重组
 
-            println("[target]: ${targetValue.floatValue}")
-            return@LaunchedEffect
-        }
+                    println("[visible target]: ${targetValue.floatValue}")
+                    return@mapLatest offset
+                }
 
-        // 2. 使用实时维护的sizeMap查找并计算目标元素的offset （非准确值）
-        // Use the real-time maintained sizeMap to find and calculate the offset of the target element
-        val index = scrollToHelper.keys.indexOfFirst { it == line.value?.time }
-        if (index == -1) return@LaunchedEffect // 元素不存在keys列表中，则不进行滚动
+                return@mapLatest null
+            }
+            .debounce(20L)
+            .collectLatest { result ->
+                if (result != null) return@collectLatest
 
-        val firstVisibleIndex = listState.firstVisibleItemIndex
-        val firstVisibleOffset = listState.firstVisibleItemScrollOffset
-        targetRange.value = minOf(firstVisibleIndex, index)..maxOf(firstVisibleIndex, index)
+                // 2. 使用实时维护的sizeMap查找并计算目标元素的offset （非准确值）
+                // Use the real-time maintained sizeMap to find and calculate the offset of the target element
+                val index = scrollToHelper.keys.indexOfFirst { it == line.value?.time }
+                if (index == -1) return@collectLatest // 元素不存在keys列表中，则不进行滚动
 
-        // 计算方向乘数，向下滚动则为正数
-        // calculate the direction multiplier, if scrolling down, it's positive
-        val forwardMultiple = if (index >= firstVisibleIndex) 1f else -1f
+                if (!isActive) return@collectLatest
+                val firstVisibleIndex = listState.firstVisibleItemIndex
+                val firstVisibleOffset = listState.firstVisibleItemScrollOffset
+                targetRange.value = minOf(firstVisibleIndex, index)..maxOf(firstVisibleIndex, index)
 
-        // 计算目标距离，若未缓存有相应位置的值，则计算使用平均值
-        // calculate the target offset，if these no value cached then use the average value
-        val sizeAverage = sizeMap.values.average().toInt()
-        val sizeSum = targetRange.value.sumOf { sizeMap.getOrPut(it) { sizeAverage } }
-        val spacingSum =
-            (targetRange.value.last - targetRange.value.first) * listState.layoutInfo.mainAxisItemSpacing
-        var offsetTemp = (sizeSum + spacingSum) * forwardMultiple
+                // 计算方向乘数，向下滚动则为正数
+                // calculate the direction multiplier, if scrolling down, it's positive
+                val forwardMultiple = if (index >= firstVisibleIndex) 1f else -1f
 
-        val firstVisibleHeight = listState.layoutInfo.visibleItemsInfo
-            .getOrNull(firstVisibleIndex)?.size
-            ?: sizeMap[firstVisibleIndex]
-            ?: return@LaunchedEffect // TODO 获取当前可见的第一个元素的高度可能会失败，需要处理获取不到这个高度的情况
+                if (!isActive) return@collectLatest
+                // 计算目标距离，若未缓存有相应位置的值，则计算使用平均值
+                // calculate the target offset，if these no value cached then use the average value
+                val sizeAverage = sizeMap.values.average().toInt()
+                val sizeSum = targetRange.value.sumOf { sizeMap.getOrPut(it) { sizeAverage } }
+                val spacingSum =
+                    (targetRange.value.last - targetRange.value.first) * listState.layoutInfo.mainAxisItemSpacing
+                var offsetTemp = (sizeSum + spacingSum) * forwardMultiple
 
-        // 针对firstVisibleItem的边界情况修正offset值
-        // fix the offset value for the boundary case of the firstVisibleItem
-        offsetTemp += if (forwardMultiple > 0) firstVisibleOffset else (firstVisibleOffset - firstVisibleHeight)
+                val firstVisibleHeight = listState.layoutInfo.visibleItemsInfo
+                    .getOrNull(firstVisibleIndex)?.size
+                    ?: sizeMap[firstVisibleIndex]
+                    ?: sizeAverage
 
-        // 使用非准确值进行滚动
-        // use the non-accurate value for scrolling
-        animator.cancel()
-        currentValue.floatValue = 0f
-        targetValue.floatValue = offsetTemp + Random(System.currentTimeMillis()).nextFloat() * 0.1f
-        println("[target]: ${targetValue.floatValue}")
+                // 针对firstVisibleItem的边界情况修正offset值
+                // fix the offset value for the boundary case of the firstVisibleItem
+                offsetTemp += if (forwardMultiple > 0) firstVisibleOffset else (firstVisibleOffset - firstVisibleHeight)
+
+                // 使用非准确值进行滚动
+                // use the non-accurate value for scrolling
+                if (!isActive) return@collectLatest
+                animator.cancel()
+                currentValue.floatValue = 0f
+                targetValue.floatValue =
+                    offsetTemp + Random(System.currentTimeMillis()).nextFloat() * 0.1f
+
+                println("[calculate target]: ${targetValue.floatValue} -> range: [${targetRange.value.first} -> ${targetRange.value.last}]")
+            }
     }
 
     LazyColumn(
         state = listState,
-        modifier = Modifier.fillMaxSize(),
-        userScrollEnabled = false,
+        modifier = modifier.fillMaxSize(),
+        userScrollEnabled = true,
         contentPadding = PaddingValues(top = 300.dp, bottom = 500.dp),
         verticalArrangement = Arrangement.spacedBy(20.dp)
     ) {
@@ -209,6 +242,7 @@ fun LyricLayout(
                     constraints = constraints,
                     textMeasurer = textMeasurer,
                     fontFamily = fontFamily,
+                    onLongClick = { onItemLongClick() },
                     isCurrent = { it.time == line.value?.time }
                 )
             }
@@ -217,6 +251,7 @@ fun LyricLayout(
     }
 }
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun LyricSentence(
     lyric: LyricEntry,
@@ -224,6 +259,8 @@ fun LyricSentence(
     textMeasurer: TextMeasurer,
     fontFamily: State<FontFamily?>,
     density: Density = LocalDensity.current,
+    onLongClick: () -> Unit = {},
+    onClick: () -> Unit = {},
     isCurrent: () -> Boolean
 ) {
     val result = remember {
@@ -263,6 +300,7 @@ fun LyricSentence(
         modifier = Modifier
             .fillMaxWidth()
             .height(heightDp)
+            .combinedClickable(onLongClick = onLongClick, onClick = onClick)
     ) {
         scale(
             scale = scale.value / 100f,
