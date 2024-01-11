72fd512d7e5edf2e2ace2ce974798b61c55316fa
lalilu <1248393538@qq.com>
2024-01-12 01:44:17 +0800

    [modify]优化StackBlurUtils，添加预加载方法，增加extraKey用于扩展区分逻辑

--- app/src/main/java/com/lalilu/lmusic/utils/StackBlurUtils.kt
+++ app/src/main/java/com/lalilu/lmusic/utils/StackBlurUtils.kt
@@ -3,15 +3,50 @@ package com.lalilu.lmusic.utils
 import android.graphics.Bitmap
 import android.util.LruCache
 import com.enrique.stackblur.NativeBlurProcess
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.isActive
+import kotlinx.coroutines.launch
+import kotlin.coroutines.CoroutineContext
 
-object StackBlurUtils : NativeBlurProcess() {
-    private val cache = LruCache<String, Bitmap?>(50)
+object StackBlurUtils : NativeBlurProcess(), CoroutineScope {
+    const val MAX_RADIUS = 40
+
+    private val cache = LruCache<String, Bitmap?>(MAX_RADIUS + 1)
+    override val coroutineContext: CoroutineContext = Dispatchers.Default
+    private var preloadJob: Job? = null
 
     fun evictAll() = cache.evictAll()
 
-    fun processWithCache(source: Bitmap, radius: Int): Bitmap? {
-        return cache.get("${source.generationId}|$radius") ?: blur(source, radius.toFloat()).also {
-            cache.put("${source.generationId}|$radius", it)
+    fun processWithCache(
+        source: Bitmap,
+        radius: Int,
+        extraKey: String = ""
+    ): Bitmap? {
+        val key = "${source.generationId}|$extraKey|$radius"
+        return cache.get(key) ?: blur(source, radius.toFloat()).also { cache.put(key, it) }
+    }
+
+    fun preload(
+        source: Bitmap,
+        extraKey: String = ""
+    ) {
+        preloadJob?.cancel()
+        preloadJob = launch {
+            (1..MAX_RADIUS).map { radius ->
+                async {
+                    val key = "${source.generationId}|$extraKey|$radius"
+                    if (cache.get(key) != null || !isActive) return@async
+
+                    val temp = blur(source, radius.toFloat())
+                    if (cache.get(key) != null || !isActive) return@async
+
+                    cache.put(key, temp)
+                }
+            }.awaitAll()
         }
     }
 }
