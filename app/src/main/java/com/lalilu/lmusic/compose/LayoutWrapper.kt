1b7291852bd3ef5f6542ed85a1151864a6a641a3
lalilu <1248393538@qq.com>
2024-01-25 16:48:57 +0800

    [modify]优化代码

--- app/src/main/java/com/lalilu/lmusic/compose/LayoutWrapper.kt
+++ app/src/main/java/com/lalilu/lmusic/compose/LayoutWrapper.kt
@@ -7,19 +7,13 @@ import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.platform.LocalConfiguration
-import cafe.adriel.voyager.core.annotation.InternalVoyagerApi
-import cafe.adriel.voyager.navigator.Navigator
-import cafe.adriel.voyager.navigator.compositionUniqueId
-import com.lalilu.component.base.HiddenBottomSheetScreen
 import com.lalilu.component.base.LocalWindowSize
 import com.lalilu.component.extension.rememberIsPad
-import com.lalilu.lmusic.compose.new_screen.HomeScreen
 import com.lalilu.lmusic.compose.screen.ShowScreen
 import com.lalilu.lmusic.compose.screen.playing.PlayingLayout
 
 object LayoutWrapper {
 
-    @OptIn(InternalVoyagerApi::class)
     @Composable
     fun Content(windowSize: WindowSizeClass = LocalWindowSize.current) {
         val configuration = LocalConfiguration.current
@@ -28,25 +22,16 @@ object LayoutWrapper {
             derivedStateOf { configuration.orientation == Configuration.ORIENTATION_LANDSCAPE }
         }
 
-        // 共用Navigator避免切换时导致导航栈丢失
-        Navigator(
-            HomeScreen,
-            onBackPressed = null,
-            key = compositionUniqueId()
-        ) { navigator ->
-            DrawerWrapper.Content(
-                isPad = { isPad },
-                isLandscape = { isLandscape },
-                mainContent = { PlayingLayout() },
-                secondContent = {
-                    NavigationWrapper.Content(
-                        navigator = navigator,
-                        emptyScreen = remember { HiddenBottomSheetScreen },
-                        forPad = { isPad && isLandscape }
-                    )
-                }
-            )
-        }
+        DrawerWrapper.Content(
+            isPad = { isPad },
+            isLandscape = { isLandscape },
+            mainContent = { PlayingLayout() },
+            secondContent = {
+                NavigationWrapper.Content(
+                    forPad = { isPad && isLandscape }
+                )
+            }
+        )
 
         if (isLandscape) {
             ShowScreen()
--- app/src/main/java/com/lalilu/lmusic/compose/NavigationWrapper.kt
+++ app/src/main/java/com/lalilu/lmusic/compose/NavigationWrapper.kt
@@ -12,13 +12,16 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import cafe.adriel.voyager.core.screen.Screen
+import cafe.adriel.voyager.core.annotation.InternalVoyagerApi
 import cafe.adriel.voyager.navigator.Navigator
+import cafe.adriel.voyager.navigator.compositionUniqueId
 import com.lalilu.component.base.BottomSheetNavigatorLayout
+import com.lalilu.component.base.HiddenBottomSheetScreen
 import com.lalilu.component.base.SideSheetNavigatorLayout
 import com.lalilu.component.base.TabScreen
 import com.lalilu.component.navigation.SheetNavigator
 import com.lalilu.lmusic.compose.component.navigate.NavigationSheetContent
+import com.lalilu.lmusic.compose.new_screen.HomeScreen
 
 
 @OptIn(ExperimentalMaterialApi::class)
@@ -31,13 +34,15 @@ object NavigationWrapper {
         @Composable
         get() = remember { navigator?.isVisible ?: false }
 
+    @OptIn(InternalVoyagerApi::class)
     @Composable
     fun Content(
         modifier: Modifier = Modifier,
-        navigator: Navigator,
-        emptyScreen: Screen,
         forPad: () -> Boolean = { false }
     ) {
+        val emptyScreen = remember {
+            HiddenBottomSheetScreen
+        }
         val animationSpec = remember {
             SpringSpec<Float>(
                 dampingRatio = Spring.DampingRatioNoBouncy,
@@ -45,56 +50,63 @@ object NavigationWrapper {
             )
         }
 
-        if (forPad()) {
-            SideSheetNavigatorLayout(
-                modifier = modifier.fillMaxSize(),
-                navigator = navigator,
-                defaultIsVisible = isSheetVisible && navigator.lastItemOrNull !is TabScreen,
-                scrimColor = Color.Black.copy(alpha = 0.5f),
-                sheetBackgroundColor = MaterialTheme.colors.background,
-                animationSpec = animationSpec,
-                sheetContent = { sheetNavigator ->
-                    NavigationWrapper.navigator = sheetNavigator
-                    NavigationSheetContent(
-                        modifier = modifier,
-                        transitionKeyPrefix = "SideSheet",
-                        sheetNavigator = sheetNavigator,
-                        getScreenFrom = {
-                            sheetNavigator.lastItemOrNull?.takeIf { it !is TabScreen }
-                                ?: emptyScreen
-                        }
-                    )
-                },
-                content = { sheetNavigator ->
-                    NavigationSheetContent(
-                        modifier = modifier,
-                        transitionKeyPrefix = "Tab",
-                        sheetNavigator = sheetNavigator,
-                        getScreenFrom = {
-                            sheetNavigator.items.lastOrNull { it is TabScreen }
-                                ?: emptyScreen
-                        }
-                    )
-                }
-            )
-        } else {
-            BottomSheetNavigatorLayout(
-                modifier = modifier.fillMaxSize(),
-                navigator = navigator,
-                defaultIsVisible = isSheetVisible,
-                scrimColor = Color.Black.copy(alpha = 0.5f),
-                sheetBackgroundColor = MaterialTheme.colors.background,
-                animationSpec = animationSpec,
-                sheetContent = { sheetNavigator ->
-                    NavigationWrapper.navigator = sheetNavigator
-                    NavigationSheetContent(
-                        modifier = modifier,
-                        transitionKeyPrefix = "bottomSheet",
-                        sheetNavigator = sheetNavigator
-                    )
-                },
-                content = { }
-            )
+        // 共用Navigator避免切换时导致导航栈丢失
+        Navigator(
+            HomeScreen,
+            onBackPressed = null,
+            key = compositionUniqueId()
+        ) { navigator ->
+            if (forPad()) {
+                SideSheetNavigatorLayout(
+                    modifier = modifier.fillMaxSize(),
+                    navigator = navigator,
+                    defaultIsVisible = isSheetVisible && navigator.lastItemOrNull !is TabScreen,
+                    scrimColor = Color.Black.copy(alpha = 0.5f),
+                    sheetBackgroundColor = MaterialTheme.colors.background,
+                    animationSpec = animationSpec,
+                    sheetContent = { sheetNavigator ->
+                        NavigationWrapper.navigator = sheetNavigator
+                        NavigationSheetContent(
+                            modifier = modifier,
+                            transitionKeyPrefix = "SideSheet",
+                            sheetNavigator = sheetNavigator,
+                            getScreenFrom = {
+                                sheetNavigator.lastItemOrNull?.takeIf { it !is TabScreen }
+                                    ?: emptyScreen
+                            }
+                        )
+                    },
+                    content = { sheetNavigator ->
+                        NavigationSheetContent(
+                            modifier = modifier,
+                            transitionKeyPrefix = "Tab",
+                            sheetNavigator = sheetNavigator,
+                            getScreenFrom = {
+                                sheetNavigator.items.lastOrNull { it is TabScreen }
+                                    ?: emptyScreen
+                            }
+                        )
+                    }
+                )
+            } else {
+                BottomSheetNavigatorLayout(
+                    modifier = modifier.fillMaxSize(),
+                    navigator = navigator,
+                    defaultIsVisible = isSheetVisible,
+                    scrimColor = Color.Black.copy(alpha = 0.5f),
+                    sheetBackgroundColor = MaterialTheme.colors.background,
+                    animationSpec = animationSpec,
+                    sheetContent = { sheetNavigator ->
+                        NavigationWrapper.navigator = sheetNavigator
+                        NavigationSheetContent(
+                            modifier = modifier,
+                            transitionKeyPrefix = "bottomSheet",
+                            sheetNavigator = sheetNavigator
+                        )
+                    },
+                    content = { }
+                )
+            }
         }
     }
 }
