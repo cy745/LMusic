427da67106219670be3ef3d8bf81c2b07a1cf0fb
lalilu <1248393538@qq.com>
2024-02-02 23:51:42 +0800

    [modify]优化SettingSwitcher，添加自定义样式参数

--- component/src/main/java/com/lalilu/component/settings/SettingSwitcher.kt
+++ component/src/main/java/com/lalilu/component/settings/SettingSwitcher.kt
@@ -1,10 +1,14 @@
 package com.lalilu.component.settings
 
 import androidx.annotation.StringRes
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.MarqueeSpacing
+import androidx.compose.foundation.basicMarquee
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -23,71 +27,89 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.lalilu.component.extension.enableFor
 
 @Composable
 fun SettingSwitcher(
     state: MutableState<Boolean>,
     @StringRes titleRes: Int,
-    @StringRes subTitleRes: Int? = null
+    @StringRes subTitleRes: Int? = null,
+    enableContentClickable: Boolean = true
 ) = SettingSwitcher(
     state = state,
     title = stringResource(id = titleRes),
-    subTitle = subTitleRes?.let { stringResource(id = it) }
+    subTitle = subTitleRes?.let { stringResource(id = it) },
+    enableContentClickable = enableContentClickable
 )
 
 @Composable
 fun SettingSwitcher(
     state: MutableState<Boolean>,
     title: String,
-    subTitle: String? = null
+    subTitle: String? = null,
+    enableContentClickable: Boolean = true
 ) = SettingSwitcher(
     state = { state.value },
     onStateUpdate = { state.value = it },
     title = title,
-    subTitle = subTitle
+    subTitle = subTitle,
+    enableContentClickable = enableContentClickable
 )
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun SettingSwitcher(
     state: () -> Boolean,
     onStateUpdate: (Boolean) -> Unit,
     title: String,
-    subTitle: String? = null
+    subTitle: String? = null,
+    enableContentClickable: Boolean = true
 ) {
     val textColor = contentColorFor(backgroundColor = MaterialTheme.colors.background)
 
     SettingSwitcher(
+        enableContentClickable = enableContentClickable,
         onContentStartClick = { onStateUpdate(!state()) },
         contentStart = {
             Text(
+                modifier = Modifier.basicMarquee(
+                    iterations = Int.MAX_VALUE,
+                    spacing = MarqueeSpacing(30.dp)
+                ),
                 text = title,
                 color = textColor,
                 fontSize = 14.sp
             )
             if (subTitle != null) {
                 Text(
+                    modifier = Modifier.basicMarquee(
+                        iterations = Int.MAX_VALUE,
+                        spacing = MarqueeSpacing(30.dp)
+                    ),
                     text = subTitle,
                     fontSize = 12.sp,
                     color = textColor.copy(0.5f)
                 )
             }
-        }
-    ) { interaction ->
-        Switch(
-            checked = state(),
-            onCheckedChange = { onStateUpdate(it) },
-            interactionSource = interaction,
-            colors = SwitchDefaults.colors(
-                checkedThumbColor = textColor.multiply(0.7f)
+        },
+        contentEnd = { interaction ->
+            Switch(
+                checked = state(),
+                onCheckedChange = { onStateUpdate(it) },
+                interactionSource = interaction,
+                colors = SwitchDefaults.colors(
+                    checkedThumbColor = textColor.multiply(0.7f)
+                )
             )
-        )
-    }
+        }
+    )
 }
 
 @Composable
 fun SettingSwitcher(
+    enableContentClickable: Boolean = true,
     onContentStartClick: () -> Unit = {},
-    contentStart: @Composable () -> Unit = {},
+    contentStart: @Composable ColumnScope.() -> Unit = {},
     contentEnd: @Composable (MutableInteractionSource) -> Unit = {}
 ) {
     val interactionSource = remember { MutableInteractionSource() }
@@ -95,21 +117,23 @@ fun SettingSwitcher(
     Row(
         modifier = Modifier
             .fillMaxWidth()
-            .clickable(
-                interactionSource = interactionSource,
-                indication = rememberRipple(),
-                onClick = onContentStartClick
-            )
+            .enableFor(enable = { enableContentClickable }) {
+                clickable(
+                    interactionSource = interactionSource,
+                    indication = rememberRipple(),
+                    onClick = onContentStartClick
+                )
+            }
             .padding(horizontal = 20.dp, vertical = 8.dp),
         verticalAlignment = Alignment.CenterVertically,
         horizontalArrangement = Arrangement.SpaceBetween
     ) {
         Column(
+            modifier = Modifier.weight(1f),
             horizontalAlignment = Alignment.Start,
-            verticalArrangement = Arrangement.spacedBy(5.dp)
-        ) {
-            contentStart()
-        }
+            verticalArrangement = Arrangement.spacedBy(5.dp),
+            content = contentStart
+        )
         contentEnd(interactionSource)
     }
 }
