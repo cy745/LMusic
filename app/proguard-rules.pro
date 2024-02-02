e73e0955926755d98017037efb9ce93a469ef0c2
lalilu <1248393538@qq.com>
2024-02-02 15:26:21 +0800

    [modify]完善AOP配置，将所有的android.util.Log调用重定向至LogUtils输出

--- app/proguard-rules.pro
+++ app/proguard-rules.pro
@@ -20,52 +20,23 @@
 # hide the original source file name.
 #-renamesourcefileattribute SourceFile
 
-# 取消混淆(obfuscate)，只执行压缩(shrink)、优化操作(optimize)
--dontobfuscate
-
--dontwarn org.bouncycastle.**
--dontwarn org.conscrypt.**
--dontwarn org.openjsse.**
-
--dontwarn org.jetbrains.kotlin.compiler.**
--dontwarn org.jetbrains.kotlin.diagnostics.**
-
-# retrofit 使用的所有bean都不可以混淆
--keep class com.lalilu.lmusic.apis.bean.** { *; }
-
+# Gson反射所需混淆规则
 -keep class com.google.gson.reflect.TypeToken
 -keep class * extends com.google.gson.reflect.TypeToken
 -keep public class * implements java.lang.reflect.Type
 
--keep public class * extends android.app.Activity
--keep class * implements android.os.Parcelable {
-    public static final android.os.Parcelable$Creator *;
+# AndroidAop必备混淆规则
+-keep class * { @androidx.annotation.Keep <fields>; }
+-keepnames class * implements com.flyjingfish.android_aop_annotation.base.BasePointCut
+-keepnames class * implements com.flyjingfish.android_aop_annotation.base.MatchClassMethod
+-keep class * implements com.flyjingfish.android_aop_annotation.base.BasePointCut{
+    public <init>();
 }
-# support
--keep public class * extends android.support.annotation.** { *; }
-# androidx
--keep public class * extends androidx.annotation.** { *; }
--keep public class * extends androidx.core.content.FileProvider
+-keep class * implements com.flyjingfish.android_aop_annotation.base.MatchClassMethod{
+    public <init>();
+}
 
 # 墨 · 状态栏歌词
 -keep class StatusBarLyric.API.StatusBarLyric { *; }
-
-# 为插件保留相应的依赖环境（既不混淆也不去除未使用的类）
--keep class kotlin.** { public protected *; }
--keep class kotlinx.coroutines.** { public protected *; }
--keep class androidx.lifecycle.** { public protected *; }
--keep class androidx.compose.** { public protected *; }
--keep class android.support.v4.media.** { public protected *; }
--keep class coil.compose.** { public protected *; }
-
--keep class com.lalilu.extension_core.** { public protected *; }
--keep class com.lalilu.common.** { public protected *; }
--keep class com.lalilu.lplayer.** { public protected *; }
--keep class lalilu.extension_ksp.ExtensionsConstants { public protected *;}
-
--keepclassmembers class * implements com.lalilu.extension_core.Extension {
-    <init>(...);
-    com.lalilu.extension_core.Extension *;
-}
 
 -printmapping ../mapping.txt
---
+++ app/src/main/java/com/lalilu/lmusic/aop/LogOverride.kt
@@ -0,0 +1,91 @@
+package com.lalilu.lmusic.aop
+
+import android.util.Log
+import com.blankj.utilcode.util.LogUtils
+import com.flyjingfish.android_aop_annotation.anno.AndroidAopReplaceClass
+import com.flyjingfish.android_aop_annotation.anno.AndroidAopReplaceMethod
+
+@AndroidAopReplaceClass("android.util.Log")
+class LogOverride {
+
+    companion object {
+        const val isEnableOriginOutput = false
+
+        @AndroidAopReplaceMethod("int v(java.lang.String,java.lang.String)")
+        @JvmStatic
+        fun v(tag: String, msg: String): Int {
+            LogUtils.vTag(tag, msg)
+            return if (isEnableOriginOutput) Log.v(tag, msg) else 0
+        }
+
+        @AndroidAopReplaceMethod("int v(java.lang.String,java.lang.String,java.lang.Throwable)")
+        @JvmStatic
+        fun v(tag: String, msg: String, tr: Throwable): Int {
+            LogUtils.vTag(tag, msg, tr)
+            return if (isEnableOriginOutput) Log.v(tag, msg, tr) else 0
+        }
+
+        @AndroidAopReplaceMethod("int d(java.lang.String,java.lang.String)")
+        @JvmStatic
+        fun d(tag: String, msg: String): Int {
+            LogUtils.dTag(tag, msg)
+            return if (isEnableOriginOutput) Log.d(tag, msg) else 0
+        }
+
+        @AndroidAopReplaceMethod("int d(java.lang.String,java.lang.String,java.lang.Throwable)")
+        @JvmStatic
+        fun d(tag: String, msg: String, tr: Throwable): Int {
+            LogUtils.dTag(tag, msg, tr)
+            return if (isEnableOriginOutput) Log.d(tag, msg, tr) else 0
+        }
+
+        @AndroidAopReplaceMethod("int i(java.lang.String,java.lang.String)")
+        @JvmStatic
+        fun i(tag: String, msg: String): Int {
+            LogUtils.iTag(tag, msg)
+            return if (isEnableOriginOutput) Log.i(tag, msg) else 0
+        }
+
+        @AndroidAopReplaceMethod("int i(java.lang.String,java.lang.String,java.lang.Throwable)")
+        @JvmStatic
+        fun i(tag: String, msg: String, tr: Throwable): Int {
+            LogUtils.iTag(tag, msg, tr)
+            return if (isEnableOriginOutput) Log.i(tag, msg, tr) else 0
+        }
+
+        @AndroidAopReplaceMethod("int w(java.lang.String,java.lang.String)")
+        @JvmStatic
+        fun w(tag: String, msg: String): Int {
+            LogUtils.wTag(tag, msg)
+            return if (isEnableOriginOutput) Log.w(tag, msg) else 0
+        }
+
+        @AndroidAopReplaceMethod("int w(java.lang.String,java.lang.String,java.lang.Throwable)")
+        @JvmStatic
+        fun w(tag: String, msg: String, tr: Throwable): Int {
+            LogUtils.wTag(tag, msg, tr)
+            return if (isEnableOriginOutput) Log.w(tag, msg, tr) else 0
+        }
+
+        @AndroidAopReplaceMethod("int w(java.lang.String,java.lang.Throwable)")
+        @JvmStatic
+        fun w(tag: String, tr: Throwable): Int {
+            LogUtils.wTag(tag, tr)
+            return if (isEnableOriginOutput) Log.w(tag, tr) else 0
+        }
+
+        @AndroidAopReplaceMethod("int e(java.lang.String,java.lang.String)")
+        @JvmStatic
+        fun e(tag: String, msg: String): Int {
+            LogUtils.eTag(tag, msg)
+            return if (isEnableOriginOutput) Log.e(tag, msg) else 0
+        }
+
+        @AndroidAopReplaceMethod("int e(java.lang.String,java.lang.String,java.lang.Throwable)")
+        @JvmStatic
+        fun e(tag: String, msg: String, tr: Throwable): Int {
+            LogUtils.eTag(tag, msg, tr)
+            return if (isEnableOriginOutput) Log.e(tag, msg, tr) else 0
+        }
+    }
+}
