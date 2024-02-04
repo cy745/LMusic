828b969d70098046b682ae12d21b8e094dbdb7b1
lalilu <1248393538@qq.com>
2024-02-05 00:39:05 +0800

    [modify]创建LPlaylistFastEncoder，完善LPlaylist的序列化过程

--- lplaylist/src/main/java/com/lalilu/lplaylist/entity/LPlaylist.kt
+++ lplaylist/src/main/java/com/lalilu/lplaylist/entity/LPlaylist.kt
@@ -1,9 +1,53 @@
 package com.lalilu.lplaylist.entity
 
+import io.fastkv.interfaces.FastEncoder
+import io.packable.PackCreator
+import io.packable.PackDecoder
+import io.packable.PackEncoder
+import io.packable.Packable
+
 data class LPlaylist(
     val id: String,
     val title: String,
     val subTitle: String,
     val coverUri: String,
     val mediaIds: List<String>
-)
+) : Packable {
+    override fun encode(encoder: PackEncoder) {
+        encoder.putString(0, id)
+            .putString(1, title)
+            .putString(2, subTitle)
+            .putString(3, coverUri)
+            .putStringList(4, mediaIds)
+    }
+
+    companion object CREATOR : PackCreator<LPlaylist> {
+        override fun decode(decoder: PackDecoder): LPlaylist? {
+            val id = decoder.getString(0) ?: return null
+            val title = decoder.getString(1) ?: "Unknown"
+            val subTitle = decoder.getString(2) ?: ""
+            val coverUri = decoder.getString(3) ?: ""
+            val mediaIds = decoder.getStringList(4) ?: emptyList()
+
+            return LPlaylist(
+                id = id,
+                title = title,
+                subTitle = subTitle,
+                coverUri = coverUri,
+                mediaIds = mediaIds
+            )
+        }
+    }
+}
+
+object LPlaylistFastEncoder : FastEncoder<LPlaylist> {
+    override fun tag(): String = "LPlaylistFastEncoder"
+
+    override fun decode(bytes: ByteArray, offset: Int, length: Int): LPlaylist {
+        return PackDecoder.unmarshal(bytes, offset, length, LPlaylist.CREATOR)
+    }
+
+    override fun encode(obj: LPlaylist): ByteArray {
+        return PackEncoder.marshal(obj)
+    }
+}
