63aa8ed0dab8e515ffc7fe35edf0e6eaea59787b
lalilu <1248393538@qq.com>
2024-02-05 00:39:50 +0800

    [modify]创建PlaylistKV替代PlaylistSp

--- lplaylist/src/main/java/com/lalilu/lplaylist/PlaylistModule.kt
+++ lplaylist/src/main/java/com/lalilu/lplaylist/PlaylistModule.kt
@@ -1,17 +1,17 @@
 package com.lalilu.lplaylist
 
+import com.lalilu.lplaylist.repository.PlaylistKV
 import com.lalilu.lplaylist.repository.PlaylistRepository
 import com.lalilu.lplaylist.repository.PlaylistRepositoryImpl
-import com.lalilu.lplaylist.repository.PlaylistSp
-import com.lalilu.lplaylist.screen.PlaylistDetailScreenModel
 import com.lalilu.lplaylist.screen.PlaylistCreateOrEditScreenModel
-import org.koin.android.ext.koin.androidApplication
+import com.lalilu.lplaylist.screen.PlaylistDetailScreenModel
 import org.koin.core.module.dsl.factoryOf
 import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
 
 val PlaylistModule = module {
-    single { PlaylistSp(androidApplication()) }
+    singleOf(::PlaylistKV)
+
     singleOf(::PlaylistRepositoryImpl)
     factoryOf(::PlaylistDetailScreenModel)
     factoryOf(::PlaylistCreateOrEditScreenModel)
