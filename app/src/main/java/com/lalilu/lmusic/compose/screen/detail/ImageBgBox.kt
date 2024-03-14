04c29d9b07a049205532fe03f1684853c458a67d
lalilu <1248393538@qq.com>
2024-03-15 01:04:34 +0800

    [create]封装使用图片作为背景的Box组件

---
+++ app/src/main/java/com/lalilu/lmusic/compose/screen/detail/ImageBgBox.kt
@@ -0,0 +1,46 @@
+package com.lalilu.lmusic.compose.screen.detail
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxScope
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
+
+
+@Composable
+fun ImageBgBox(
+    modifier: Modifier = Modifier,
+    contentAlignment: Alignment = Alignment.TopCenter,
+    imageData: Any? = null,
+    imageModifier: Modifier = Modifier,
+    content: @Composable BoxScope.() -> Unit = {},
+) {
+    val context = LocalContext.current
+    val model = remember(imageData) {
+        imageData?.let {
+            ImageRequest.Builder(context)
+                .data(it)
+                .crossfade(true)
+                .build()
+        }
+    }
+
+    Box(
+        modifier = modifier.fillMaxSize(),
+        contentAlignment = contentAlignment
+    ) {
+        AsyncImage(
+            modifier = imageModifier,
+            model = model,
+            contentScale = ContentScale.Crop,
+            contentDescription = ""
+        )
+        content()
+    }
+}
