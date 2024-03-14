c1f8fb7722a264803665cd170df8859163a7cf7d
lalilu <1248393538@qq.com>
2024-03-15 00:38:38 +0800

    [modify]提取NavigatorHeader组件的Padding为函数参数

--- component/src/main/java/com/lalilu/component/base/NavigatorHeader.kt
+++ component/src/main/java/com/lalilu/component/base/NavigatorHeader.kt
@@ -4,6 +4,7 @@ import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.padding
@@ -23,11 +24,18 @@ fun NavigatorHeader(
     @StringRes titleRes: Int,
     @StringRes subTitleRes: Int,
     titleScale: Float = 1f,
+    paddingValues: PaddingValues = PaddingValues(
+        top = 26.dp,
+        bottom = 20.dp,
+        start = 20.dp,
+        end = 20.dp
+    ),
     extraContent: @Composable RowScope.() -> Unit = {}
 ) = NavigatorHeader(
     modifier = modifier,
     title = stringResource(id = titleRes),
     subTitle = stringResource(id = subTitleRes),
+    paddingValues = paddingValues,
     titleScale = titleScale,
     extraContent = extraContent
 )
@@ -38,38 +46,49 @@ fun NavigatorHeader(
     title: String,
     subTitle: String,
     titleScale: Float = 1f,
+    paddingValues: PaddingValues = PaddingValues(
+        top = 26.dp,
+        bottom = 20.dp,
+        start = 20.dp,
+        end = 20.dp
+    ),
     extraContent: @Composable RowScope.() -> Unit = {}
-) {
-    NavigatorHeader(
-        modifier = modifier,
-        title = title,
-        titleScale = titleScale,
-        rowExtraContent = extraContent,
-        columnExtraContent = {
-            if (subTitle.isNotBlank()) {
-                Text(
-                    text = subTitle,
-                    fontSize = 14.sp,
-                    color = contentColorFor(backgroundColor = MaterialTheme.colors.background)
-                        .copy(alpha = 0.5f)
-                )
-            }
+) = NavigatorHeader(
+    modifier = modifier,
+    title = title,
+    titleScale = titleScale,
+    rowExtraContent = extraContent,
+    paddingValues = paddingValues,
+    columnExtraContent = {
+        if (subTitle.isNotBlank()) {
+            Text(
+                text = subTitle,
+                fontSize = 14.sp,
+                color = contentColorFor(backgroundColor = MaterialTheme.colors.background)
+                    .copy(alpha = 0.5f)
+            )
         }
-    )
-}
+    }
+)
 
 @Composable
 fun NavigatorHeader(
     modifier: Modifier = Modifier,
     title: String,
     titleScale: Float = 1f,
+    paddingValues: PaddingValues = PaddingValues(
+        top = 26.dp,
+        bottom = 20.dp,
+        start = 20.dp,
+        end = 20.dp
+    ),
     columnExtraContent: @Composable ColumnScope.() -> Unit = {},
     rowExtraContent: @Composable RowScope.() -> Unit = {}
 ) {
     Row(
         verticalAlignment = Alignment.Top,
         horizontalArrangement = Arrangement.spacedBy(20.dp),
-        modifier = modifier.padding(top = 26.dp, bottom = 20.dp, start = 20.dp, end = 20.dp)
+        modifier = modifier.padding(paddingValues)
     ) {
         Column(
             modifier = Modifier.weight(1f),
