363f3bed2efa6b0e60b19db3b36a762f1834d30e
lalilu <1248393538@qq.com>
2024-03-15 01:03:57 +0800

    [modify]优化完善HomeScreen布局

--- app/src/main/java/com/lalilu/lmusic/compose/new_screen/HomeScreen.kt
+++ app/src/main/java/com/lalilu/lmusic/compose/new_screen/HomeScreen.kt
@@ -1,23 +1,11 @@
 package com.lalilu.lmusic.compose.new_screen
 
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.BoxWithConstraints
-import androidx.compose.foundation.layout.ExperimentalLayoutApi
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.WindowInsets
-import androidx.compose.foundation.layout.asPaddingValues
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.statusBarsIgnoringVisibility
-import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.dp
 import com.lalilu.R
-import com.lalilu.component.LLazyColumn
+import com.lalilu.component.TwoColumnWithPad
 import com.lalilu.component.base.DynamicScreen
 import com.lalilu.component.base.ScreenInfo
 import com.lalilu.component.base.TabScreen
@@ -37,63 +25,42 @@ object HomeScreen : DynamicScreen(), TabScreen {
         icon = R.drawable.ic_loader_line
     )
 
-    @OptIn(ExperimentalLayoutApi::class)
     @Composable
     override fun Content() {
-        val density = LocalDensity.current
-        val vm: LibraryViewModel = singleViewModel()
+        val libraryVM: LibraryViewModel = singleViewModel()
         val historyVM: HistoryViewModel = singleViewModel()
         val playingVM: PlayingViewModel = singleViewModel()
-        val paddingTop = WindowInsets.statusBarsIgnoringVisibility.asPaddingValues()
-            .calculateTopPadding()
 
         LaunchedEffect(Unit) {
-            vm.checkOrUpdateToday()
+            libraryVM.checkOrUpdateToday()
         }
 
-        BoxWithConstraints(modifier = Modifier.fillMaxSize()) {
-            val expended = with(density) { constraints.maxWidth.toDp() } > 500.dp
-
-            Row(modifier = Modifier.fillMaxSize()) {
-                if (expended) {
-                    LLazyColumn(
-                        modifier = Modifier
-                            .fillMaxHeight()
-                            .wrapContentWidth(),
-                        contentPadding = PaddingValues(top = paddingTop, start = 20.dp),
-                        verticalArrangement = Arrangement.spacedBy(20.dp)
-                    ) {
-                        dailyRecommendVertical(libraryVM = vm)
-                    }
+        TwoColumnWithPad(
+            arrangementForPad = Arrangement.spacedBy(10.dp),
+            columnForPad = {
+                dailyRecommendVertical(libraryVM = libraryVM)
+            },
+            columnForNormal = { isPad ->
+                if (!isPad) {
+                    dailyRecommend(
+                        libraryVM = libraryVM,
+                    )
                 }
 
-                LLazyColumn(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .weight(1f),
-                    contentPadding = PaddingValues(top = paddingTop)
-                ) {
-                    if (!expended) {
-                        dailyRecommend(
-                            libraryVM = vm,
-                        )
-                    }
+                latestPanel(
+                    libraryVM = libraryVM,
+                    playingVM = playingVM
+                )
 
-                    latestPanel(
-                        libraryVM = vm,
-                        playingVM = playingVM
-                    )
+                historyPanel(
+                    historyVM = historyVM,
+                    playingVM = playingVM
+                )
 
-                    historyPanel(
-                        historyVM = historyVM,
-                        playingVM = playingVM
-                    )
-
-                    item {
-                        EntryPanel()
-                    }
+                item {
+                    EntryPanel()
                 }
             }
-        }
+        )
     }
 }
