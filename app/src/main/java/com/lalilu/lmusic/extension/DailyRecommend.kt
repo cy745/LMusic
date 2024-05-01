599432584ffd9d132f7d3839953254c9716209ea
lalilu <1248393538@qq.com>
2024-05-01 18:01:40 +0800

    [modify]优化dailyRecommend组件的显示效果，适配平板端

--- app/src/main/java/com/lalilu/lmusic/extension/DailyRecommend.kt
+++ app/src/main/java/com/lalilu/lmusic/extension/DailyRecommend.kt
@@ -1,16 +1,29 @@
 package com.lalilu.lmusic.extension
 
-import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.animation.AnimatedContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyListScope
-import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Chip
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import com.lalilu.component.base.LocalWindowSize
 import com.lalilu.lmusic.GlobalNavigatorImpl
 import com.lalilu.lmusic.compose.component.card.RecommendCard2
 import com.lalilu.lmusic.compose.component.card.RecommendRow
@@ -23,6 +36,7 @@ fun LazyListScope.dailyRecommend(
 ) {
     item {
         RecommendTitle(
+            modifier = Modifier.padding(vertical = 8.dp),
             title = "每日推荐",
             onClick = {
                 val ids = libraryVM.dailyRecommends.value.map { it.mediaId }
@@ -38,51 +52,124 @@ fun LazyListScope.dailyRecommend(
         }
     }
     item {
-        RecommendRow(
-            items = { libraryVM.dailyRecommends.value },
-            getId = { it.id }
-        ) {
+        AnimatedContent(
+            targetState = LocalWindowSize.current.widthSizeClass,
+            label = ""
+        ) { windowWidthSizeClass ->
+            when (windowWidthSizeClass) {
+                WindowWidthSizeClass.Medium -> RecommendRowForSizeMedium(libraryVM)
+                WindowWidthSizeClass.Expanded -> RecommendRowForSizeExpanded(libraryVM)
+                else -> RecommendRow(
+                    items = { libraryVM.dailyRecommends.value },
+                    getId = { it.id }
+                ) {
+                    RecommendCard2(
+                        item = { it },
+                        modifier = Modifier.size(width = 250.dp, height = 250.dp),
+                        onClick = { GlobalNavigatorImpl.goToDetailOf(mediaId = it.id) }
+                    )
+                }
+            }
+        }
+    }
+}
+
+
+@Composable
+fun RecommendRowForSizeMedium(libraryVM: LibraryViewModel) {
+    val items by remember { derivedStateOf { libraryVM.dailyRecommends.value.take(3) } }
+
+    Row(
+        modifier = Modifier
+            .height(250.dp)
+            .fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        items[0].let {
             RecommendCard2(
                 item = { it },
-                contentModifier = Modifier.size(width = 250.dp, height = 250.dp),
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .weight(1f),
+                onClick = { GlobalNavigatorImpl.goToDetailOf(mediaId = it.id) }
+            )
+        }
+
+        items[1].let {
+            RecommendCard2(
+                item = { it },
+                modifier = Modifier
+                    .width(150.dp)
+                    .fillMaxHeight(),
+                onClick = { GlobalNavigatorImpl.goToDetailOf(mediaId = it.id) }
+            )
+        }
+
+        items[2].let {
+            RecommendCard2(
+                item = { it },
+                modifier = Modifier
+                    .width(150.dp)
+                    .fillMaxHeight(),
                 onClick = { GlobalNavigatorImpl.goToDetailOf(mediaId = it.id) }
             )
         }
     }
 }
 
+@Composable
+fun RecommendRowForSizeExpanded(libraryVM: LibraryViewModel) {
+    val items by remember { derivedStateOf { libraryVM.dailyRecommends.value.take(3) } }
 
-@OptIn(ExperimentalMaterialApi::class)
-fun LazyListScope.dailyRecommendVertical(
-    libraryVM: LibraryViewModel,
-) {
-    item {
-        RecommendTitle(
-            modifier = Modifier.width(250.dp),
-            paddingValues = PaddingValues(),
-            title = "每日推荐",
-            onClick = {
-                val ids = libraryVM.dailyRecommends.value.map { it.mediaId }
-                GlobalNavigatorImpl.showSongs(ids)
-            }
+    Row(
+        modifier = Modifier
+            .height(250.dp)
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        items[0].let {
+            RecommendCard2(
+                item = { it },
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .weight(1f),
+                onClick = { GlobalNavigatorImpl.goToDetailOf(mediaId = it.id) }
+            )
+        }
+
+        Column(
+            modifier = Modifier
+                .width(275.dp)
+                .fillMaxHeight(),
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Chip(onClick = { libraryVM.forceUpdate() }) {
-                Text(
-                    style = MaterialTheme.typography.caption,
-                    text = "换一换"
+            items[1].let {
+                RecommendCard2(
+                    item = { it },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .weight(1f),
+                    onClick = {
+                        GlobalNavigatorImpl.goToDetailOf(mediaId = it.id)
+                    }
+                )
+            }
+
+            items[2].let {
+                RecommendCard2(
+                    item = { it },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .weight(1f),
+                    onClick = {
+                        GlobalNavigatorImpl.goToDetailOf(mediaId = it.id)
+                    }
                 )
             }
         }
-    }
-    items(
-        items = libraryVM.dailyRecommends.value,
-        key = { it.id },
-        contentType = { "dailyRecommendsCard" }
-    ) {
-        RecommendCard2(
-            item = { it },
-            contentModifier = Modifier.size(width = 250.dp, height = 250.dp),
-            onClick = { GlobalNavigatorImpl.goToDetailOf(mediaId = it.id) }
-        )
     }
 }
