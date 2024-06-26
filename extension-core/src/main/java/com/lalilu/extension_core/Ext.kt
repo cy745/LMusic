1c052ada0651f4f80e2e7b18e119f4101e23c044
lalilu <1248393538@qq.com>
2024-06-27 02:00:15 +0800

    [modify]删除无用代码

--- extension-core/src/main/java/com/lalilu/extension_core/Ext.kt
+++
@@ -1,5 +0,0 @@
-package com.lalilu.extension_core
-
-@Target(AnnotationTarget.CLASS)
-@Retention(AnnotationRetention.SOURCE)
-annotation class Ext
--- register/src/main/java/com/lalilu/register/ClassInfo.kt
+++
@@ -1,9 +0,0 @@
-package com.lalilu.register
-
-class ClassInfo(
-    val className: String,
-    var isObject: Boolean = false,
-    var isAbleToCreate: Boolean = false,
-    var isInterface: Boolean = false,
-    var isAbstract: Boolean = false
-)
