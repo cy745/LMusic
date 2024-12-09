852f073efa20aa56e01f81f6bfb2bc11fa71c185
cy745 <1248393538@qq.com>
2024-12-09 15:19:58 +0800

    [refactor]转移Playlist相关逻辑至对应模块

--- app/src/main/java/com/lalilu/lmusic/compose/new_screen/detail/SongDetailScreen.kt
+++ app/src/main/java/com/lalilu/lmusic/compose/new_screen/detail/SongDetailScreen.kt
@@ -15,6 +15,7 @@ import androidx.compose.ui.res.stringResource
 import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.core.screen.ScreenKey
 import com.lalilu.R
+import com.lalilu.common.ext.requestFor
 import com.lalilu.component.base.LocalEnhanceSheetState
 import com.lalilu.component.base.screen.ScreenAction
 import com.lalilu.component.base.screen.ScreenActionFactory
@@ -25,11 +26,12 @@ import com.lalilu.component.extension.DynamicTipsItem
 import com.lalilu.component.override.ModalBottomSheetValue
 import com.lalilu.lmedia.LMedia
 import com.lalilu.lmedia.entity.LSong
-import com.lalilu.lmusic.compose.screen.detail.provideSongLikeAction
 import com.lalilu.lmusic.compose.screen.detail.provideSongPlayAction
 import com.lalilu.lplayer.extensions.QueueAction
 import com.zhangke.krouter.annotation.Destination
 import com.zhangke.krouter.annotation.Param
+import org.koin.core.parameter.parametersOf
+import org.koin.core.qualifier.named
 
 @Destination("/pages/songs/detail")
 data class SongDetailScreen(
@@ -46,8 +48,11 @@ data class SongDetailScreen(
 
     @Composable
     override fun provideScreenActions(): List<ScreenAction> = remember(this) {
-        listOf(
-            provideSongLikeAction(mediaId),
+        listOfNotNull<ScreenAction>(
+            requestFor<ScreenAction>(
+                qualifier = named("like_action"),
+                parameters = { parametersOf(mediaId) }
+            ),
             provideSongPlayAction(mediaId),
             ScreenAction.Static(
                 title = { stringResource(id = R.string.button_set_song_to_next) },
--- lplaylist/src/main/java/com/lalilu/lplaylist/PlaylistActions.kt
+++ lplaylist/src/main/java/com/lalilu/lplaylist/PlaylistActions.kt
@@ -1,7 +1,34 @@
 package com.lalilu.lplaylist
 
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.SpringSpec
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsPressedAsState
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.selection.toggleable
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.hapticfeedback.HapticFeedbackType
+import androidx.compose.ui.platform.LocalHapticFeedback
+import androidx.compose.ui.semantics.Role
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.blankj.utilcode.util.ToastUtils
 import com.lalilu.RemixIcon
 import com.lalilu.common.ext.requestFor
@@ -11,8 +38,10 @@ import com.lalilu.lmedia.entity.LSong
 import com.lalilu.lplaylist.repository.PlaylistRepository
 import com.lalilu.remixicon.HealthAndMedical
 import com.lalilu.remixicon.Media
+import com.lalilu.remixicon.healthandmedical.heart3Fill
 import com.lalilu.remixicon.healthandmedical.heart3Line
 import com.lalilu.remixicon.media.playListAddLine
+import kotlinx.coroutines.launch
 import org.koin.core.annotation.Factory
 import org.koin.core.annotation.Named
 
@@ -51,3 +80,72 @@ fun provideAddToFavouriteAction(
         }
     }
 )
+
+@Factory(binds = [ScreenAction::class])
+@Named("like_action")
+fun provideLikeAction(
+    mediaId: String,
+    playlistRepo: PlaylistRepository,
+) = ScreenAction.Dynamic { actionContext ->
+    val isLiked by playlistRepo.isItemInFavourite(mediaId)
+        .collectAsState(initial = false)
+
+    val scope = rememberCoroutineScope()
+    val interactionSource: MutableInteractionSource = remember { MutableInteractionSource() }
+    val haptic = LocalHapticFeedback.current
+    val pressedState = interactionSource.collectIsPressedAsState()
+    val iconColor by animateColorAsState(
+        targetValue = if (isLiked) MaterialTheme.colors.primary
+        else MaterialTheme.colors.onBackground.copy(0.3f),
+        label = ""
+    )
+    val scaleValue by animateFloatAsState(
+        animationSpec = SpringSpec(dampingRatio = Spring.DampingRatioMediumBouncy),
+        targetValue = if (pressedState.value) 1.2f else 1f,
+        label = ""
+    )
+
+    Surface(
+        modifier = Modifier,
+        color = iconColor.copy(0.15f)
+    ) {
+        Row(
+            modifier = Modifier
+                .padding(horizontal = 20.dp)
+                .toggleable(
+                    value = isLiked,
+                    onValueChange = { like ->
+                        scope.launch {
+                            if (like) playlistRepo.addMediaIdsToFavourite(mediaIds = listOf(mediaId))
+                            else playlistRepo.removeMediaIdsFromFavourite(mediaIds = listOf(mediaId))
+                        }
+                        if (like) haptic.performHapticFeedback(HapticFeedbackType.TextHandleMove)
+                    },
+                    role = Role.Checkbox,
+                    interactionSource = interactionSource,
+                    indication = null
+                ),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            Icon(
+                modifier = Modifier
+                    .size(24.dp)
+                    .scale(scaleValue),
+                imageVector = if (isLiked) RemixIcon.HealthAndMedical.heart3Fill else RemixIcon.HealthAndMedical.heart3Line,
+                tint = iconColor,
+                contentDescription = "A Checkable Button"
+            )
+
+            if (actionContext.isFullyExpanded) {
+                Text(
+                    text = "收藏",
+                    fontSize = 14.sp,
+                    lineHeight = 14.sp,
+                    color = iconColor,
+                    fontWeight = FontWeight.Medium
+                )
+            }
+        }
+    }
+}
